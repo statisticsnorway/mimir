132268488bca4a227d036eb00abb1f82f9f78453
Øyvind Nordli <oe.nordli@gmail.com>
2020-01-17 15:22:20 +0100

    fetch tbml data, and convert from xml to json

---
+++ src/main/resources/lib/tbml/tbml.ts
@@ -0,0 +1,33 @@
+import { HttpLibrary } from 'enonic-types/lib/http'
+import { TbmlData, XmlParser } from '../types/xmlParser'
+const xmlParser: XmlParser = __.newBean('no.ssb.xp.xmlparser.XmlParser')
+const http: HttpLibrary = __non_webpack_require__( '/lib/http-client')
+
+export function fetch(tbmlId: string): string {
+  const url: string = `https://i.utv.ssb.no/tbprocessor/process/tbmldata/${tbmlId}`
+  let result: string = '<tbml></tbml>'
+
+  try {
+    const body: string | null = http.request({
+      url
+    }).body
+
+    if (body) {
+      result = body
+    }
+  } catch (e) {
+    log.error('Failed fetching ')
+    log.error(e)
+  }
+
+  return result
+}
+
+export function getTbmlData(tbmlId: string): TbmlData {
+  return xmlToJson(fetch(tbmlId))
+}
+
+function xmlToJson(xml: string): TbmlData {
+  const result: TbmlData = __.toNativeObject(xmlParser.parse(xml))
+  return result
+}
