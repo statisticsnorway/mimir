869c9ecef303b93df87413a7be37789ce8390697
Øyvind Nordli <oe.nordli@gmail.com>
2020-01-20 13:18:15 +0100

    fix typing in dataquery.ts

--- src/main/resources/lib/dataquery.ts
+++ src/main/resources/lib/dataquery.ts
@@ -1,11 +1,10 @@
-const http = __non_webpack_require__( '/lib/http-client')
+import { HttpResponse, HttpLibrary } from 'enonic-types/lib/http'
+const http: HttpLibrary = __non_webpack_require__( '/lib/http-client')
 
-const readTimeout = 5000
-const connectionTimeout = 20000
-const headers = { 'Cache-Control': 'no-cache', 'Accept': 'application/json' }
-const contentType = 'application/json'
-
-exports.get = function(url, json, selection = { filter: 'all', values: ['*'] }) {
+export function get(url: string, json: DataqueryRequestData, selection: SelectionFilter = {
+  filter: 'all',
+  values: ['*']
+}): object | null {
   if (json && json.query) {
     for (const query of json.query) {
       if (query.code === 'KOKkommuneregion0000' || query.code === 'Region') {
@@ -13,10 +12,41 @@ exports.get = function(url, json, selection = { filter: 'all', values: ['*'] }) 
       }
     }
   }
-  const method = json && json.query ? 'POST' : 'GET'
-  const result = http.request({ url, method, headers, connectionTimeout, readTimeout, body: JSON.stringify(json, null, ''), contentType })
+  const method: string = json && json.query ? 'POST' : 'GET'
+  const result: HttpResponse = http.request({
+    url,
+    method,
+    contentType: 'application/json',
+    headers: {
+      'Cache-Control': 'no-cache',
+      'Accept': 'application/json'
+    },
+    connectionTimeout: 20000,
+    readTimeout: 5000,
+    body: JSON.stringify(json, null, '')
+  })
   result.status !== 200 && log.error(`HTTP ${url} (${result.status} ${result.message})`)
   result.status !== 200 && log.info(JSON.stringify(json, null, ' '))
   result.status !== 200 && log.info(JSON.stringify(result, null, ' '))
-  return result.status === 200 && JSON.parse(result.body)
+  if (result.status === 200 && result.body) {
+    return JSON.parse(result.body)
+  }
+  return null
+}
+
+export interface SelectionFilter {
+  filter: string;
+  values: Array<string>;
+}
+
+export interface DataqueryRequestData {
+  query: Array<Dimension>;
+  response: {
+    format: string;
+  };
+}
+
+export interface Dimension {
+  code: string;
+  selection: SelectionFilter;
 }
