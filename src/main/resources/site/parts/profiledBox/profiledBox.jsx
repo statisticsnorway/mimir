6e63e458155ffed057ff692a82f9dd9a59e32143
johnnadeluy <johnnadel.salita@gmail.com>
2020-02-12 10:34:59 +0100

    Adds margin-top for part.
    MIMIR-157

--- src/main/resources/site/parts/profiledBox/profiledBox.jsx
+++ src/main/resources/site/parts/profiledBox/profiledBox.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Card, Paragraph } from '@statisticsnorway/ssb-component-library'
 
-export default (props) => <Card {...props} image={<img src={props.imgUrl} alt="" />}>
-  <Paragraph {...props}>{props.preambleText}</Paragraph>
-</Card>
+export default (props) => <div className="part-profiledBox">
+  <Card {...props} image={<img src={props.imgUrl} alt="" />}>
+    <Paragraph {...props}>{props.preambleText}</Paragraph>
+  </Card>
+</div>
