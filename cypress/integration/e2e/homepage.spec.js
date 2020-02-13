a3ff8505a65c9e9a5791f8948493991ab7e810bd
Khalid Sahlqeath <khalid.sahlqeath@soprasteria.com>
2020-02-13 15:44:26 +0100

    set environment variables for test environment

--- cypress/integration/e2e/homepage.spec.js
+++ cypress/integration/e2e/homepage.spec.js
@@ -4,4 +4,10 @@ describe("Request https://www.test.ssb.no/", () => {
     it('Check if site is up', () => {
         cy.request('https://www.test.ssb.no/')        
     })
+    it('Request municipality fact page', () => [
+        cy.request('https://www.test.ssb.no/kommunefakta')        
+    ])
+    it('Request specific municipality', () => {
+        cy.request('https://www.test.ssb.no/kommunefakta/ski')
+    })
 })
