59cdb5fdc7e568d8f2ecf781db3ed3d8f569ba0b
Khalid Sahlqeath <khalid.sahlqeath@soprasteria.com>
2020-02-13 16:36:15 +0100

    start-server-and-test added

--- cypress/integration/e2e/homepage.spec.js
+++ cypress/integration/e2e/homepage.spec.js
@@ -4,9 +4,9 @@ describe("Request https://www.test.ssb.no/", () => {
     it('Check if site is up', () => {
         cy.request('https://www.test.ssb.no/')        
     })
-    it('Request municipality fact page', () => [
+    it('Request municipality fact page', () => {
         cy.request('https://www.test.ssb.no/kommunefakta')        
-    ])
+    })
     it('Request specific municipality', () => {
         cy.request('https://www.test.ssb.no/kommunefakta/ski')
     })
