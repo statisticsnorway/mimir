a275b75d8acd94ba3536a87346271dc48ece4738
Larsen, Glenruben Engen <Glenruben.Larsen@ssb.no>
2020-02-19 17:17:43 +0100

    Converts accordion part to React
    - Refactors controller
    - Rewrites view to use react
    - prints all html pretty like
    
    MIMIR-288

---
+++ src/main/resources/site/parts/accordion/accordion.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Accordion, NestedAccordion } from '@statisticsnorway/ssb-component-library'
+
+function createMarkup(html) {
+  return {
+    __html: html
+  }
+}
+
+export default (props) =>
+  <div>
+    {props.accordions.map((accordion) =>
+      <Accordion key={accordion.id} header={accordion.open}>
+        <div dangerouslySetInnerHTML={createMarkup(accordion.body)}/>
+        {accordion.items.map((item, i) =>
+          <NestedAccordion key={accordion.key + i} header={item.title}>
+            <div dangerouslySetInnerHTML={createMarkup(item.body)}/>
+          </NestedAccordion>)}
+      </Accordion>
+    )}
+  </div>
