9ba8a59132e8ecb3dc85658c52def8394d721133
Øyvind Dahl <oyvind.dahl@item.no>
2020-02-28 09:42:37 +0100

    Items didnt possition on bottom of elements - Change wrap button content in a span and set display flex on span
    In some browsers, chrome and safari was the one I saw this error in, do not support other display
    settings than block or inline-block. Thats why flex elements would not possition themselves on the
    bottom because button was not set to be flex.
    
    - Add more mobile styling.
    - Add more logic for opening individual submenues
    
    MIMIR-300

--- src/main/resources/react4xp/_entries/Header.jsx
+++ src/main/resources/react4xp/_entries/Header.jsx
@@ -1,68 +1,125 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Divider, Input, Link, Tabs } from '@statisticsnorway/ssb-component-library'
+import { Divider, Input, Link } from '@statisticsnorway/ssb-component-library'
+import { ChevronDown, ChevronUp, Menu, X } from 'react-feather';
 
-
-//const Header = (props) => {
+// const Header = (props) => {
 class Header extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       showSubMenu: false,
-      currentFocusedElement: undefined,
-      searchFieldInput: ''
+      showMainMenuOnMobile: false,
+      searchFieldInput: '',
+      mainMenu: props.mainNavigation.map( () => false )
     }
+
+    this.goToSearchResultPage = this.goToSearchResultPage.bind(this)
+    this.languageLinks = this.languageLinks.bind(this)
+    this.renderSubMenu = this.renderSubMenu.bind(this)
+    this.toggleMainMenu = this.toggleMainMenu.bind(this)
+    this.toggleSubMenu = this.toggleSubMenu.bind(this)
+    this.topLinks = this.topLinks.bind(this)
   }
 
   goToSearchResultPage(value) {
     window.location = `${this.props.searchResultPageUrl}?sok=${value}`
   }
 
-  toggleSubMenu(element) {
-    this.setState({showSubMenu: !this.state.showSubMenu})
+  toggleMainMenu() {
+    this.setState({
+      showMainMenuOnMobile: !this.state.showMainMenuOnMobile
+    })
   }
 
+  toggleSubMenu(index) {
+    const mainMenu = [...this.state.mainMenu]
+    mainMenu[index] = !mainMenu[index]
+    this.setState({
+      showSubMenu: !this.state.showSubMenu,
+      mainMenu: mainMenu
+    })
+  }
+
+  menuButtonStatus() {
+    if(this.state.showMainMenuOnMobile) {
+      return (<span>Lukk <X /></span> )
+    } else {
+      return (<span>Meny <Menu/></span>)
+    }
+  }
+  languageLinks() {
+    return this.props.language.alternativeLanguages.map((altLanguage, index) => {
+      return (<Link key={'link_' + index} href={altLanguage.path}>{altLanguage.title}</Link>)
+    })
+  }
+  renderSubMenu(topMenuItem) {
+    return topMenuItem.menuItems && topMenuItem.menuItems.map((menuItem, itemIndex) => {
+      return (
+        <li key={itemIndex}>
+          <Link tabindex="-1" href={menuItem.path} icon={<img src={menuItem.icon}></img>}>{menuItem.title}</Link>
+        </li>)
+      })
+  }
+
+  topLinks() {
+    return this.props.topLinks.map((topLink, index) => {
+      return (<Link key={'link_' + index} href={topLink.path}>{topLink.title}</Link>)
+    })
+  }
+
+
   render() {
-    const {topLinks, language, searchInputPlaceholder, logoUrl, mainNavigation } = this.props
+    const {
+      searchInputPlaceholder, logoUrl, mainNavigation
+    } = this.props
     return (
       <header className="ssb-header-wrapper">
-        <nav className="global-links" style={{float: 'right', marginBottom: '12px', marginTop: '10px'}}>
-            {topLinks.map((topLink, index) => {
-              return (<Link key={'link_' + index} href={topLink.path} style={{marginLeft: '20px'}}>{topLink.title}</Link>)
-            })}
-            {language.alternativeLanguages.map((altLanguage, index) => {
-              return (<Link key={'link_' + index} href={altLanguage.path} style={{marginLeft: '20px'}}>{altLanguage.title}</Link>)
-            })}
+        <nav className="global-links">
+          {this.topLinks()}
+          {this.languageLinks()}
         </nav>
-        <div className="top-row flex-row justify-space-between flex-wrap" style={{width: '100%'}}>
-          <a className="plain" href="/"><img src={logoUrl} alt="" style={{width: '248px', height: '50px'}} /></a>
-          <div className="searchfield" style={{width: '285px', alignSelf: 'flex-end'}}>
+        <div className="misc top-row flex-row justify-space-between flex-wrap">
+          <a className="plainLink" href="/">
+            <img src={logoUrl} alt="" className="logo" />
+          </a>
+
+          <button className="hamburger" onClick={this.toggleMainMenu}>
+            {this.menuButtonStatus()}
+          </button>
+
+          <div className={this.state.showMainMenuOnMobile ? 'show searchfield' : 'searchfield'}>
             <Input
               ariaLabel={searchInputPlaceholder}
               searchField
-              submitCallback={(value) => this.goToSearchResultPage(value)}
+              submitCallback={this.goToSearchResultPage}
               placeholder={searchInputPlaceholder} />
           </div>
         </div>
-        <div className="header-content">
+        <Divider className="mobileMenuDivider" />
+        <div className={this.state.showMainMenuOnMobile ? 'showOnMobile header-content' : 'header-content'}>
           <nav id="mainMenu" className="ssb-tabs">
-            {mainNavigation.map((topMenuItem, menuIndex) => (
-              <div key={menuIndex} className="tabItem">
-                <button className={topMenuItem.isActive? 'active navigation-item' : 'navigation-item'} onClick={() => this.toggleSubMenu()}>
-                  <span>{topMenuItem.title}</span>
-                </button>
-                <Divider/>
-                <ol className={this.state.showSubMenu ? 'visible subMenu' : 'subMenu' }>
-                  {topMenuItem.menuItems && topMenuItem.menuItems.map((menuItem, itemIndex) => {
-                    return (
-                      <li key={itemIndex}>
-                        <Link tabindex="-1" href={menuItem.path} icon={<img src={menuItem.icon}></img>}>{menuItem.title}</Link>
-                      </li>)
-                  })
-                  }
-                </ol>
-              </div>
-            ))}
+            {mainNavigation.map((topMenuItem, index) => {
+              const menuItemClick = this.toggleSubMenu.bind(this, index)
+              return (
+                <div key={index} className={this.state.mainMenu[index] ? 'mobileActive tabItem' : 'tabItem'}>
+                  <button onClick={menuItemClick}>
+                    <span className={topMenuItem.isActive? 'active navigation-item':'navigation-item'} >
+                      <span>{topMenuItem.title}</span>
+                      {this.state.mainMenu[index]? <ChevronUp/>: <ChevronDown/>}
+                    </span>
+                  </button>
+                  <Divider/>
+                  <ol className={this.state.showSubMenu ? 'visible subMenu' : 'subMenu' }>
+                    {this.renderSubMenu(topMenuItem)}
+                  </ol>
+                </div>
+              )
+            })}
+          </nav>
+          <nav className={this.state.showMainMenuOnMobile ? 'active global-bottom-links':'global-bottom-links'}>
+            {this.topLinks()}
+            {this.languageLinks()}
           </nav>
         </div>
       </header>
@@ -89,7 +146,7 @@ Header.propTypes = {
         PropTypes.shape({
           title: PropTypes.string,
           path: PropTypes.string,
-          icon: PropTypes.string,
+          icon: PropTypes.string
         })
       )
     })
