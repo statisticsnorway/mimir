f6fb46e9a312089c7cbe98a4d57072828a5f3c30
Øyvind Dahl <oyvind.dahl@item.no>
2020-06-23 08:58:22 +0200

    MIMIR-693: Add dataSource mixin to replace dataquery and dataset contenttype - Add mixin - Add mixin include statement in highcharts and keyfigures contenttypes - Changed names according to jiratasks
    MIMIR-693

--- src/main/resources/site/content-types/highchart/highchart.ts
+++ src/main/resources/site/content-types/highchart/highchart.ts
@@ -158,4 +158,104 @@ export interface Highchart {
    * Spørring mot Statistikkbanken
    */
   dataquery?: string;
+
+  /**
+   * Datakilde
+   */
+  dataSource?: {
+    /**
+     * Selected
+     */
+    _selected: string;
+
+    /**
+     * Api-spørring mot statbanken
+     */
+    statbankApi?: {
+      /**
+       * URL eller tabell-id
+       */
+      urlOrId?: string;
+
+      /**
+       * Format
+       */
+      format: "json" | "xml";
+
+      /**
+       * Navn på x-akse dimensjon
+       */
+      xAxisLabel?: string;
+
+      /**
+       * Navn på y-akse dimensjon
+       */
+      yAxisLabel?: string;
+
+      /**
+       * Filtrering på dataset
+       */
+      datasetFilterOptions?: {
+        /**
+         * Selected
+         */
+        _selected?: string;
+
+        /**
+         * Filtrer på kommune
+         */
+        municipalityFilter?: {
+          /**
+           * Hvilken dimensjon skal filtreres på kommunenummer
+           */
+          municipalityDimension: string;
+        };
+      };
+    };
+
+    /**
+     * Tall fra tallbygger
+     */
+    tallbygger?: {
+      /**
+       * URL eller TBML-id
+       */
+      urlOrId?: string;
+    };
+
+    /**
+     * Lagrede spørringer mot statistikkbanken
+     */
+    statbankSaved?: {
+      /**
+       * URL
+       */
+      url?: string;
+    };
+
+    /**
+     * Ferdige dataset
+     */
+    dataset?: {
+      /**
+       * ID
+       */
+      id?: string;
+
+      /**
+       * Format
+       */
+      format: "json" | "csv";
+    };
+
+    /**
+     * Klass
+     */
+    klass?: {
+      /**
+       * URL
+       */
+      url?: string;
+    };
+  };
 }
