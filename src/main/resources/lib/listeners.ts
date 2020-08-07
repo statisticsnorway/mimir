2af3ee8a26b828d9166592eae6ad5f3cdaee03d1
Øyvind Dahl <oyvind.dahl@item.no>
2020-08-07 15:27:35 +0200

    Add support for tbprocessor option
    MIMIR-697

--- src/main/resources/lib/listeners.ts
+++ src/main/resources/lib/listeners.ts
@@ -25,29 +25,20 @@ const {
 
 
 export function setupFetchDataOnCreateListener() {
-  log.info('Setting up listeners')
   listener({
     type: 'node.updated',
     callback: function(event: EnonicEvent) {
       const contentWithDataSource: QueryResponse<HighchartConfig | KeyFigure> = query({
-        query: `_id = '${event.data.nodes[0].id}' AND data.dataSource._selected = '${DataSource.STATBANK_API}'`,
+        query: `_id = '${event.data.nodes[0].id}' AND 
+          ( data.dataSource._selected = '${DataSource.STATBANK_API}' OR data.dataSource._selected = '${DataSource.TBPROCESSOR}' )`,
         contentTypes: [`${app.name}:highchart`, `${app.name}:keyFigure`],
         filters: {
           exists: {
-            field: `data.dataSource.${DataSource.STATBANK_API}.urlOrId`
+            field: `data.dataSource.*.urlOrId`
           }
         }
       })
-      log.info('WHAT DID YOU FIND? what? ...WHAT?')
-      log.info('%s', JSON.stringify(contentWithDataSource, null, 2))
-      contentWithDataSource.hits.forEach((content: Content<HighchartConfig | KeyFigure>) => {
-        log.info('this content have this in repo:::\n\n')
-        const repoData: DatasetRepoNode<JSONstat | TbmlData> | null = getDataset(content)
-        log.info('%s', JSON.stringify(repoData, null, 2))
-        log.info('refresh data')
-        const refreshresult: CreateOrUpdateStatus = refreshDataset(content)
-        log.info('%s', JSON.stringify(refreshresult, null, 2))
-      })
+      contentWithDataSource.hits.forEach((content: Content<HighchartConfig | KeyFigure>) =>  refreshDataset(content))
     }
   })
 }
