a585b49c061a88345064f700d39fb58619f9e268
Garlov <oe.nordli@gmail.com>
2020-08-13 15:08:59 +0200

    keep alive for socket, and icon to show connection
    MIMIR-689

--- src/main/resources/react4xp/_entries/Dashboard/Convert.jsx
+++ src/main/resources/react4xp/_entries/Dashboard/Convert.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Badge from 'react-bootstrap/Badge'
 import ProgressBar from 'react-bootstrap/ProgressBar'
 import Button from 'react-bootstrap/Button'
 import Axios from 'axios'
+import { Zap, ZapOff } from 'react-feather'
 
 class Convert extends React.Component {
   constructor(props) {
@@ -16,7 +18,8 @@ class Convert extends React.Component {
       errorMessage: ``,
       jobs: [],
       wsConnection: new ExpWS(),
-      io: null
+      io: null,
+      isConnected: false
     }
   }
 
@@ -43,7 +46,15 @@ class Convert extends React.Component {
   }
 
   onConnectionClose(event) {
-    console.log('Lost websocket connection to server', event)
+    this.setState({
+      isConnected: false
+    })
+  }
+
+  onConnectionOpen(event) {
+    this.setState({
+      isConnected: true
+    })
   }
 
   componentDidMount() {
@@ -56,11 +67,19 @@ class Convert extends React.Component {
         this.onConnectionClose(event)
       })
 
+      wsConnection.setEventHandler('open', (event) => {
+        this.onConnectionOpen(event)
+      })
+
       this.setState({
         jobs: res.data.jobs,
         loadingJobs: false,
         io: new wsConnection.Io()
       })
+
+      setInterval(() => {
+        this.state.io.emit('keep-alive', 'ping')
+      }, 1000 * 60 * 3)
 
       this.state.jobs.forEach((job) => {
         this.setupWSListener(job.key)
@@ -92,7 +111,7 @@ class Convert extends React.Component {
     } else {
       return this.state.jobs.map((job) => {
         return (
-          <Row key={job.key}>
+          <Row key={job.key} className="mb-4">
             <Col className="col-4 convert-label">{`${job.label} (${job.max}stk)`}</Col>
             <Col className="col-6">
               <ProgressBar min={job.min} max={job.max} now={job.current} label={`${job.current}/${job.max}`} animated/>
@@ -106,9 +125,26 @@ class Convert extends React.Component {
     }
   }
 
+  renderBadge() {
+    if (this.state.isConnected) {
+      return (<span>Connected<Zap></Zap></span>)
+    } else {
+      return (<span>Disconnected<ZapOff></ZapOff></span>)
+    }
+  }
+
   render() {
     return (
       <Container className="dashboard-convert">
+        <Row>
+          <Col>
+            <Badge variant={this.state.isConnected ? 'success' : 'danger'}>
+              {
+                this.renderBadge()
+              }
+            </Badge>
+          </Col>
+        </Row>
         {this.renderJobs()}
       </Container>
     )
