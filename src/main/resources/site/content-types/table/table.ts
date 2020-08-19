9a0d7e1654eba69353f484161f3fc6687cbcc59f
Carina Nordseth <cgn@ssb.no>
2020-08-19 15:20:02 +0200

    MIMIR-727: Removed dublicate contenttype table

--- src/main/resources/site/content-types/table/table.ts
+++ src/main/resources/site/content-types/table/table.ts
@@ -1,5 +1,10 @@
 export interface Table {
   /**
+   * Rettemelding
+   */
+  correctionNotice?: string;
+
+  /**
    * Datakilde
    */
   dataSource?: {
@@ -96,6 +101,51 @@ export interface Table {
        * URL
        */
       url?: string;
+    };
+  };
+
+  /**
+   * Fotnote-tekst
+   */
+  footNote?: string;
+
+  /**
+   * Kildelenke
+   */
+  checkOptionSet?: {
+    /**
+     * Selected
+     */
+    _selected: string;
+
+    /**
+     * Url lenke
+     */
+    urlSource?: {
+      /**
+       * Tekst til kildelenke
+       */
+      urlText?: string;
+
+      /**
+       * Kildelenke
+       */
+      url?: string;
+    };
+
+    /**
+     * Lenke til internt innhold
+     */
+    relatedSource?: {
+      /**
+       * Tekst til kildelenke
+       */
+      urlText?: string;
+
+      /**
+       * Relatert innhold
+       */
+      sourceSelector?: string;
     };
   };
 }
