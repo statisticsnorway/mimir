cb8f9514aeb8c1797247906196e2fa0cbe97d503
Garlov <oe.nordli@gmail.com>
2020-09-30 09:07:58 +0200

    move connection badge to component
    MIMIR-816

--- src/main/resources/react4xp/_entries/Dashboard/containers/HomePage/index.jsx
+++ src/main/resources/react4xp/_entries/Dashboard/containers/HomePage/index.jsx
@@ -1,23 +1,16 @@
 import React from 'react'
-import { Badge, Container, Tab, Tabs } from 'react-bootstrap'
-import { Zap, ZapOff } from 'react-feather'
+import { Container, Tab, Tabs } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
 import { StatRegDashboard } from '../StatRegDashboard'
 import { selectIsConnected } from './selectors'
+import { ConnectionBadge } from '../../components/ConnectionBadge'
 
 export function HomePage() {
   const isConnected = useSelector(selectIsConnected)
-  const renderBadge = () => {
-    if (isConnected) {
-      return (<Badge variant="success"><span>Connected<Zap></Zap></span></Badge>)
-    } else {
-      return (<Badge variant="danger"><span>Disconnected<ZapOff></ZapOff></span></Badge>)
-    }
-  }
 
   return (
     <Container>
-      {renderBadge()}
+      <ConnectionBadge isConnected={isConnected} />
       <Tabs defaultActiveKey="queries">
         <Tab eventKey="queries" title="Spørringer">
           <section className="xp-part part-dashboard container">
