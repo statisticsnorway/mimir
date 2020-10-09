0bfbfce1ec37bb671066b0a461ef427895924596
Carina Nordseth <cgn@ssb.no>
2020-10-09 12:46:21 +0200

    MIMIR-12:Added interface Statistic and removed som code

--- src/main/resources/lib/ssb/statistic.ts
+++ src/main/resources/lib/ssb/statistic.ts
@@ -1,16 +1,9 @@
 import { Socket, SocketEmitter } from '../types/socket'
 import { Content, ContentLibrary, QueryResponse } from 'enonic-types/lib/content'
-import { Statistic } from '../../site/mixins/statistic/statistic'
-import { StatisticInListing } from './statreg/types'
-import { find } from 'ramda'
 
 const {
   query
 }: ContentLibrary = __non_webpack_require__( '/lib/xp/content')
-
-const {
-  getAllStatisticsFromRepo
-} = __non_webpack_require__('/lib/repo/statreg/statistics')
 
 export function setupHandlers(socket: Socket, socketEmitter: SocketEmitter): void {
   socket.on('get-statistics', () => {
@@ -21,13 +14,12 @@ export function setupHandlers(socket: Socket, socketEmitter: SocketEmitter): voi
 
 function prepStatistics(statistics: Array<Content<Statistic>>): Array<StatisticDashboard> {
   const statisticData: Array<StatisticDashboard> = []
-  //const statregData: Array<StatisticInListing> | null = getAllStatisticsFromRepo()
 
   statistics.map((statistic: Content<Statistic>) => {
-    //const statregStatistic: StatisticInListing | undefined = statregData?.find((o) => o.id === statistic._id)
+    const name: string = statistic.language === 'en' ? 'Engelsk: ' + statistic._name : statistic._name
     const statisticDataDashboard: StatisticDashboard = {
       id: statistic._id,
-      name: statistic._name,
+      name: name,
       nextRelease: 'Dato'
     }
     statisticData.push(statisticDataDashboard)
@@ -51,4 +43,9 @@ interface StatisticDashboard {
   id: string;
   name: string;
   nextRelease?: string;
+}
+
+interface Statistic {
+  statistic?: string;
+  language: string;
 }
