4bd0e20ffed1f15cc55100c88e14931de0499cfd
Carina Nordseth <cgn@ssb.no>
2020-10-20 14:54:44 +0200

    MIMIR-95:Trying to replace Overlay with Modal
    This dont work yet

--- src/main/resources/react4xp/_entries/Dashboard/components/DataQuery.jsx
+++ src/main/resources/react4xp/_entries/Dashboard/components/DataQuery.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { DataQueryBadges } from './DataQueryBadges'
 import { Link } from '@statisticsnorway/ssb-component-library'
-import { Button, OverlayTrigger, Popover } from 'react-bootstrap'
+import { Button, Modal } from 'react-bootstrap'
 import { AlertTriangle, RefreshCw } from 'react-feather'
 
 export function DataQuery(props) {
@@ -25,32 +25,22 @@ export function DataQuery(props) {
     contentStudioBaseUrl
   } = props
 
+  const [show, setShow] = useState(false)
+  const [, setShowModal] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  const toggleTrueFalse = () => {
+    setShowModal(handleShow)
+  }
+
   function renderLogData() {
     if (logData) {
       return (
-        <td>
-          {eventLogNodes &&
-        <OverlayTrigger
-          trigger="click"
-          key={id}
-          placement="bottom"
-          onToggle={(opening) => {
-            if (opening) {
-              onOpenEventLogData()
-            }
-          }}
-          overlay={
-            <Popover id={`popover-positioned-${id}`}>
-              <Popover.Title as="h3">Logg detaljer</Popover.Title>
-              <Popover.Content className="ssbPopoverBody">
-                {renderJobLogs()}
-              </Popover.Content>
-            </Popover>
-          }
+        <td
+          onClick={() => openEventlogQuery()}
         >
-          <span className="haveList">{logData.message ? logData.message : ''}</span>
-        </OverlayTrigger>
-          }
+          {eventLogNodes}
           {logData.showWarningIcon && <span className="warningIcon"><AlertTriangle size="12" color="#FF4500"/></span>}<br/>
           {logData.modifiedReadable ? logData.modifiedReadable : ''}<br/>
           {logData.modified ? logData.modified : ''}<br/>
@@ -58,6 +48,29 @@ export function DataQuery(props) {
         </td>
       )
     } else return <td>no logs</td>
+  }
+
+  function openEventlogQuery() {
+    onOpenEventLogData()
+    toggleTrueFalse()
+  }
+
+
+  const ModalContent = () => {
+    return (
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Logg detaljer</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h2> {displayName}</h2>
+          {renderJobLogs()}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>Lukk</Button>
+        </Modal.Footer>
+      </Modal>
+    )
   }
 
   function renderJobLogs() {
@@ -98,6 +111,7 @@ export function DataQuery(props) {
           { loading ? <span className="spinner-border spinner-border-sm" /> : <RefreshCw size={16}/> }
         </Button>
       </td>
+      {show ? <ModalContent/> : null }
     </tr>
   )
 }
