3a3a9afd03edc8a510d5a029287ef491bf22ec1e
johnnadeluy <johnnadel.salita@gmail.com>
2020-10-29 10:34:03 +0100

    MIMIR-97: First draft; adds React Table to dataqueries containers package. Implements ReactTable to DataQueryTable, reusing code from DataQuery component.

---
+++ src/main/resources/react4xp/_entries/Dashboard/components/ReactTable.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useTable, useSortBy } from 'react-table'
+import { Table } from 'react-bootstrap'
+import { ChevronUp, ChevronDown } from 'react-feather'
+import PropTypes from 'prop-types'
+
+export function ReactTable(props) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns: props.columns,
+    data: props.data
+  },
+  useSortBy)
+
+  return (
+    <Table bordered striped {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup, index) => (
+          <tr key={index} {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column, index) => (
+              <th key={index} {...column.getHeaderProps(column.getSortByToggleProps())}>
+                {column.render('Header')}
+                <span>
+                  {column.disableSortBy ? '' :
+                    column.isSorted ?
+                      (column.isSortedDesc ?
+                        <ChevronDown size={17} /> :
+                        <ChevronUp size={17} />
+                      ) : <span><ChevronUp size={17} /><ChevronDown size={17} /></span>}
+                </span>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, index) => {
+          prepareRow(row)
+          return (
+            <tr key={index} {...row.getRowProps()}>
+              {row.cells.map((cell, i) => {
+                return (
+                  <td key={i} {...cell.getCellProps}>{cell.render('Cell')}</td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </Table>
+  )
+}
+
+ReactTable.propTypes = {
+  columns: PropTypes.array,
+  data: PropTypes.array,
+  subComponent: PropTypes.node
+}
+
+export default (props) => <ReactTable {...props} />
+
+
--- src/main/resources/react4xp/_entries/Dashboard/containers/DataQueries/DataQueryTable.jsx
+++ src/main/resources/react4xp/_entries/Dashboard/containers/DataQueries/DataQueryTable.jsx
@@ -1,14 +1,15 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { WebSocketContext } from '../../utils/websocket/WebsocketProvider'
-import { Button, Table } from 'react-bootstrap'
+import { Button, Modal } from 'react-bootstrap'
 import { selectDataQueriesByParentType } from './selectors'
-import { Accordion } from '@statisticsnorway/ssb-component-library'
+import { Accordion, Link } from '@statisticsnorway/ssb-component-library'
 import PropTypes from 'prop-types'
 import { requestDatasetUpdate, requestEventLogData } from './actions'
 import { DataQuery } from '../../components/DataQuery'
-import { RefreshCw } from 'react-feather'
+import { AlertTriangle, RefreshCw } from 'react-feather'
 import { selectContentStudioBaseUrl, selectDataToolBoxBaseUrl } from '../HomePage/selectors'
+import { ReactTable } from '../../components/ReactTable'
 
 export function DataQueryTable(props) {
   const dataQueries = useSelector(selectDataQueriesByParentType(props.dataQueryType))
@@ -23,48 +24,137 @@ export function DataQueryTable(props) {
   }
 
   const anyLoading = dataQueries.filter((q) => q.loading).length > 0
+
+  const columns = React.useMemo(() => [
+    {
+      Header: 'Spørring ',
+      accessor: 'dataquery'
+    },
+    {
+      Header: 'Sist oppdatert ',
+      accessor: 'lastUpdated',
+      sortType: 'datetime' // TODO: replace with function, retrieve only the date and sort by that
+    },
+    {
+      Header: 'Siste aktivitet ',
+      accessor: 'lastActivity',
+      sortType: 'datetime' // TODO: replace with function, for now only sort by date of last activity
+    },
+    {
+      Header: '',
+      accessor: 'refreshDataQuery',
+      disableSortBy: true
+    }
+  ], [])
+
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  const openEventlog = (dataQuery) => {
+    requestEventLogData(dispatch, io, dataQuery.id)
+    setShow(handleShow)
+  }
+
+  function renderLogData(dataQuery) {
+    if (dataQuery.logData) {
+      return (
+        <span className="text-center haveList" onClick={() => openEventlog(dataQuery)}>
+          <span>
+            {dataQuery.logData.message ? dataQuery.logData.message : ''}
+            {dataQuery.logData.showWarningIcon && <span className="warningIcon"><AlertTriangle size="12" color="#FF4500"/></span>}<br/>
+            {dataQuery.logData.modifiedReadable ? dataQuery.logData.modifiedReadable : ''}<br/>
+            {dataQuery.logData.modified ? dataQuery.logData.modified : ''}<br/>
+            {dataQuery.logData.by && dataQuery.logData.by.displayName ? `av ${dataQuery.logData.by.displayName}` : '' }
+          </span>
+        </span>
+      )
+    } else return <span>no logs</span>
+  }
+
+  const openToolBox = (dataQuery) => {
+    window.open(dataToolBoxBaseUrl + dataQuery.id)
+  }
+
+  function renderJobLogs(dataQuery) {
+    if (dataQuery.loadingLogs === true) {
+      return (
+        <span className="spinner-border spinner-border" />
+      )
+    } else {
+      return dataQuery.eventLogNodes.map((logNode, index) => {
+        return (
+          <p key={index}>
+            <span>{logNode.modifiedTs}</span> - <span>{logNode.by}</span><br/>
+            <span> &gt; {logNode.result}</span>
+          </p>
+        )
+      })
+    }
+  }
+
+  // TODO: Implement ModalContent
+  const ModalContent = (dataQuery) => {
+    return (
+      <Modal
+        show={show}
+        onHide={handleClose}
+        animation={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+              Logg detaljer
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h3>{dataQuery.displayName}</h3>
+          {renderJobLogs(dataQuery)}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="info" onClick={openToolBox(dataQuery)}>Datatoolbox</Button>
+          <Button variant="secondary" onClick={handleClose}>Lukk</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
+
+  function getDataQueries(dataQueries) {
+    return dataQueries.map((dataQuery) => {
+      return {
+        dataquery: (
+          <span className={`${dataQuery.hasData ? 'ok' : 'error'} dataset`}>
+            <Link isExternal href={contentStudioBaseUrl + dataQuery.id}>{dataQuery.displayName}</Link>
+          </span>
+        ),
+        lastUpdated: (
+          <span>
+            {dataQuery.dataset.modifiedReadable ? dataQuery.dataset.modifiedReadable : ''}
+            <br/>
+            {dataQuery.dataset.modified ? dataQuery.dataset.modified : ''}
+          </span>
+        ),
+        lastActivity: dataQuery.logData ? renderLogData(dataQuery) : <span></span>,
+        refreshDataQuery: (
+          <Button variant="primary"
+            size="sm"
+            className="mx-1"
+            onClick={() => requestDatasetUpdate(dispatch, io, [dataQuery.id])}
+          >
+            {dataQuery.loading ? <span className="spinner-border spinner-border-sm"/> : <RefreshCw size={16}/>}
+          </Button>
+        )
+      }
+    })
+  }
+  const data = React.useMemo(() => getDataQueries(dataQueries), [])
+
   return (
     <Accordion header={`${props.header} (${dataQueries.length})`} className="mx-0" openByDefault={!!props.openByDefault}>
       <Button className="mb-3" onClick={() => updateAll()}>
         Oppdater liste
         {anyLoading ? <span className="spinner-border spinner-border-sm ml-2 mb-1" /> : <RefreshCw className="ml-2" />}
       </Button>
-      <Table bordered striped>
-        <thead>
-          <tr>
-            <th className="roboto-bold sortable-column">
-              <div className="sortable-column-header">
-                <span>Spørring</span>
-              </div>
-            </th>
-            <th className="roboto-bold sortable-column">
-              <div className="sortable-column-header">
-                <span>Sist oppdatert</span>
-              </div>
-            </th>
-            <th className="roboto-bold sortable-column">
-              <div className="sortable-column-header">
-                <span>Siste aktivitet</span>
-              </div>
-            </th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {dataQueries.map((dataQuery) => {
-            return (
-              <DataQuery
-                key={dataQuery.id}
-                dataQuery={dataQuery}
-                onRefresh={() => requestDatasetUpdate(dispatch, io, [dataQuery.id])}
-                onOpenEventLogData={() => requestEventLogData(dispatch, io, dataQuery.id)}
-                contentStudioBaseUrl={contentStudioBaseUrl}
-                dataToolBoxBaseUrl={dataToolBoxBaseUrl}
-              />
-            )
-          })}
-        </tbody>
-      </Table>
+      <ReactTable columns={columns} data={data} />
     </Accordion>
   )
 }
