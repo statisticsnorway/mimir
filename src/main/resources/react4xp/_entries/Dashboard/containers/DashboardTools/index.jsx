23c96dcef73f5c0330e3bc6e40f383b5d1fd7a93
Garlov <oe.nordli@gmail.com>
2020-11-16 13:34:10 +0100

    statistics search in dashboard tools
    MIMIR-135

--- src/main/resources/react4xp/_entries/Dashboard/containers/DashboardTools/index.jsx
+++ src/main/resources/react4xp/_entries/Dashboard/containers/DashboardTools/index.jsx
@@ -6,9 +6,13 @@ import { WebSocketContext } from '../../utils/websocket/WebsocketProvider'
 import { requestClearCache } from '../HomePage/actions.es6'
 import { Trash } from 'react-feather'
 import { Col, Container, Row } from 'react-bootstrap'
+import { Dropdown } from '@statisticsnorway/ssb-component-library'
+import { selectStatistics, selectLoading } from '../Statistics/selectors'
 
 export function DataQueryTools() {
   const loadingCache = useSelector(selectLoadingClearCache)
+  const statistics = useSelector(selectStatistics)
+  const loadingStatistics = useSelector(selectLoading)
   const io = useContext(WebSocketContext)
   const dispatch = useDispatch()
 
@@ -23,11 +27,36 @@ export function DataQueryTools() {
     return (<Trash size={16}/>)
   }
 
+  function onStatisticsSearchSelect(e) {
+    console.log(e)
+  }
+
+  function renderStatisticsSearch() {
+    if (loadingStatistics) {
+      return (
+        <span className="spinner-border spinner-border-sm ml-2 mb-1" />
+      )
+    }
+    return (
+      <Dropdown
+        placeholder="Finn statistikk"
+        searchable
+        items={statistics.map((s) => {
+          return {
+            title: `${s.shortName} - ${s.name}`,
+            id: s.id
+          }
+        })}
+        onSelect={(e) => onStatisticsSearchSelect(e)}
+      />
+    )
+  }
+
   return (
     <div className="p-4 tables-wrapper">
       <h2>Diverse verktøy</h2>
       <Container>
-        <Row>
+        <Row className="mb-3">
           <Col>
             <span>Tøm Cache</span>
             <Button
@@ -37,6 +66,11 @@ export function DataQueryTools() {
               disabled={loadingCache}>
               {renderIcon(loadingCache)}
             </Button>
+          </Col>
+        </Row>
+        <Row className="mb-3">
+          <Col>
+            {renderStatisticsSearch()}
           </Col>
         </Row>
       </Container>
