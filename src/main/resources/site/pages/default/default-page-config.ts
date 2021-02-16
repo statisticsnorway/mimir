2362d1ce51e2b198dd67161ec1699abdb9d4304c
Øyvind Dahl <oyvind.dahl@item.no>
2021-02-16 16:22:42 +0100

    Add more rows to config
    MIMIR-348

--- src/main/resources/site/pages/default/default-page-config.ts
+++ src/main/resources/site/pages/default/default-page-config.ts
@@ -21,7 +21,7 @@ export interface DefaultPageConfig {
     /**
      * Region
      */
-    region: "Rad_A" | "Rad_B" | "Rad_C" | "Rad_D" | "Rad_E" | "Rad_F" | "Rad_G" | "Rad_H" | "Rad_I" | "Rad_J" | "Rad_K" | "Rad_L" | "Rad_M" | "Rad_N" | "Rad_O" | "Rad_P" | "Rad_Q" | "Rad_R" | "Rad_S" | "Rad_T";
+    region: "Rad_A" | "Rad_B" | "Rad_C" | "Rad_D" | "Rad_E" | "Rad_F" | "Rad_G" | "Rad_H" | "Rad_I" | "Rad_J" | "Rad_K" | "Rad_L" | "Rad_M" | "Rad_N" | "Rad_O" | "Rad_P" | "Rad_Q" | "Rad_R" | "Rad_S" | "Rad_T" | "Rad_U" | "Rad_V" | "Rad_W" | "Rad_X" | "Rad_Y" | "Rad_Z";
 
     /**
      * Med mørk og skrå bakgrunn
