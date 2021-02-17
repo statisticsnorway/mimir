f6e6faf4324b1ed287e5bc0138822fe25d9e1014
Garlov <oe.nordli@gmail.com>
2021-02-17 14:05:17 +0100

    mock tbml source list
    MIMIR-432

--- src/main/resources/lib/tbml/tbmlMock.ts
+++ src/main/resources/lib/tbml/tbmlMock.ts
@@ -8,6 +8,8 @@ export function getTbmlMock(url: string): HttpResponse | null {
   if (app.config && app.config['ssb.mock.enable'] === 'true') {
     if (url.includes('process/tbmldata/-1')) {
       return getTbmlMock1()
+    } else if (url.includes('document/sourceList/-1')) {
+      return getSourceListMock1()
     }
   }
   return null
@@ -20,7 +22,7 @@ function getTbmlMock1(): HttpResponse {
     body: `<?xml version="1.0" encoding="utf-8"?>
     <tbml>
         <metadata>
-            <instance definitionId="0" xml:lang="no" relatedTableIds="0" publicRelatedTableIds="0"/>
+            <instance definitionId="-1" xml:lang="no" relatedTableIds="-1" publicRelatedTableIds="-1"/>
             <title>Dagens Dato</title>
             <tablesource>Statistisk sentralbyrå</tablesource>
             <category>FACTSPAGEFIGURES</category>
@@ -41,6 +43,22 @@ function getTbmlMock1(): HttpResponse {
             </table>
         </presentation>
     </tbml>`,
+    contentType: 'text/xml; charset=utf-8',
+    headers: {},
+    bodyStream: {} as unknown as ByteSource
+  }
+}
+
+function getSourceListMock1(): HttpResponse {
+  return {
+    status: 200,
+    message: '',
+    body: `<?xml version="1.0" encoding="utf-8"?>
+    <sourceList>
+      <tbml id="-1">
+        <source id="ID2001882" owner="241" table="dagensdato" tableId="-1" tableApproved="internet"/>
+      </tbml>
+    </sourceList>`,
     contentType: 'text/xml; charset=utf-8',
     headers: {},
     bodyStream: {} as unknown as ByteSource
