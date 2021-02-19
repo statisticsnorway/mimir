efc0b184b439910d008fad4d083a90eb8571af7b
erik chrisander gustavsen <e.c.gustavsen@gmail.com>
2021-02-19 02:06:36 +0100

    Added part externalCard

---
+++ src/main/resources/react4xp/_entries/ExternalCards.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Link } from '@statisticsnorway/ssb-component-library'
+import PropTypes from 'prop-types'
+import { ExternalLink } from 'react-feather'
+
+const ExternalCards = (props) => {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row justify-content-start">
+          {props.links.map((link, index) => {
+            return (
+              <div className="external-card mb-4 mr-3" key={index}>
+                <img src={link.image} alt=" "/>
+                <p className="text-center"
+                  dangerouslySetInnerHTML={{
+                    __html: link.content
+                  }}
+                />
+                <div className="link-wrapper text-center">
+                  <Link
+                    href={link.href}
+                    icon={<ExternalLink size="18"/>}
+                    className=''
+                  >
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: link.children
+                      }}
+                    />
+                  </Link>
+                </div>
+              </div>
+            )
+          }
+          )}
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+ExternalCards.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      image: PropTypes.string,
+      imageAlt: PropTypes.string,
+      content: PropTypes.string,
+      href: PropTypes.string.isRequired,
+      children: PropTypes.string
+    })
+  )
+}
+
+export default ExternalCards
