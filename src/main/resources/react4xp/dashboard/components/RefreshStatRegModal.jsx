7a6c1a053dfc627fd162e2137c254846fc88b18c
Uy <jsu@ssb.no>
2021-03-08 16:22:42 +0100

    More detailed message for refresh statreg modal and code refactoring
    MIMIR-228

--- src/main/resources/react4xp/dashboard/components/RefreshStatRegModal.jsx
+++ src/main/resources/react4xp/dashboard/components/RefreshStatRegModal.jsx
@@ -1,21 +1,59 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useContext } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { PropTypes } from 'prop-types'
 import { Button, Modal } from 'react-bootstrap'
 import { selectStatRegStatus } from '../containers/StatRegDashboard/selectors'
+import { requestStatRegEventLogData } from '../containers/StatRegDashboard/actions'
+import { WebSocketContext } from '../utils/websocket/WebsocketProvider'
 
 export function RefreshStatRegModal(props) {
+  const io = useContext(WebSocketContext)
+  const dispatch = useDispatch()
+
   const statReg = useSelector(selectStatRegStatus(props.statRegKey))
+  const {
+    displayName,
+    eventLogNodes,
+    logData,
+    loading
+  } = statReg
+
+  function renderJobLogs() {
+    requestStatRegEventLogData(dispatch, io, props.statRegKey)
+
+    return eventLogNodes.map((logNode, index) => {
+      return (
+        <p key={index}>
+          <span>{logNode.modifiedTs}</span> - <span>{logNode.by}</span><br/>
+          <span> &gt; {logNode.result}</span>
+        </p>
+      )
+    })
+  }
+
+  let statRegName
+  if (displayName === 'statistikk') {
+    statRegName = 'statistikker'
+  } else {
+    statRegName = displayName
+  }
 
   function renderMessage(logData) {
-    if (!statReg.loading) {
+    if (!loading) {
       if (logData.showWarningIcon) {
         return (
-          <p>Noe gikk galt</p>
+          <div>
+            <span>
+              Noe gikk galt med oppdatering av {statRegName}. Her er jobbloggen:
+            </span>
+            <div className="mt-4">
+              {renderJobLogs()}
+            </div>
+          </div>
         )
       }
       return (
-        <p>Alt gikk OK</p>
+        <p>{statRegName.charAt(0).toUpperCase() + statRegName.slice(1)} ble oppdatert uten feil.</p>
       )
     }
     return <span className="spinner-border spinner-border-sm" />
@@ -24,10 +62,10 @@ export function RefreshStatRegModal(props) {
   return (
     <Modal size="lg" show={true} onHide={props.handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>Oppdatering av {statReg.displayName}</Modal.Title>
+        <Modal.Title>Oppdatering av {statRegName}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        {renderMessage(statReg.logData)}
+        {renderMessage(logData)}
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={props.handleClose}>
