f4468479e401942016f835d3b9eb2195b7e8d39c
Garlov <oe.nordli@gmail.com>
2021-03-09 13:35:08 +0100

    use Date and not moment to display ServerTime
    MIMIR-447

--- src/main/resources/react4xp/dashboard/components/ServerTime.jsx
+++ src/main/resources/react4xp/dashboard/components/ServerTime.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import moment from 'moment/min/moment-with-locales'
 
 export function ServerTime(props) {
   const {
@@ -19,9 +18,18 @@ export function ServerTime(props) {
     }
   }, [props.serverTime, props.serverTimeReceived])
 
+  function appendZero(num) {
+    if (num < 10) return `0${num}`
+    return `${num}`
+  }
+
   function renderServerTime() {
     if (currentServerTime) {
-      return <span>{moment(currentServerTime).locale('nb').format('HH.mm.ss')}</span>
+      const t = new Date(currentServerTime)
+      const hour = appendZero(t.getHours())
+      const minute = appendZero(t.getMinutes())
+      const second = appendZero(t.getSeconds())
+      return <span>{hour}:{minute}:{second}</span>
     }
     return <span className="spinner-border spinner-border-sm my-1" />
   }
