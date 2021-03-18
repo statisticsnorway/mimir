adccc55a309c73c89313c543884c121c84dc88cd
Carina Nordseth <cgn@ssb.no>
2021-03-18 15:20:13 +0100

    MIMIR-511:Started on contenttype varnish rules

---
+++ src/main/resources/site/content-types/rewriteVarnish/rewriteVarnish.ts
@@ -0,0 +1,16 @@
+export interface RewriteVarnish {
+  /**
+   * Lenker
+   */
+  requests?: Array<{
+    /**
+     * Lenke XP
+     */
+    requestUrl?: string;
+
+    /**
+     * Aktiver regel
+     */
+    enableRule: boolean;
+  }>;
+}
