7ff8fa40bb4ece3bc355f33b00a0f438412e0198
erik chrisander gustavsen <e.c.gustavsen@gmail.com>
2021-04-27 09:34:03 +0200

    added the title to phrases

--- src/main/resources/site/parts/articleList/articleList.ts
+++ src/main/resources/site/parts/articleList/articleList.ts
@@ -39,9 +39,13 @@ function renderPart(req: Request): React4xpResponse {
     key: 'publicationLinkText',
     locale: language === 'nb' ? 'no' : language
   })
+  const headerText: string = localize( {
+    key: 'articleList.heading',
+    locale: language === 'nb' ? 'no' : language
+  })
 
   const props: PartProperties = {
-    title: 'Nye artikler, analyser og publikasjoner',
+    title: headerText,
     articles: preparedArticles,
     archiveLinkText: archiveLinkText,
     archiveLinkUrl: component.config.pubArchiveUrl ? component.config.pubArchiveUrl : '#'
