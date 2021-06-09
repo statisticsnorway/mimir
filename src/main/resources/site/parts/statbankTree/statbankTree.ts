e2456967ddec367b257be5d023b43293173f97b8
Øyvind Dahl <oyvind.dahl@item.no>
2021-06-09 09:46:56 +0200

    Get statistics mainsubjects and subsubjects

--- src/main/resources/site/parts/statbankTree/statbankTree.ts
+++ src/main/resources/site/parts/statbankTree/statbankTree.ts
@@ -1,8 +1,28 @@
 import { Request, Response } from 'enonic-types/controller'
+import { SubjectItem } from '../../../lib/ssb/utils/subjectUtils'
+const {
+  getMainSubjects,
+  getSubSubjectsByPath
+} = __non_webpack_require__( '/lib/ssb/utils/subjectUtils')
 
 
 export function get(req: Request): Response {
+  const allMainSubjects: Array<SubjectItem> = getMainSubjects()
+
+  const mainSubbjectsWithSubSubjects: Array<BB> = allMainSubjects.map( (subjectItem) => {
+    const c: Array<SubjectItem> = getSubSubjectsByPath(allMainSubjects, subjectItem.path)
+    return {
+      ...subjectItem,
+      subSubjects: c
+    }
+  })
+  log.info(JSON.stringify(mainSubbjectsWithSubSubjects, null, 2))
   return {
     body: ''
   }
+}
+
+type BB = SubjectItem & SubSubs
+interface SubSubs {
+  subSubjects: Array<SubjectItem>;
 }
