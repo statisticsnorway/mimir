a4b205ef749a9bf52b0ca36edaeb2a5d300d4a6f
Garlov <oe.nordli@gmail.com>
2021-06-24 16:10:57 +0200

    upcoming releases content type
    MIMIR-630

---
+++ src/main/resources/site/content-types/upcomingRelease/upcomingRelease.ts
@@ -0,0 +1,16 @@
+export interface UpcomingRelease {
+  /**
+   * Publisering
+   */
+  date: string;
+
+  /**
+   * Innholdstype
+   */
+  type: string;
+
+  /**
+   * Hovedemne
+   */
+  mainSubject: string;
+}
