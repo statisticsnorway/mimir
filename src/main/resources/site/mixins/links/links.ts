b0616d0464bee5e9f7d4be92c8613077b6a19f5c
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2021-06-29 14:42:37 +0200

    Moves all link related parts configs into the new link part
    MIMIR-593

---
+++ src/main/resources/site/mixins/links/links.ts
@@ -0,0 +1,81 @@
+export interface Links {
+  /**
+   * Lenke type
+   */
+  linkTypes?: {
+    /**
+     * Selected
+     */
+    _selected: string;
+
+    /**
+     * Tabell lenke
+     */
+    tableLink?: {
+      /**
+       * Tittel
+       */
+      title: string;
+
+      /**
+       * Lenketekst
+       */
+      description: string;
+
+      /**
+       * Url
+       */
+      href?: string;
+
+      /**
+       * Relatert innhold
+       */
+      relatedContent?: string;
+    };
+
+    /**
+     * Uthevet lenke
+     */
+    headerLink?: {
+      /**
+       * Innhold
+       */
+      linkedContent: string;
+
+      /**
+       * Lenketekst
+       */
+      linkText: string;
+    };
+
+    /**
+     * Nedlastningslenke som knapp
+     */
+    downloadLink?: {
+      /**
+       * Tekst
+       */
+      text?: string;
+
+      /**
+       * Innhold
+       */
+      file?: string;
+    };
+
+    /**
+     * Profilert lenke
+     */
+    profiledLink?: {
+      /**
+       * Lenketekst
+       */
+      text?: string;
+
+      /**
+       * Innhold
+       */
+      href?: string;
+    };
+  };
+}
