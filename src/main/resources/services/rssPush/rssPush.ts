8c4535ce607db220d40f1aa17682819170466d36
Larsen, Glenruben Engen <Glenruben.Larsen@ssb.no>
2021-08-17 13:05:15 +0200

    Remove fluff

--- src/main/resources/services/rssPush/rssPush.ts
+++ src/main/resources/services/rssPush/rssPush.ts
@@ -1,13 +1,12 @@
-import { Request, Response } from 'enonic-types/controller'
+import { Response } from 'enonic-types/controller'
 
 const {
   pushRssNews
 } = __non_webpack_require__('/lib/ssb/cron/pushRss')
 
-exports.get = function(/* req: Request */): Response {
+exports.get = function(): Response {
   return {
     body: pushRssNews(),
-    // body: 'ITSA ME, DARIO',
     contentType: 'text/html',
     status: 200
   }
