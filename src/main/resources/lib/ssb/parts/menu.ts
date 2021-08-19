0a103700787df88d9b82f3a34c5bd829f4b0e896
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2021-08-19 13:36:21 +0200

    Minor code refactoring
    MIMIR-834

--- src/main/resources/lib/ssb/parts/menu.ts
+++ src/main/resources/lib/ssb/parts/menu.ts
@@ -13,39 +13,11 @@ const {
   getAttachment
 } = __non_webpack_require__('/lib/ssb/utils/utils')
 
-export function createMenuTree(menuItemId: string): Array<MenuItemParsed> {
-  const menuContent: Content<MenuItem> | null = get({
-    key: menuItemId
-  })
-
-  if (menuContent) {
-    const menuContentChildren: QueryResponse<MenuItem> = getChildren({
-      count: 100,
-      key: menuContent._id
-    })
-
-    const parsedMenu: Array<MenuItemParsed> = menuContentChildren.hits.map((menuItem) => createMenuBranch(menuItem))
-    const flatMenu: Array<MenuItemParsed> = flatten(parsedMenu)
-    query<Image>({
-      count: flatMenu.length,
-      query: `_id IN(${flatMenu.map((menuItem) => `"${menuItem.iconId}"`).join(',')}) AND type = "media:vector"`
-    }).hits.forEach((icon) => {
-      const menuItem: MenuItemParsed | undefined = flatMenu.find((fm) => fm.iconId === icon._id)
-      if (menuItem) {
-        menuItem.iconAltText = icon ? icon.data.caption : ' '
-        menuItem.iconSvgTag = icon ? getAttachment(icon) : undefined
-      }
-    })
-    return parsedMenu
-  }
-  return []
-}
-
-function flatten(menuItems: Array<MenuItemParsed>): Array<MenuItemParsed> {
+function flattenMenu(menuItems: Array<MenuItemParsed>): Array<MenuItemParsed> {
   return menuItems.reduce((acc: Array<MenuItemParsed>, menuItem) => {
     acc = acc.concat(menuItem)
     if (menuItem.menuItems) {
-      acc = acc.concat(flatten(menuItem.menuItems))
+      acc = acc.concat(flattenMenu(menuItem.menuItems))
     }
     return acc
   }, [])
@@ -67,6 +39,34 @@ function createMenuBranch(menuItem: Content<MenuItem>, depth: number = 0): MenuI
     iconId: menuItem.data.icon,
     menuItems: children.length > 0 ? children.map((childMenuItem) => createMenuBranch(childMenuItem, depth + 1)) : undefined
   }
+}
+
+export function createMenuTree(menuItemId: string): Array<MenuItemParsed> {
+  const menuContent: Content<MenuItem> | null = get({
+    key: menuItemId
+  })
+
+  if (menuContent) {
+    const menuContentChildren: QueryResponse<MenuItem> = getChildren({
+      count: 100,
+      key: menuContent._id
+    })
+
+    const parsedMenu: Array<MenuItemParsed> = menuContentChildren.hits.map((menuItem) => createMenuBranch(menuItem))
+    const flatMenu: Array<MenuItemParsed> = flattenMenu(parsedMenu)
+    query<Image>({
+      count: flatMenu.length,
+      query: `_id IN(${flatMenu.map((menuItem) => `"${menuItem.iconId}"`).join(',')}) AND type = "media:vector"`
+    }).hits.forEach((icon) => {
+      const menuItem: MenuItemParsed | undefined = flatMenu.find((fm) => fm.iconId === icon._id)
+      if (menuItem) {
+        menuItem.iconAltText = icon ? icon.data.caption : ' '
+        menuItem.iconSvgTag = icon ? getAttachment(icon) : undefined
+      }
+    })
+    return parsedMenu
+  }
+  return []
 }
 
 export function isMenuItemActive(children: QueryResponse<MenuItem>, content: Content | null): boolean {
