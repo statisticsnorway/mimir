5e0709bf3263d2f63b099071c2a8c69fab0e199d
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2021-08-19 15:10:43 +0200

    Auto generated typescript for part and content type configs

--- src/main/resources/site/macros/links/links-config.ts
+++ src/main/resources/site/macros/links/links-config.ts
@@ -4,4 +4,49 @@ export interface LinksConfig {
    * Velg lenketype
    */
   linkTypes: "tableLink" | "headerLink" | "profiledLink";
+
+  /**
+   * Tittel
+   */
+  title?: string;
+
+  /**
+   * Lenketekst
+   */
+  description?: string;
+
+  /**
+   * Url
+   */
+  url?: string;
+
+  /**
+   * Relatert innhold
+   */
+  relatedContent?: string;
+
+  /**
+   * Innhold
+   */
+  linkedContent?: string;
+
+  /**
+   * Lenketekst
+   */
+  linkText?: string;
+
+  /**
+   * Innhold
+   */
+  contentUrl?: string;
+
+  /**
+   * Lenketekst
+   */
+  text?: string;
+
+  /**
+   * Med ikon
+   */
+  withIcon: boolean;
 }
