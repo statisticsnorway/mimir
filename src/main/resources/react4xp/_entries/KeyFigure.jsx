9e39c3e38433ffd5bd34aa33f5043926c79eb42a
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2021-09-02 14:35:13 +0200

    Converts keyfigure part from es6 to typescript
    MIMIR-889

--- src/main/resources/react4xp/_entries/KeyFigure.jsx
+++ src/main/resources/react4xp/_entries/KeyFigure.jsx
@@ -85,10 +85,10 @@ class KeyFigures extends React.Component {
 
   addKeyFigureSource(keyFigure) {
     if ((!this.props.source || !this.props.source.url) && keyFigure.source && keyFigure.source.url) {
-      const source = this.props.phrases.source
+      const sourceLabel = this.props.sourceLabel
 
       return (
-        <References className={`${keyFigure.size !== 'large' ? 'mt-3' : ''}`} title={source} referenceList={[{
+        <References className={`${keyFigure.size !== 'large' ? 'mt-3' : ''}`} title={sourceLabel} referenceList={[{
           href: keyFigure.source.url,
           label: keyFigure.source.title
         }]}/>
@@ -99,10 +99,10 @@ class KeyFigures extends React.Component {
 
   addSource() {
     if (this.props.source && this.props.source.url) {
-      const source = this.props.phrases.source
+      const sourceLabel = this.props.sourceLabel
 
       return (
-        <References className="col-12 mt-3" title={source} referenceList={[{
+        <References className="col-12 mt-3" title={sourceLabel} referenceList={[{
           href: this.props.source.url,
           label: this.props.source.title
         }]}/>
@@ -121,15 +121,22 @@ class KeyFigures extends React.Component {
   }
 
   render() {
-    return <div className="container">
-      {this.addPreviewButton()}
-      {this.addPreviewInfo()}
-      {this.addHeader()}
-      <div className="row">
-        {this.createRows()}
-        {this.addSource()}
-      </div>
-    </div>
+    return (
+      <section className="xp-part key-figures">
+        <div className="d-none searchabletext">
+          <span>{this.props.hiddenTitle}</span>
+        </div>
+        <div className="container">
+          {this.addPreviewButton()}
+          {this.addPreviewInfo()}
+          {this.addHeader()}
+          <div className="row">
+            {this.createRows()}
+            {this.addSource()}
+          </div>
+        </div>
+      </section>
+    )
   }
 }
 
@@ -152,7 +159,7 @@ KeyFigures.propTypes = {
       }),
       glossary: PropTypes.string,
       greenBox: PropTypes.bool,
-      phrases: PropTypes.object,
+      sourceLabel: PropTypes.string,
       source: PropTypes.shape({
         url: PropTypes.string,
         title: PropTypes.title
@@ -176,14 +183,14 @@ KeyFigures.propTypes = {
       }),
       glossary: PropTypes.string,
       greenBox: PropTypes.bool,
-      phrases: PropTypes.object,
+      sourceLabel: PropTypes.string,
       source: PropTypes.shape({
         url: PropTypes.string,
         title: PropTypes.title
       })
     })
   ),
-  phrases: PropTypes.object,
+  sourceLabel: PropTypes.string,
   source: PropTypes.shape({
     url: PropTypes.string,
     title: PropTypes.title
@@ -192,7 +199,8 @@ KeyFigures.propTypes = {
   showPreviewDraft: PropTypes.bool,
   paramShowDraft: PropTypes.bool,
   draftExist: PropTypes.bool,
-  pageTypeKeyFigure: PropTypes.bool
+  pageTypeKeyFigure: PropTypes.bool,
+  hiddenTitle: PropTypes.string
 }
 
 export default (props) => <KeyFigures {...props}/>
