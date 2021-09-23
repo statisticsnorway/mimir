b0a1e60187f46bf5509ce41e577140e865273da3
Carina Nordseth <cgn@ssb.no>
2021-09-23 12:45:15 +0200

    MIMIR-529:Changed label from Subtittel to Stikktittel

--- src/main/resources/site/parts/banner/banner-part-config.ts
+++ src/main/resources/site/parts/banner/banner-part-config.ts
@@ -31,7 +31,7 @@ export interface BannerPartConfig {
          */
         faktaside: {
           /**
-           * Subtittel
+           * Stikktittel
            */
           subTitle?: string;
 
