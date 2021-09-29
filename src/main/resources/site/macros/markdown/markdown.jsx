6f4baa8b254b3c0915188d39da952b4867022866
Larsen, Glenruben Engen <gle@ssb.no>
2021-09-29 15:11:30 +0200

    it's a mess
    Co-authored-by: Johnnadel Salita Uy <johnnadeluy@users.noreply.github.com>

--- src/main/resources/site/macros/markdown/markdown.jsx
+++ src/main/resources/site/macros/markdown/markdown.jsx
@@ -1,27 +1,49 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import { ArrowRight } from 'react-feather'
+import { Link } from '@statisticsnorway/ssb-component-library'
 
-class MarkdownTest extends React.Component {
-  constructor(props) {
-    super(props)
+
+// class MarkdownTest extends React.Component {
+//   constructor(props) {
+//     super(props)
+//   }
+//   render() {
+function MarkdownTest(props) {
+  const options = {
+    title: {
+      text: 'My chart'
+    },
+    series: [{
+      data: [1, 2, 3]
+    }]
   }
 
-  render() {
-    return (
-      <section className="test">
+  console.log(JSON.stringify(options, null, 2))
+  // console.log(JSON.stringify(Highcharts, null, 2))
+
+  return (
+    <section className="test">
         Vi kan sette inn feks en Highcharts her!
-        {this.props.content.displayName}
-        {this.props.content.markdown}
-      </section>
-    )
-  }
+      <div>
+        <Link href={'/that/content'} isExternal={true} className="ssb-link header">
+          {'En flott tekst med lenke som type'}
+        </Link>
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={options}
+        />
+      </div>
+      {props.content}
+      <ArrowRight size={12}></ArrowRight>
+    </section>
+  )
 }
 
 export default (props) => <MarkdownTest {...props} />
 
 MarkdownTest.propTypes = {
-  content: PropTypes.shape({
-    displayName: PropTypes.string,
-    markdown: PropTypes.string
-  })
+  content: PropTypes.string
 }
--- src/main/resources/site/macros/markdown/markdown.ts
+++ src/main/resources/site/macros/markdown/markdown.ts
@@ -15,26 +15,34 @@ exports.macro = (context: MacroContext): React4xpResponse => {
 exports.preview = (context: MacroContext): React4xpResponse => renderPart(context)
 
 function renderPart(context: MacroContext): React4xpResponse {
-  const {
-    jsconfig
-  } = context.params
+  // const {
+  //   jsconfig
+  // } = context.params
 
-  const repo: RepoConnection = connect({
-    repoId: 'no.ssb.pubmd',
-    branch: 'master',
-    principals: ['role:system.admin']
-  })
-  const content: RepoNode = repo.get(jsconfig)
+  // const repo: RepoConnection = connect({
+  //   repoId: 'no.ssb.pubmd',
+  //   branch: 'master',
+  //   principals: ['role:system.admin']
+  // })
+  // const content: ContentProperties = repo.get(jsconfig)
 
-  log.info(JSON.stringify(content, null, 2))
-
-  const props: PartProperties = {
-    content
+  const props: PropProps = {
+    content: 'teststring her'
   }
+
+  // log.info(JSON.stringify(content, null, 2))
 
   return React4xp.render('site/macros/markdown/markdown', props)
 }
 
-interface PartProperties {
-    content: RepoNode;
+// interface PartProperties {
+//     content: RepoNode;
+// }
+
+// interface ContentProperties extends PartProperties {
+//   markdown: string;
+// }
+
+interface PropProps {
+  content: string;
 }
