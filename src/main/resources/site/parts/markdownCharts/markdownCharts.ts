6b87278f8690516877aef607d2416c0d3c61fb47
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2021-09-30 10:44:40 +0200

    Fix option function for markdown charts

--- src/main/resources/site/parts/markdownCharts/markdownCharts.ts
+++ src/main/resources/site/parts/markdownCharts/markdownCharts.ts
@@ -11,7 +11,7 @@ exports.get = (req: Request): React4xpResponse => {
 exports.preview = (req: Request, content: MarkdownRepoNode): React4xpResponse => renderPart(req, content)
 
 function renderPart(req: Request, content?: MarkdownRepoNode): React4xpResponse {
-  const options: object = content && content.markdown && content.markdown.chart ? JSON.parse(content.markdown.chart) : {}
+  const options: object = content && content.markdown ? JSON.parse(content.markdown) : {}
 
   const props: PartProperties = {
     options
@@ -21,9 +21,7 @@ function renderPart(req: Request, content?: MarkdownRepoNode): React4xpResponse 
 }
 
 interface MarkdownRepoNode extends RepoNode {
-  markdown?: {
-    chart?: string;
-  };
+  markdown?: string;
 }
 
 interface PartProperties {
