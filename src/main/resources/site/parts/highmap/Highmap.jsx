b364e6031049534473e00680cfd66d50fed8d8d6
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2021-10-21 15:48:50 +0200

    Add a few options from props
    MIMIR-929

--- src/main/resources/site/parts/highmap/Highmap.jsx
+++ src/main/resources/site/parts/highmap/Highmap.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+// import Highcharts from 'highcharts'
 import Highmaps from 'highcharts/highmaps'
-import { HighchartsMapChart, HighmapsProvider, MapNavigation, MapSeries, Title, Tooltip, Credits } from 'react-jsx-highmaps'
+import { HighchartsMapChart,
+  HighmapsProvider,
+  MapNavigation,
+  MapSeries,
+  Title,
+  Subtitle,
+  Tooltip,
+  Credits,
+  ColorAxis,
+  Legend,
+  Accessibility } from 'react-jsx-highmaps'
 import { Fetch } from 'react-request'
 import PropTypes from 'prop-types'
+
+// require('highcharts/modules/accessibility')(Highcharts)
+// require('highcharts/modules/exporting')(Highcharts)
+// require('highcharts/modules/export-data')(Highcharts)
 
 function Highmap(props) {
   return (
     <section className="part-highmap">
       <HighmapsProvider Highcharts={Highmaps}>
-        <Fetch url={props.mapfile}>
+        <Fetch url={props.mapFile}>
           {({
             fetching, failed, data
           }) => {
@@ -16,18 +31,32 @@ function Highmap(props) {
             if (failed) return <div>Failed to load map.</div>
 
             if (data) {
+              const series = props.tableData
+              //   const series = [
+              //     ['Hordaland', 26.1],
+              //     ['Troms', 38.1],
+              //     ['Vestfold', 50.7],
+              //     ['Oslo', 25.8],
+              //     ['Hedmark', 25.9]
+              //   ]
+              //   const mappedSeries = series.map((element) => {
+              //     const foundProp = data.find((it) => it.name == element[0])
+              //     return [foundProp, element[1]]
+              //   })
+              //   console.log(mappedSeries)
               return (
                 <HighchartsMapChart map={data}>
-                  <Title>Nordic countries</Title>
+                  {props.title && <Title align="left" style={props.hideTitle ? {
+                    color: 'transparent'
+                  } : {
+                    fontSize: '18px'
+                  }}>{props.title}</Title>}
+                  {props.subtitle && <Subtitle align="left">{props.subtitle}</Subtitle>}
+                  {/* {props.description && <Accessibility description={props.description} />} */}
 
                   <MapSeries
-                    data={[
-                      ['is', 1],
-                      ['no', 1],
-                      ['se', 1],
-                      ['dk', 1],
-                      ['fi', 1]
-                    ]}
+                    data={series}
+                    // data={props.tableData}
                     dataLabels={{
                       enabled: true,
                       color: '#FFFFFF',
@@ -40,9 +69,12 @@ function Highmap(props) {
                     <MapNavigation.ZoomOut/>
                   </MapNavigation>
 
-                  <Tooltip/>
+                  {/* TODO: Accomodate props.legendAlign */}
+                  {props.legendTitle && <Legend title={props.legendTitle && props.legendTitle} align={'center'} verticalAlign={'bottom'}/>}
 
-                  <Credits/>
+                  {/* {props.thresholdValues && <ColorAxis dataClasses={props.thresholdValues} dataClassColor="category" />}
+                  {props.numberDecimals && <Tooltip valueDecimals={props.numberDecimals}/>} */}
+                  <Credits enabled={false}/>
                 </HighchartsMapChart>
               )
             }
@@ -56,7 +88,20 @@ function Highmap(props) {
 }
 
 Highmap.propTypes = {
-  mapfile: PropTypes.string
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  description: PropTypes.string,
+  mapFile: PropTypes.string,
+  tableData: PropTypes.array,
+  thresholdValues: PropTypes.string,
+  hideTitle: PropTypes.boolean,
+  colorPalette: PropTypes.string,
+  numberDecimals: PropTypes.string,
+  heightAspectRatio: PropTypes.string,
+  seriesTitle: PropTypes.string,
+  legendTitle: PropTypes.string,
+  legendAlign: PropTypes.string,
+  footnoteText: PropTypes.array
 }
 
 export default (props) => <Highmap {...props} />
--- src/main/resources/site/parts/highmap/highmap.es6
+++ src/main/resources/site/parts/highmap/highmap.es6
@@ -1,5 +1,7 @@
 const {
-  data
+  data: {
+    forceArray
+  }
 } = __non_webpack_require__('/lib/util')
 const {
   get
@@ -7,15 +9,14 @@ const {
 const {
   attachmentUrl,
   getComponent,
-  getContent,
-  processHtml
+  getContent
 } = __non_webpack_require__('/lib/xp/portal')
 const {
   renderError
 } = __non_webpack_require__('/lib/ssb/error/error')
 
 const i18nLib = __non_webpack_require__('/lib/xp/i18n')
-
+const xmlParser = __.newBean('no.ssb.xp.xmlparser.XmlParser')
 const React4xp = __non_webpack_require__('/lib/enonic/react4xp')
 
 exports.get = function(req) {
@@ -43,22 +44,33 @@ const renderPart = (req) => {
     id: highmapContent.data.mapFile
   }), null, 2))
 
+  const tableData = []
+  if (highmapContent.data.htmlTable) {
+    const stringJson = highmapContent.data.htmlTable ? __.toNativeObject(xmlParser.parse(highmapContent.data.htmlTable)) : undefined
+    const result = stringJson ? JSON.parse(stringJson) : undefined
+    log.info('result %s', JSON.stringify(result, null))
+
+    tableData.push(result)
+  }
+
+  // TODO: Add if check for highmapContent
   const props = {
-    subtitle: '',
-    description: '',
-    mapfile: highmapContent && highmapContent.data.mapFile ? attachmentUrl({
+    title: highmapContent.displayName,
+    subtitle: highmapContent.data.subtitle,
+    description: highmapContent.data.description,
+    mapFile: highmapContent.data.mapFile ? attachmentUrl({
       id: highmapContent.data.mapFile
     }) : undefined,
-    htmlTable: '',
-    thresholdValues: '',
-    hideTitle: false,
-    colorPalette: '',
-    numberDecimals: '',
-    heightAspectRatio: '',
-    seriesTitle: '',
-    legendTitle: '',
-    legendAlign: '',
-    footnoteText: ''
+    tableData,
+    thresholdValues: highmapContent.data.thresholdValues,
+    hideTitle: highmapContent.data.hideTitle,
+    colorPalette: highmapContent.data.colorPalette,
+    numberDecimals: highmapContent.data.numberDecimals,
+    heightAspectRatio: highmapContent.data.heightAspectRatio,
+    seriesTitle: highmapContent.data.seriesTitle,
+    legendTitle: highmapContent.data.legendTitle,
+    legendAlign: highmapContent.data.legendAlign,
+    footnoteText: highmapContent.data.footnoteText ? forceArray(highmapContent.data.footnoteText) : []
   }
 
   return React4xp.render('site/parts/highmap/Highmap', props, req)
