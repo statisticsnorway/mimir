fab65ea76ebd84c8517edba97e197ad8ead0967e
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2021-10-27 08:15:53 +0200

    Disable export options with no map bug
    MIMIR-929

--- src/main/resources/site/parts/highmap/Highmap.jsx
+++ src/main/resources/site/parts/highmap/Highmap.jsx
@@ -114,17 +114,19 @@ function Highmap(props) {
     credits: {
       enabled: false
     },
-    exporting: { // WIP: exported graph does not display map
+    exporting: {
       buttons: {
         contextButton: {
+          // Map export in formats png, jpeg, pdf and svg does not work with the newest versions of highcharts and highcharts-react
+          // Enable options again once the bug has been patched
           menuItems: [
             'printChart',
             'separator',
-            'downloadPNG',
-            'downloadJPEG',
-            'downloadPDF',
-            'downloadSVG',
-            'separator',
+            // 'downloadPNG',
+            // 'downloadJPEG',
+            // 'downloadPDF',
+            // 'downloadSVG',
+            // 'separator',
             'downloadCSV',
             'downloadXLS'
           ]
