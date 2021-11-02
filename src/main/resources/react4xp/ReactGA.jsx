a766d1bb59354cc42648791e158580cb307cc6e1
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2021-11-02 15:35:10 +0100

    Minor code refactoring; remove debug
    MIMIR-939

--- src/main/resources/react4xp/ReactGA.jsx
+++ src/main/resources/react4xp/ReactGA.jsx
@@ -2,7 +2,6 @@ import ReactGA from 'react-ga'
 
 export const addGtagForEvent = (GA_TRACKING_ID, action, category, label) => {
   ReactGA.initialize(GA_TRACKING_ID, {
-    debug: true, // TODO: Remove after testing in QA/PROD
     titleCase: false
   })
   ReactGA.event({
