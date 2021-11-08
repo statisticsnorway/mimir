49c0a4e781d2adb36abb37a784115eb7ef232378
Larsen, Glenruben Engen <gle@ssb.no>
2021-11-08 10:54:31 +0100

    Adds props, names services

--- src/main/resources/admin/tools/bestbet/bestbet.ts
+++ src/main/resources/admin/tools/bestbet/bestbet.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'enonic-types/controller'
 import { React4xp, React4xpResponse } from '../../../lib/types/react4xp'
 
-
+const {
+  assetUrl, serviceUrl
+} = __non_webpack_require__('/lib/xp/portal')
 const {
   renderError
 } = __non_webpack_require__('/lib/ssb/error/error')
@@ -26,11 +28,20 @@ exports.preview = (req: Request): React4xpResponse | Response => {
 
 interface BestbetProps {
     value: string;
+    list: string;
+    model: string;
+
 }
 
 function renderPart(req: Request): React4xpResponse | Response {
   const props: BestbetProps = {
     value: 'test'
+    list: serviceUrl({
+      service: 'bestBetList'
+    })
+    model: serviceUrl({
+      service: 'bestBetModel'
+    })
   }
 
   return React4xp.render('bestbet/Bestbet', props, req)
