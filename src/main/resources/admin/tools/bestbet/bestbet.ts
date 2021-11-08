a1269e063da6963fb5dcb64af4acb4a05bc81204
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2021-11-08 12:01:04 +0100

    Add thymeleaf view to bestbet for css
    MIMIR-950

--- src/main/resources/admin/tools/bestbet/bestbet.ts
+++ src/main/resources/admin/tools/bestbet/bestbet.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'enonic-types/controller'
-import { React4xp, React4xpResponse } from '../../../lib/types/react4xp'
+import { ResourceKey } from 'enonic-types/thymeleaf'
+import { React4xp, React4xpObject, React4xpResponse } from '../../../lib/types/react4xp'
 
 const {
   assetUrl, serviceUrl
@@ -7,8 +8,11 @@ const {
 const {
   renderError
 } = __non_webpack_require__('/lib/ssb/error/error')
+const {
+  render
+} = __non_webpack_require__('/lib/thymeleaf')
+const view: ResourceKey = resolve('./bestbet.html')
 const React4xp: React4xp = __non_webpack_require__('/lib/enonic/react4xp')
-
 
 exports.get = function(req: Request): React4xpResponse | Response {
   try {
@@ -26,23 +30,39 @@ exports.preview = (req: Request): React4xpResponse | Response => {
   }
 }
 
-interface BestbetProps {
-    value: string;
-    list: string;
-    model: string;
+function renderPart(req: Request): React4xpResponse | Response {
+  const bestbetComponent: React4xpObject = new React4xp('bestbet/Bestbet')
+    .setProps({
+      value: 'test',
+      list: serviceUrl({
+        service: 'bestBetList'
+      }),
+      model: serviceUrl({
+        service: 'bestBetModel'
+      })
+    })
+    .setId('app-bestbet')
 
+  return {
+    body: bestbetComponent.renderBody({
+      body: render(view, {
+        ...getAssets()
+      })
+    }),
+    pageContributions: bestbetComponent.renderPageContributions()
+  }
 }
 
-function renderPart(req: Request): React4xpResponse | Response {
-  const props: BestbetProps = {
-    value: 'test'
-    list: serviceUrl({
-      service: 'bestBetList'
-    })
-    model: serviceUrl({
-      service: 'bestBetModel'
+function getAssets(): object {
+  return {
+    jsLibsUrl: assetUrl({
+      path: 'js/bundle.js'
+    }),
+    stylesUrl: assetUrl({
+      path: 'styles/bundle.css'
+    }),
+    logoUrl: assetUrl({
+      path: 'SSB_logo_black.svg'
     })
   }
-
-  return React4xp.render('bestbet/Bestbet', props, req)
 }
--- src/main/resources/react4xp/_entries/bestbet/Bestbet.jsx
+++ src/main/resources/react4xp/_entries/bestbet/Bestbet.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Container, Row, Col } from 'react-bootstrap'
+import { Title } from '@statisticsnorway/ssb-component-library'
 
 function Bestbet(props) {
   return (
-    <div>
-      <h1>
-        HELLO WORLD!!
-      </h1>
-      {props.value}
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Title size={1}>Best-bet app</Title>
+          {props.value}
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
