7b503a21780bd432649775564491a4a8fce458be
johnnadeluy <johnnadel.salita@gmail.com>
2021-11-16 13:07:15 +0100

    Adjust best bet list ux for better workflow Fix flickering edit search words modal
    MIMIR-950

--- src/main/resources/react4xp/_entries/bestbet/Bestbet.jsx
+++ src/main/resources/react4xp/_entries/bestbet/Bestbet.jsx
@@ -4,9 +4,12 @@ import { Container, Row, Col, Modal } from 'react-bootstrap'
 import { Title, Link, Tag, Dropdown, Input, Button } from '@statisticsnorway/ssb-component-library'
 import { XCircle, Edit } from 'react-feather'
 import { post } from 'axios'
+import EditSearchWordsModal from './EditSearchWordsModal'
 
 function Bestbet(props) {
-  const [selectedSearchWord, setSelectedSearchWord] = useState('')
+  const [bestBetList, setBestBetList] = useState(props.bestBetList) // need?
+  const [bestbetItem, setBestBetItem] = useState([])
+
   const [showDeleteSearchWordModal, setShowDeleteSearchWordModal] = useState(false)
   const handleCloseDeleteSearchWordModal = () => setShowDeleteSearchWordModal(false)
 
@@ -14,16 +17,16 @@ function Bestbet(props) {
   const handleCloseEditSearchWordModal = () => setShowEditSearchWordsModal(false)
 
   const [inputTag, setInputTag] = useState('')
-  const [displaySearchWordsForm, setDisplaySearchWordsForm] = useState([])
+  const [searchWordsList, setSearchWordsList] = useState([])
+  const [selectedSearchWord, setSelectedSearchWord] = useState('')
+  const [displaySearchWordsForm, setDisplaySearchWordsForm] = useState(false)
 
-  const [bestBetList, setBestBetList] = useState(props.bestBetList)
-  const [bestbetItem, setBestBetItem] = useState([])
-
-  function handleSubmit(event) {
+  function handleSubmit() {
+    if (showEditSearchWordsModal) setShowEditSearchWordsModal(false)
     const updatedBestBetItem = bestbetItem.length ? bestbetItem.map((item) => {
       return {
         ...item,
-        searchWords: displaySearchWordsForm
+        searchWords: searchWordsList
       }
     }) : []
 
@@ -48,8 +51,8 @@ function Bestbet(props) {
 
   function handleDeleteSearchWord(selectedSearchWord) {
     setShowDeleteSearchWordModal(false)
-    const updateDisplaySearchWordForm = displaySearchWordsForm.filter((searchWord) => searchWord !== selectedSearchWord)
-    setDisplaySearchWordsForm(updateDisplaySearchWordForm)
+    const updateDisplaySearchWordForm = searchWordsList.filter((searchWord) => searchWord !== selectedSearchWord)
+    setSearchWordsList(updateDisplaySearchWordForm)
   }
 
   const DeleteSearchWordModal = () => {
@@ -72,46 +75,47 @@ function Bestbet(props) {
     )
   }
 
-  function handleEditSearchWordOnClick(item) {
-    setShowEditSearchWordsModal(true)
-    setDisplaySearchWordsForm(item.searchWords)
-  }
-
-  const EditSearchWordsModal = () => {
+  function renderEditSearchWordModal() {
     return (
-      <Modal
+      <EditSearchWordsModal
         show={showEditSearchWordsModal}
         onHide={handleCloseEditSearchWordModal}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Rediger nøkkelord</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {displaySearchWordsForm.length ?
+        body={
+          <>
+            {searchWordsList.length ?
+              <Row>
+                <Col className="d-flex flex-wrap mt-3">
+                  {searchWordsList.map((searchWord) => renderSearchWord(searchWord, true))}
+                </Col>
+              </Row> : null}
             <Row>
-              <Col className="d-flex flex-wrap mt-3">
-                {displaySearchWordsForm.map((searchWord) => renderSearchWord(searchWord))}
+              <Col>
+                <Input
+                  handleChange={handleTagInput}
+                  value={inputTag}
+                  className="mt-3"
+                />
               </Col>
-            </Row> : null}
-          <Row>
-            <Col>
-              <Input
-                handleChange={handleTagInput}
-                value={inputTag}
-                className="mt-3"
-              />
-            </Col>
-            <Col>
-              <Button primary onClick={handleTagSubmit} className="mt-3">Legg til</Button>
-            </Col>
-          </Row>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button primary>Rediger</Button>
-          <Button onClick={handleCloseEditSearchWordModal}>Lukk</Button>
-        </Modal.Footer>
-      </Modal>
+              <Col>
+                <Button primary onClick={handleTagSubmit} className="mt-3">Legg til</Button>
+              </Col>
+            </Row>
+          </>
+        }
+        footer={
+          <>
+            <Button primary onClick={handleSubmit}>Rediger</Button>
+            <Button onClick={handleCloseEditSearchWordModal}>Lukk</Button>
+          </>
+        }
+      />
     )
+  }
+
+  function handleEditSearchWordOnClick(item) {
+    setShowEditSearchWordsModal(true)
+    setBestBetItem([item])
+    setSearchWordsList(item.searchWords)
   }
 
   function handleDropdownOnSelect(item) {
@@ -121,7 +125,8 @@ function Bestbet(props) {
 
     if (getBestBetItem.length) {
       setBestBetItem(getBestBetItem)
-      setDisplaySearchWordsForm(getBestBetItem[0].searchWords)
+      setSearchWordsList(getBestBetItem[0].searchWords)
+      setDisplaySearchWordsForm(true)
     }
   }
 
@@ -130,8 +135,7 @@ function Bestbet(props) {
   }
 
   function handleTagSubmit() {
-    setDisplaySearchWordsForm([...displaySearchWordsForm, inputTag])
-    setInputTag('')
+    setSearchWordsList([...searchWordsList, inputTag])
   }
 
   function renderForm() {
@@ -155,10 +159,10 @@ function Bestbet(props) {
           onSelect={handleDropdownOnSelect}
           searchable
         />
-        {displaySearchWordsForm.length ?
+        {displaySearchWordsForm && searchWordsList.length ?
           <Row>
             <Col className="d-flex flex-wrap mt-3">
-              {displaySearchWordsForm.map((searchWord) => renderSearchWord(searchWord))}
+              {searchWordsList.map((searchWord) => renderSearchWord(searchWord))}
             </Col>
           </Row> : null}
         <Row>
@@ -183,6 +187,7 @@ function Bestbet(props) {
   }
 
   function renderBestbetList() {
+    const sortedBestBetList = bestBetList.body.hits.sort((a, b) => a.linkedContentId - b.linkedContentId)
     return (
       <Row className="justify-content-between">
         <Col className="col-8 bestbet-list">
@@ -194,18 +199,27 @@ function Bestbet(props) {
               <Title size={2}>Nøkkelord</Title>
             </Col>
           </Row>
-          {bestBetList.body.hits.map((bet) => renderListItem(bet))}
+          {sortedBestBetList.map((bet) => renderListItem(bet))}
         </Col>
         {renderForm()}
       </Row>
     )
   }
 
-  function renderSearchWord(searchWord) {
+  function renderSearchWord(searchWord, isInEditModal, disabled) {
+    if (!disabled) {
+      const searchWordOnClickCallback = isInEditModal ? () => handleDeleteSearchWord(searchWord) : () => handleSearchWordOnClick(searchWord)
+      return (
+        <Tag className="m-1" onClick={searchWordOnClickCallback}>
+          {searchWord}<XCircle size={16} className="ml-1" />
+        </Tag>
+      )
+    }
+
     return (
-      <Tag className="m-1" onClick={() => handleSearchWordOnClick(searchWord)}>
-        {searchWord}<XCircle size={16} className="ml-1" />
-      </Tag>
+      <div className="search-words m-1">
+        {searchWord}
+      </div>
     )
   }
 
@@ -217,7 +231,7 @@ function Bestbet(props) {
         </Col>
         <Col>
           <div className="d-flex flex-wrap">
-            {item.searchWords.map((searchWord) => renderSearchWord(searchWord))}
+            {item.searchWords.map((searchWord) => renderSearchWord(searchWord, false, true))}
             <Tag className="m-1" onClick={() => handleEditSearchWordOnClick(item)}>
               Rediger <Edit size={16} className="ml-1" />
             </Tag>
@@ -230,7 +244,7 @@ function Bestbet(props) {
   return (
     <Container fluid>
       <DeleteSearchWordModal />
-      <EditSearchWordsModal />
+      {renderEditSearchWordModal()}
       <Row className="bestbet-header">
         <Col className="flex-row align-items-center">
           <img src={props.logoUrl} className="logo" />
