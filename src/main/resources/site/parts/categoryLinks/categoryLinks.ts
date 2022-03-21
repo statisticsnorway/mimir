f387c25b63d1c9108b54add91fbfa1d7fd2eda44
Larsen, Glenruben Engen <gle@ssb.no>
2022-03-21 16:21:19 +0100

    Add types to categoryLinks.ts, working.

--- src/main/resources/site/parts/categoryLinks/categoryLinks.ts
+++ src/main/resources/site/parts/categoryLinks/categoryLinks.ts
@@ -1,3 +1,10 @@
+import {Request, Response} from "enonic-types/controller";
+import {React4xpObject, React4xpResponse} from "../../../lib/types/react4xp";
+import {Component} from "enonic-types/portal";
+import {CategoryLinksPartConfig} from "./categoryLinks-part-config";
+import {Content} from "enonic-types/content";
+import {Language, Phrases} from "../../../lib/types/language";
+
 const {
   data
 } = __non_webpack_require__('/lib/util')
@@ -19,7 +26,7 @@ const {
 
 const view = resolve('./categoryLinks.html')
 
-exports.get = function(req) {
+exports.get = function(req: Request) {
   try {
     return renderPart(req)
   } catch (e) {
@@ -27,27 +34,29 @@ exports.get = function(req) {
   }
 }
 
-exports.preview = (req) => renderPart(req)
+exports.preview = (req: Request) => renderPart(req)
 
 const NO_LINKS_FOUND = {
   body: '',
   contentType: 'text/html'
 }
 
-const renderPart = (req) => {
-  const part = getComponent()
-  const page = getContent()
-  const language = getLanguage(page)
-  const phrases = language.phrases
-  const links = part.config.CategoryLinkItemSet ? data.forceArray(part.config.CategoryLinkItemSet) : []
-  const methodsAndDocumentation = part.config.methodsDocumentation
+function renderPart(req: Request): Response | React4xpResponse {
+  const part: Component<CategoryLinksPartConfig> = getComponent()
+  const page: Content = getContent()
+  const language: Language = getLanguage(page)
+  const phrases: Phrases = language.phrases as Phrases
+  const links: Array<CategoryLink> = part.config.CategoryLinkItemSet ? data.forceArray(part.config.CategoryLinkItemSet) : []
+  const methodsAndDocumentation: DocumentationContent | DocumentationUrl | undefined = part.config.methodsDocumentation
   let methodsAndDocumentationUrl
   if (methodsAndDocumentation) {
     if (methodsAndDocumentation._selected == 'urlSource') {
       methodsAndDocumentationUrl = methodsAndDocumentation.urlSource.url
     }
 
-    if (methodsAndDocumentation._selected == 'relatedSource') {
+    if (methodsAndDocumentation
+        && methodsAndDocumentation._selected == 'relatedSource'
+        && methodsAndDocumentation.relatedSource.content) {
       methodsAndDocumentationUrl = pageUrl({
         id: methodsAndDocumentation.relatedSource.content
       })
@@ -55,7 +64,7 @@ const renderPart = (req) => {
   }
 
   if (links && links.length) {
-    const categoryLinksComponent = new React4xp('CategoryLinks')
+    const categoryLinksComponent: React4xpObject = new React4xp('CategoryLinks')
       .setProps({
         links: links.map((link) => {
           return {
@@ -86,4 +95,26 @@ const renderPart = (req) => {
   return NO_LINKS_FOUND
 }
 
+interface CategoryLink {
+  titleText: string;
+  subText: string;
+  href: string;
+}
 
+interface MethodDocumentation {
+  _selected: string
+}
+
+interface DocumentationUrl extends MethodDocumentation {
+  _selected: 'urlSource'
+  urlSource: {
+    url: string;
+  };
+}
+
+interface DocumentationContent extends MethodDocumentation {
+  _selected: 'relatedSource'
+  relatedSource: {
+    content?: string;
+  };
+}
