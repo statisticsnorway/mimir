366b739d8f9c17154f709357ac84a419e452983e
johnnadeluy <johnnadel.salita@gmail.com>
2022-03-22 10:13:22 +0100

    Create calcultor utils file for duplicate code; move functions and import in respective parts
    MIMIR-646

--- src/main/resources/site/parts/husleieCalculator/husleieCalculator.ts
+++ src/main/resources/site/parts/husleieCalculator/husleieCalculator.ts
@@ -1,8 +1,9 @@
 import { Content } from 'enonic-types/content'
 import { Request, Response } from 'enonic-types/controller'
+import { allMonths, lastPeriodKpi, monthLabel, nextPeriod } from '../../../lib/ssb/utils/calculatorUtils'
 import { CalculatorPeriod } from '../../../lib/types/calculator'
-import { DropdownItem as MonthDropdownItem, DropdownItems as MonthDropdownItems } from '../../../lib/types/components'
-import { Data, Dataset, Dimension } from '../../../lib/types/jsonstat-toolkit'
+import { DropdownItems as MonthDropdownItems } from '../../../lib/types/components'
+import { Dataset } from '../../../lib/types/jsonstat-toolkit'
 import { Language, Phrases } from '../../../lib/types/language'
 import { React4xp, React4xpObject, React4xpResponse } from '../../../lib/types/react4xp'
 import { CalculatorConfig } from '../../content-types/calculatorConfig/calculatorConfig'
@@ -56,8 +57,8 @@ function getHusleiekalkulator(req: Request, page: Content): React4xpResponse {
   const phrases: Phrases = language.phrases as Phrases
   const calculatorConfig: Content<CalculatorConfig> | undefined = getCalculatorConfig()
   const kpiDataMonth: Dataset | null = getKpiDatasetMonth(calculatorConfig)
-  const months: MonthDropdownItems = allMonths(phrases)
-  const lastUpdated: CalculatorPeriod = lastPeriod(kpiDataMonth)
+  const months: MonthDropdownItems = allMonths(phrases, false, 'husleie')
+  const lastUpdated: CalculatorPeriod = lastPeriodKpi(kpiDataMonth)
   const nextUpdate: CalculatorPeriod = nextPeriod(lastUpdated.month as string, lastUpdated.year as string)
   const nextReleaseMonth: number = (nextUpdate.month as number) === 12 ? 1 : (nextUpdate.month as number) + 1
   const nextPublishText: string = i18nLib.localize({
@@ -105,106 +106,4 @@ function getHusleiekalkulator(req: Request, page: Content): React4xpResponse {
       clientRender: req.mode !== 'edit'
     })
   }
-}
-
-function lastPeriod(kpiDataMonth: Dataset | null): CalculatorPeriod {
-  const kpiDataYearDimension: Dimension | null = kpiDataMonth?.Dimension('Tid') as Dimension
-  const dataYear: string = kpiDataYearDimension?.id as string
-  const kpiDataMonthDimension: Dimension | null = kpiDataMonth?.Dimension('Maaned') as Dimension
-  const dataMonth: Array<string> = kpiDataMonthDimension?.id as Array<string>
-  const lastYear: string = dataYear[dataYear.length - 1]
-  const dataLastYearMnd: Array<string> = []
-
-  dataMonth.forEach(function(month) {
-    const kpiMonthData: Data | null = kpiDataMonth?.Data({
-      'Tid': lastYear,
-      'Maaned': month
-    }) as Data
-    const verdi: Data['value'] = kpiMonthData?.value
-    if (verdi != null) {
-      dataLastYearMnd.push(month)
-    }
-  })
-  const lastMonth: string = dataLastYearMnd[dataLastYearMnd.length - 1]
-
-  return {
-    month: lastMonth,
-    year: lastYear
-  }
-}
-
-function nextPeriod(month: string, year: string): CalculatorPeriod {
-  let nextPeriodMonth: number = parseInt(month) + 1
-  let nextPeriodYear: number = parseInt(year)
-
-  if (Number(month) === 12) {
-    nextPeriodMonth = 1
-    nextPeriodYear = nextPeriodYear + 1
-  }
-
-  return {
-    month: nextPeriodMonth,
-    year: nextPeriodYear
-  }
-}
-
-function allMonths(phrases: Phrases): MonthDropdownItems {
-  return [
-    {
-      id: '01',
-      title: phrases.january
-    },
-    {
-      id: '02',
-      title: phrases.february
-    },
-    {
-      id: '03',
-      title: phrases.march
-    },
-    {
-      id: '04',
-      title: phrases.april
-    },
-    {
-      id: '05',
-      title: phrases.may
-    },
-    {
-      id: '06',
-      title: phrases.june
-    },
-    {
-      id: '07',
-      title: phrases.july
-    },
-    {
-      id: '08',
-      title: phrases.august
-    },
-    {
-      id: '09',
-      title: phrases.september
-    },
-    {
-      id: '10',
-      title: phrases.october
-    },
-    {
-      id: '11',
-      title: phrases.november
-    },
-    {
-      id: '12',
-      title: phrases.december
-    }
-  ]
-}
-
-function monthLabel(months: MonthDropdownItems, language: string | undefined, month: number | string): string {
-  const monthLabel: MonthDropdownItem | undefined = months.find((m) => parseInt(m.id) === parseInt(month as string))
-  if (monthLabel) {
-    return language === 'en' ? monthLabel.title : monthLabel.title.toLowerCase()
-  }
-  return ''
 }
--- src/main/resources/site/parts/kpiCalculator/kpiCalculator.ts
+++ src/main/resources/site/parts/kpiCalculator/kpiCalculator.ts
@@ -1,13 +1,14 @@
 import { Content } from 'enonic-types/content'
 import { PageContributions, Request, Response } from 'enonic-types/controller'
 import { ResourceKey } from 'enonic-types/thymeleaf'
+import { allMonths, lastPeriodKpi, monthLabel, nextPeriod } from '../../../lib/ssb/utils/calculatorUtils'
 import { CalculatorPeriod } from '../../../lib/types/calculator'
-import { DropdownItem as MonthDropdownItem, DropdownItems as MonthDropdownItems } from '../../../lib/types/components'
-import { Data, Dataset, Dimension } from '../../../lib/types/jsonstat-toolkit'
+import { Dataset } from '../../../lib/types/jsonstat-toolkit'
 import { Language, Phrases } from '../../../lib/types/language'
 import { React4xp, React4xpObject, React4xpResponse } from '../../../lib/types/react4xp'
 import { CalculatorConfig } from '../../content-types/calculatorConfig/calculatorConfig'
 import { KpiCalculatorPartConfig } from './kpiCalculator-part-config'
+import { DropdownItems as MonthDropdownItems } from '../../../lib/types/components'
 
 const {
   getComponent,
@@ -76,7 +77,7 @@ function getKpiCalculatorComponent(page: Content): React4xpResponse {
   const calculatorConfig: Content<CalculatorConfig> | undefined = getCalculatorConfig()
   const kpiDataMonth: Dataset | null = getKpiDatasetMonth(calculatorConfig)
   const months: MonthDropdownItems = allMonths(phrases, frontPage)
-  const lastUpdated: CalculatorPeriod = lastPeriod(kpiDataMonth)
+  const lastUpdated: CalculatorPeriod = lastPeriodKpi(kpiDataMonth)
   const nextUpdate: CalculatorPeriod = nextPeriod(lastUpdated.month as string, lastUpdated.year as string)
   const nextReleaseMonth: number = (nextUpdate.month as number) === 12 ? 1 : (nextUpdate.month as number) + 1
   const nextPublishText: string = i18nLib.localize({
@@ -126,110 +127,4 @@ function getKpiCalculatorComponent(page: Content): React4xpResponse {
     }),
     pageContributions: kpiCalculatorComponent.renderPageContributions({})
   }
-}
-
-function lastPeriod(kpiDataMonth: Dataset | null): CalculatorPeriod {
-  const kpiDataYearDimension: Dimension | null = kpiDataMonth?.Dimension('Tid') as Dimension
-  const dataYear: string | undefined = kpiDataYearDimension?.id as string
-  const kpiDataMonthDimension: Dimension | null = kpiDataMonth?.Dimension('Maaned') as Dimension
-  const dataMonth: Array<string> | undefined = kpiDataMonthDimension?.id as Array<string>
-  const lastYear: string | undefined = dataYear?.[dataYear.length - 1]
-  const dataLastYearMnd: Array<string> = []
-
-  dataMonth.forEach(function(month) {
-    const kpiMonthData: Data | null | undefined = kpiDataMonth?.Data({
-      'Tid': lastYear,
-      'Maaned': month
-    })
-    const verdi: Data['value'] | undefined = kpiMonthData?.value
-    if (verdi != null) {
-      dataLastYearMnd.push(month)
-    }
-  })
-  const lastMonth: string = dataLastYearMnd[dataLastYearMnd.length - 1]
-
-  return {
-    month: lastMonth,
-    year: lastYear
-  }
-}
-
-function nextPeriod(month: string, year: string): CalculatorPeriod {
-  let nextPeriodMonth: number = parseInt(month) + 1
-  let nextPeriodYear: number = parseInt(year)
-
-  if (Number(month) === 12) {
-    nextPeriodMonth = 1
-    nextPeriodYear = nextPeriodYear + 1
-  }
-
-  return {
-    month: nextPeriodMonth,
-    year: nextPeriodYear
-  }
-}
-
-function allMonths(phrases: Phrases, frontPage: boolean): MonthDropdownItems {
-  return [
-    {
-      id: '90',
-      title: frontPage ? phrases.calculatorMonthAverageFrontpage : phrases.calculatorMonthAverage
-    },
-    {
-      id: '01',
-      title: phrases.january
-    },
-    {
-      id: '02',
-      title: phrases.february
-    },
-    {
-      id: '03',
-      title: phrases.march
-    },
-    {
-      id: '04',
-      title: phrases.april
-    },
-    {
-      id: '05',
-      title: phrases.may
-    },
-    {
-      id: '06',
-      title: phrases.june
-    },
-    {
-      id: '07',
-      title: phrases.july
-    },
-    {
-      id: '08',
-      title: phrases.august
-    },
-    {
-      id: '09',
-      title: phrases.september
-    },
-    {
-      id: '10',
-      title: phrases.october
-    },
-    {
-      id: '11',
-      title: phrases.november
-    },
-    {
-      id: '12',
-      title: phrases.december
-    }
-  ]
-}
-
-function monthLabel(months: MonthDropdownItems, language: string | undefined, month: number | string): string {
-  const monthLabel: MonthDropdownItem | undefined = months.find((m) => parseInt(m.id) === parseInt(month as string))
-  if (monthLabel) {
-    return language === 'en' ? monthLabel.title : monthLabel.title.toLowerCase()
-  }
-  return ''
 }
