b58bcb50438a210910f3c369d5c5da1e47c7bfd9
Carina Nordseth <cgn@ssb.no>
2022-03-23 15:29:10 +0100

    MIMIR-1141:Making calendar on frontpage a list

--- src/main/resources/react4xp/_entries/ReleasedStatistics.jsx
+++ src/main/resources/react4xp/_entries/ReleasedStatistics.jsx
@@ -19,31 +19,34 @@ class ReleasedStatistics extends React.Component {
 
   renderDay(day, month, year, index) {
     return (
-      <article className={index === 0 && 'first'} key={index}>
+      <li className={`calendar-day ${index === 0 && 'first'}`} key={index} aria-labelledby={`heading-released-statistics datemonth-${index}`}>
+        <span id={`datemonth-${index}`} className="sr-only">{`Publisering ${day.day} ${month.monthName}`}</span>
         <time dateTime={`${year}-${month.month}`}>
-          <span className='day'>{day.day}</span>
-          <span className='month'>{month.monthName}</span>
+          <span className='day' aria-hidden="true">{day.day}</span>
+          <span className='month' aria-hidden="true">{month.monthName}</span>
         </time>
         <ol className='releaseList'>
           {
             day.releases.map((release, releaseIndex) => this.renderRelease(release, releaseIndex))
           }
         </ol>
-      </article>
+      </li>
     )
   }
 
   render() {
     return (
       <section className='nextStatisticsReleases'>
-        <Title size={2} className="mb-4">{this.props.title}</Title>
-        {
-          this.props.releases.reverse().map((year) => {
-            return year.releases.reverse().map((month) => {
-              return month.releases.reverse().map((day, index) => this.renderDay(day, month, year, index))
+        <Title size={2} className="mb-4" id="heading-released-statistics">{this.props.title}</Title>
+        <ol>
+          {
+            this.props.releases.reverse().map((year) => {
+              return year.releases.reverse().map((month) => {
+                return month.releases.reverse().map((day, index) => this.renderDay(day, month, year, index))
+              })
             })
-          })
-        }
+          }
+        </ol>
       </section>
     )
   }
