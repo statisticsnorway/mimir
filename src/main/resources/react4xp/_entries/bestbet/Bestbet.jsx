4b074f39dd1b1f82440e86add005af6f4382a0cd
johnnadeluy <johnnadel.salita@gmail.com>
2022-03-24 09:10:50 +0100

    Add custom styling to async react select component
    MIMIR-1151

--- src/main/resources/react4xp/_entries/bestbet/Bestbet.jsx
+++ src/main/resources/react4xp/_entries/bestbet/Bestbet.jsx
@@ -150,18 +150,6 @@ function Bestbet(props) {
     }
   }
 
-  function clearInputFields() {
-    setSelectedContentResult(initialState.selectedContentResult)
-    setTitleInputValue(initialState.titleInputValue)
-    setUrlInputValue(initialState.urlInputValue)
-    setIngressInputValue(initialState.ingressInputValue)
-    setStartDateValue(initialState.startDateValue)
-    setContentTypeValue(initialState.contentTypeValue)
-    setMainSubjectValue(initialState.mainSubjectValue)
-    setSearchWordTag(initialState.searchWordTag)
-    setSearchWordsList(initialState.searchWordsList)
-  }
-
   function handleCreateBestBetOnClick() {
     setShowCreateBestBetModal(true)
     // clear edit best bet input data
@@ -219,6 +207,18 @@ function Bestbet(props) {
     }
   }
 
+  function clearInputFields() {
+    setSelectedContentResult(initialState.selectedContentResult)
+    setTitleInputValue(initialState.titleInputValue)
+    setUrlInputValue(initialState.urlInputValue)
+    setIngressInputValue(initialState.ingressInputValue)
+    setStartDateValue(initialState.startDateValue)
+    setContentTypeValue(initialState.contentTypeValue)
+    setMainSubjectValue(initialState.mainSubjectValue)
+    setSearchWordTag(initialState.searchWordTag)
+    setSearchWordsList(initialState.searchWordsList)
+  }
+
   async function searchForTerm(inputValue = '') {
     const result = await axios.get(props.contentSearchServiceUrl, {
       params: {
@@ -250,6 +250,63 @@ function Bestbet(props) {
         {searchWord}
       </div>
     )
+  }
+
+  const ssbGreen4 = '#00824d'
+  const ssbDark6 = '#162327'
+  const borderDark = `1px solid ${ssbDark6}`
+  const customAsyncSelectStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      boxShadow: 'none',
+      border: 'none',
+      borderRadius: 'none',
+      outline: state.isFocused && `2px solid ${ssbGreen4}`,
+      outlineOffset: state.isFocused && '-1px'
+    }),
+    dropdownIndicator: (provided) => ({
+      ...provided,
+      color: ssbGreen4
+    }),
+    indicatorsContainer: (provided) => ({
+      ...provided,
+      border: borderDark,
+      borderLeft: 'transparent',
+      borderRadius: '0px',
+      color: ssbGreen4
+    }),
+    indicatorSeparator: (provided) => ({
+      ...provided,
+      backgroundColor: 'transparent'
+    }),
+    menu: (provided) => ({
+      ...provided,
+      margin: '0px',
+      border: borderDark,
+      borderRadius: '0px'
+    }),
+    menuList: (provided) => ({
+      ...provided,
+      padding: '0px'
+    }),
+    option: (provided, state) => ({
+      backgroundColor: state.isSelected ? ssbGreen4 : (state.isFocused ? '#274247' : 'white'),
+      fontWeight: state.isSelected && '700',
+      color: state.isFocused || state.isSelected ? 'white' : ssbDark6,
+      padding: '12px',
+      opacity: 1
+    }),
+    placeholder: (provided) => ({
+      ...provided,
+      color: ssbDark6
+    }),
+    valueContainer: (provided) => ({
+      ...provided,
+      border: borderDark,
+      borderRight: 'transparent',
+      borderRadius: '0px',
+      fontSize: '16px'
+    })
   }
 
   function renderBestBetForm() {
@@ -284,6 +341,7 @@ function Bestbet(props) {
                 <AsyncSelect
                   className="dropdown-interactive-area"
                   placeholder="Søk ved å skrive..."
+                  styles={customAsyncSelectStyles}
                   defaultInputValue={selectedContentResult && selectedContentResult.label}
                   cacheOptions
                   defaultOptions
@@ -321,26 +379,27 @@ function Bestbet(props) {
               onSelect={(item) => setMainSubjectValue(item.id !== '' ? item.title : '')}
             />
             {isXPContent &&
-            <RadioGroup
-              header="Velg dato format"
-              onChange={handleDatoTypeSelect}
-              selectedValue={startDateValue ? startDateValue === 'xp' ? 'date-select-xp' : 'date-select-manual' : 'date-select-none'}
-              orientation="column"
-              items={[
-                {
-                  label: 'Manuell innføring',
-                  value: 'date-select-manual'
-                },
-                {
-                  label: 'Hent fra XP innhold',
-                  value: 'date-select-xp'
-                },
-                {
-                  label: 'Ingen dato',
-                  value: 'date-select-none'
-                }
-              ]}
-            />
+              <RadioGroup
+                className="mb-3"
+                header="Velg dato format"
+                onChange={handleDatoTypeSelect}
+                selectedValue={startDateValue ? startDateValue === 'xp' ? 'date-select-xp' : 'date-select-manual' : 'date-select-none'}
+                orientation="column"
+                items={[
+                  {
+                    label: 'Manuell innføring',
+                    value: 'date-select-manual'
+                  },
+                  {
+                    label: 'Hent fra XP innhold',
+                    value: 'date-select-xp'
+                  },
+                  {
+                    label: 'Ingen dato',
+                    value: 'date-select-none'
+                  }
+                ]}
+              />
             }
             {(!isXPContent || showDatePicker) &&
               <Input
