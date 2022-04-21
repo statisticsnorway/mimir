e8ecd38566a1d1e7773f76f54322c4a5235d103c
johnnadeluy <johnnadel.salita@gmail.com>
2022-04-21 13:33:43 +0200

    Set config resolve symlinks to true
    MIMIR-1178

--- webpack.config.react4xp.js
+++ webpack.config.react4xp.js
@@ -1,6 +1,6 @@
 module.exports = function(env, config) {
   // This makes 'npm link' symlinks in node_modules work:
-  config.resolve.symlinks = false
+  config.resolve.symlinks = true
 
   return config
 }
