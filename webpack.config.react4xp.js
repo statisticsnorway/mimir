e918a59382f01b263632cba621fde2902da9553b
johnnadeluy <johnnadel.salita@gmail.com>
2022-04-21 15:26:37 +0200

    Uninstall @enonic/react4xp and @enonic/react_components Revert changes in build.gradle and webpack.config.react4xp.js Keep react4xp.config.js for now
    MIMIR-1178

--- webpack.config.react4xp.js
+++ webpack.config.react4xp.js
@@ -1,6 +1,6 @@
 module.exports = function(env, config) {
   // This makes 'npm link' symlinks in node_modules work:
-  config.resolve.symlinks = true
+  config.resolve.symlinks = false
 
   return config
 }
