c88665186d6bed1b2a3b0143a5a2d1be7552e923
Carina Nordseth <cgn@ssb.no>
2022-06-15 16:05:30 +0200

    MIMIR-764:Fixed label Highchart Pie with Excel

--- src/main/resources/lib/ssb/parts/highcharts/data/htmlTable.ts
+++ src/main/resources/lib/ssb/parts/highcharts/data/htmlTable.ts
@@ -121,7 +121,7 @@ function dataFormatAreaLineLinear(seriesAndCategories: SeriesAndCategoriesRaw): 
 function dataFormatPie(seriesAndCategories: SeriesAndCategoriesRaw): Series {
   if (seriesAndCategories.categories.length === 1) {
     return {
-      name: 'Antall',
+      name: seriesAndCategories.categories[0],
       data: seriesAndCategories.series.map((serie): PieData => ({
         ...serie,
         y: serie.data[0]
