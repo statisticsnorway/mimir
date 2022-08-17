58bacef2ed5fd859e12b4f89fc4a013e796eec44
Jenni <kdj@ssb.no>
2022-08-17 14:29:20 +0200

    Oppdatert macro for staticVisualization for å inkludere dividers

--- src/main/resources/site/macros/staticVisualization/staticVisualization.ts
+++ src/main/resources/site/macros/staticVisualization/staticVisualization.ts
@@ -6,7 +6,29 @@ const {
   preview
 } = __non_webpack_require__('../../parts/staticVisualization/staticVisualization')
 
+const {
+  preview: dividerControllerPreview
+} = __non_webpack_require__('../../parts/divider/divider')
+
+const {
+  renderError
+} = __non_webpack_require__('/lib/ssb/error/error')
+
 exports.macro = (context: MacroContext): Response | React4xpResponse => {
-  const config: StaticVisualizationConfig = context.params
-  return preview(context, config.staticVisualizationContent)
+  try {
+    const divider: Response = dividerControllerPreview(context, {
+      dark: false
+    })
+
+    const config: StaticVisualizationConfig = context.params
+    const staticVisualization: Response = preview(context, config.staticVisualizationContent)
+
+    if (staticVisualization.status && staticVisualization.status !== 200) throw new Error(`Highchart with id ${config.staticVisualizationContent} missing`)
+    staticVisualization.body = divider.body as string + staticVisualization.body + divider.body
+
+    return staticVisualization
+
+  } catch (e) {
+    return renderError(context.request, 'Error in macro', e)
+  }
 }
