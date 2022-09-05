da72a3d097beff82b1aeddf69bbb9c944c698301
Kyle Danny <codingkyle92@gmail.com>
2022-09-05 15:51:26 +0200

    added mock data for projects and publications

--- src/main/resources/site/parts/employee/employee.jsx
+++ src/main/resources/site/parts/employee/employee.jsx
@@ -2,12 +2,59 @@ import React from 'react'
 // import { Title, Link, Divider } from '@statisticsnorway/ssb-component-library'
 import PropTypes from 'prop-types'
 
+
+// mock data 
+const mockData = {
+  projects: [
+    {
+      title: "Konjunkturtendensene",
+      description: "I konjunkturrapportene beskrives og analyseres den siste utviklingen i norske og internasjonal økonomi og utsiktene framover (normalt 3-4 år framover). Det publiseres fire konjunkturrapporter hvert år. Prognosene utarbeides med hjelp av SSBs makroøkonometriske modell KVARTS."
+    },
+    {
+      title: "MODAG og KVARTS",
+      description: "MODAG og KVARTS er makroøkonometriske modeller for norsk økonomi og benyttes til framskrivninger og politikkanalyser på kort og mellomlang sikt. Den viktigste forskjellen mellom de to modellene er at MODAG regner på årlige data, mens KVARTS regner på kvartalsvise data."
+    },
+    {
+      title: "Etterspørselen etter ny teknologi",
+      description: "I dette prosjektet studeres årsakene til den teknologiske utviklingen, og teknologiens virkning på økonomisk vekst og sysselsetting. Investeringer i forskning og utvikling (FoU) søkes implementert i et fullstendig system for etterspørsel etter innsatsfaktorer." 
+    },
+  ],
+  publications: [
+    {
+      title: "Efficient combinations of taxes on fuel and vehicles",
+      author: "Kyle Danny",
+      description: "The Energy Journal / 2021"
+    },
+  ]
+}
+
 function Employee(props) {
   const {
     title, email, position, phone, description, profileImages, myCV, projects, isResearcher, cristinId
   } = props
   
   console.log(props)
+
+  const renderProjects = () => {
+    const projects = mockData.projects.map(project => {
+      return (
+        <div>
+          <h3>{project.title}</h3>
+          <p>{project.description}</p>
+        </div>
+      )
+    })
+    return <div>{projects}</div>
+  }
+
+  const renderPublications = () => {
+    const publication = mockData.publications[0]
+    return <div>
+      <h3>{publication.title}</h3>
+      <p>{publication.author}</p>
+      <p>{publication.description}</p>
+    </div>
+  }
 
   return (
     <section className="xp-part employee container p-0 mb-5">
@@ -36,13 +83,15 @@ function Employee(props) {
       <div className="row justify-content-center">
         <div className="employee-projects col-6">
           <h2>Prosjekter</h2>
-          <p>..........</p>
+          { renderProjects() }
         </div>
       </div>
       <div className="row justify-content-center">
         <div className="employee-publications col-6">
           <h2>Publiseringer</h2>
-          <p>..........</p>
+          { renderPublications() }
+          { renderPublications() }
+          { renderPublications() }
         </div>
       </div>
     </section>
