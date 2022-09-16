c66200300d68ca7737ff3e385dc21567fb7fbef2
johnnadeluy <johnnadel.salita@gmail.com>
2022-09-16 10:36:11 +0200

    Work in progress
    MIMIR-1332

--- src/main/resources/lib/cristin/cristin.ts
+++ src/main/resources/lib/cristin/cristin.ts
@@ -1,9 +1,48 @@
-import { Result, fetchResult } from '/lib/cristin/service'
+// import { Result, ListOfResults, fetchResult, fetchPersonResults } from '/lib/cristin/service'
+import { Result, ListOfResults, FetchResponse, Project, fetchResult, fetchResults, fetchProject } from '/lib/cristin/service'
 
-export function fetchCristinResult(id: string): Result | void {
+export function fetchResultCristin(id: string): Result | void {
+  // filtrering - category, contributor. per side. paginering
   const result: Result | void = fetchResult({
     id
   })
 
   return result
+//   return {
+//     category: result.category,
+//     contributors: result.contributors,
+//     cristin_result_id: result.cristin_result_id,
+//     links: result.links,
+//     title: result.title,
+//     year_reported: result.year_published
+//   }
+}
+
+// TODO: Publikasjon, forside
+export function fetchResultsCristin(): FetchResponse<ListOfResults> {
+  const results: FetchResponse<ListOfResults> = fetchResults({
+    institution: 'statistisk+sentralbyra',
+    per_page: '5',
+    sort: 'desc'
+  })
+
+  return results
+}
+
+// TODO: Publikasjonsopplisting, ansattside
+// export function fetchPersonResultsCristin(personId: string): ListOfResults {
+//   const personResults: ListOfResults = fetchPersonResults({
+//     personId
+//   })
+
+//   return personResults
+// }
+
+// TODO: Prosjektside
+export function fetchProjectCristin(projectId: string): Project {
+  const project: Project = fetchProject({
+    id: projectId
+  })
+
+  return project
 }
--- src/main/resources/services/cristin/cristin.ts
+++ src/main/resources/services/cristin/cristin.ts
@@ -1,11 +1,20 @@
-import { Result } from '/lib/cristin/service'
-import { fetchCristinResult } from '/lib/cristin/cristin'
+import { Result, Project, FetchResponse, ListOfResults } from '/lib/cristin/service'
+// import { fetchResultCristin, fetchPersonResultsCristin } from '/lib/cristin/cristin'
+import { fetchProjectCristin, fetchResultCristin, fetchResultsCristin } from '/lib/cristin/cristin'
 
 exports.get = (): XP.Response => {
-  const result: Result | void = fetchCristinResult('1929571')
+  // const result: Result | void = fetchResultCristin('1929571')
+  // const personResults: Array<Result> = fetchPersonResultsCristin('23169')
+  // const project: Project | void = fetchProjectCristin('555465')
+  const results: FetchResponse<ListOfResults> = fetchResultsCristin()
 
   return {
-    body: JSON.stringify(result, null, 2),
+    body: JSON.stringify({
+      // result,
+      // personResults,
+      // project
+      results
+    }, null, 2),
     contentType: 'application/json'
   }
 }
