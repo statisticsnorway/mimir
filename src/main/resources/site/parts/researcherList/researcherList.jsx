e014f1452d6d283345adfcfddec2f84d0255af6c
Kyle Danny <codingkyle92@gmail.com>
2022-09-26 13:25:04 +0200

    query employee content type

--- src/main/resources/site/parts/researcherList/researcherList.jsx
+++ src/main/resources/site/parts/researcherList/researcherList.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function ResearcherList(props) {
-  const { title } = props
+  const { title, researchersList } = props
+
+
+  console.log(researchersList)
 
   return (
     <section className="xp-part employee container p-0 mb-5">
@@ -14,5 +17,6 @@ function ResearcherList(props) {
 export default (props) => <ResearcherList {...props} />
 
 ResearcherList.propTypes = {
-  title: PropTypes.string
+  title: PropTypes.string,
+  researchersList: PropTypes.array
 }
--- src/main/resources/site/parts/researcherList/researcherList.ts
+++ src/main/resources/site/parts/researcherList/researcherList.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'enonic-types/controller'
 import { React4xp, React4xpResponse } from '../../../lib/types/react4xp'
-import { Content } from 'enonic-types/content'
+import { Content, QueryResponse } from 'enonic-types/content'
 import { renderError } from '../../../lib/ssb/error/error'
+import { Employee } from '../../content-types/employee/employee'
 
 const {
   getContent
 } = __non_webpack_require__('/lib/xp/portal')
+const {
+  query
+} = __non_webpack_require__('/lib/xp/content')
 const React4xp: React4xp = __non_webpack_require__('/lib/enonic/react4xp')
 
 exports.get = (req: Request): React4xpResponse | Response => {
@@ -20,14 +24,30 @@ exports.preview = (req: Request): React4xpResponse => renderPart(req)
 
 function renderPart(req: Request): React4xpResponse {
   const content: Content = getContent()
-  
+  const researchers = getResearchers()
+
   const props: PartProps = {
-    title: content.displayName
+    title: content.displayName,
+    researchersList: researchers
   }
 
   return React4xp.render('site/parts/researcherList/researcherList', props, req)
 }
 
+function getResearchers() {
+  const results: Array<Content<Employee>> = query({
+    start: 0,
+    count: 2,
+    // sort: 'publish.from DESC',
+    // query: `data.isResearcher`,
+    contentTypes: [
+      `${app.name}:employee`
+    ]
+  }).hits as unknown as Array<Content<Employee>>
+  return results.filter(result => result.data.isResearcher)
+}
+
 interface PartProps {
-  title: string;
+  title: string,
+  researchersList: Array<Content<Employee>>,
 }
