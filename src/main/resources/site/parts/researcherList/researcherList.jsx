48400708b59366b75739d0ef968e1c7431f81e74
Kyle Danny <codingkyle92@gmail.com>
2022-09-26 22:29:04 +0200

    sanitizing researchers for rendering

--- src/main/resources/site/parts/researcherList/researcherList.jsx
+++ src/main/resources/site/parts/researcherList/researcherList.jsx
@@ -4,12 +4,47 @@ import PropTypes from 'prop-types'
 function ResearcherList(props) {
   const { title, researchers } = props
 
-
   console.log(researchers)
+
+  const groupedCollection = {};
+
+  for (let i = 0; i < researchers.length; i++) {       
+    let firstLetter = researchers[i].surname.charAt(0);
+    if (groupedCollection[firstLetter] == undefined) {             
+      groupedCollection[firstLetter] = [];         
+    }         
+    groupedCollection[firstLetter].push(researchers[i]);     
+  }
+
+  console.log(groupedCollection)
+
+  const renderResearchers = () => {
+    let html = []
+    for (const [letter, researchersList] of Object.entries(groupedCollection)) {
+      
+      let box = researchersList.map((researcher, i) => {
+        return (
+          <li className={`${i === 0 ? 'first' : ''}`} >
+            <p>letter: {letter}</p>
+            <p>{researcher.surname}, {researcher.name}</p> 
+          </li>
+        )
+      }).flat()
+      console.log("divBox", box)
+      html.push(box)
+    }
+    console.log("html", html.flat().join(' '))
+    return (
+      <ul>
+        {html.join(' ')}
+      </ul>
+    ) 
+  }
 
   return (
     <section className="xp-part employee container p-0 mb-5">
       {title}
+      {researchers != [] ? renderResearchers() : null}
     </section>
   )
 }
