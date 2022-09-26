06f46dc721718f0f539e742640e9561bfa52478f
johnnadeluy <johnnadel.salita@gmail.com>
2022-09-26 09:57:49 +0200

    Return results ids instead of urls for cristin project service
    MIMIR-1332

--- src/main/resources/services/cristinProject/cristinProject.ts
+++ src/main/resources/services/cristinProject/cristinProject.ts
@@ -3,10 +3,14 @@ import { fetchProjectCristin } from '../../lib/ssb/cristin/cristin'
 
 exports.get = (req: XP.Request): XP.Response => {
   const projectId: string | number = req.params.projectId ? req.params.projectId : ''
-  const result: Project = fetchProjectCristin(projectId)
+  const project: Project = fetchProjectCristin(projectId)
+  const projectResultsIds: Array<string | undefined> | undefined = project.results && project.results.map((result) => result.split('/').pop())
 
   return {
-    body: result,
+    body: {
+      ...project,
+      results: projectResultsIds
+    },
     contentType: 'application/json'
   }
 }
