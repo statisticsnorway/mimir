28b44f291abcfb839c9d4fa0454f2606568b81ba
Kyle Danny <codingkyle92@gmail.com>
2022-09-27 13:29:33 +0200

    desktop responsivenss

--- src/main/resources/site/parts/researcherList/researcherList.jsx
+++ src/main/resources/site/parts/researcherList/researcherList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Button, Title, Link, LeadParagraph, Paragraph, Accordion } from '@statisticsnorway/ssb-component-library'
+import { Link, Divider, Text } from '@statisticsnorway/ssb-component-library'
+import { ArrowRight } from 'react-feather'
 
 
 function ResearcherList(props) {
@@ -36,31 +37,42 @@ function ResearcherList(props) {
 
       let box = researchersList.map((researcher, i) => {
         return (
-          <li className={`${i === 0 ? 'first' : ''}`} >
-            <Link href={researcher._path} linkType="header">{researcher.surname}, {researcher.name}</Link>
-            {researcher.position ? <p>{researcher.position}</p> : null}
-            <p>{researcher.phone} / {researcher.email} / Forskningsområde</p> 
+          <>
+          <li className="list-item" >
+            {i == 0 ? <div className="letter col-md-1"><h2>{letter}</h2></div> : <div className="col-md-1"></div>}
+            <div className="list col-md-11">
+              <div>
+                <Link href={researcher._path} linkType="header">{researcher.surname}, {researcher.name}</Link>
+                {researcher.position ? <div><Text small>{researcher.position}</Text></div> : null}
+                <div><Text small><Link href={'tel:' + researcher.phone}>{researcher.phone}</Link>  / <Link href={'mailto:' + researcher.email}>{researcher.email}</Link> / Forskningsområde</Text></div>
+              </div>
+              <div className="list-arrow"><ArrowRight size={30} /></div>
+            </div>
           </li>
+          <Divider light />
+          </>
         )
       })
 
       html.push(
-        <div className="letter-list">
-          <div className="letter"><h2>{letter}</h2></div>
-          <ul className="list">{box}</ul>
-        </div>
+        <ul className="letter-list">{box}</ul> 
       )
     }
 
     return (
       <div>
+        <Divider dark />
         {html}
       </div>
     ) 
   }
 
   return (
-    <section className="xp-part employee container p-0 mb-5">
+    <section className="xp-part researchers container p-0 mb-5">
+      <div className="row banner">
+        <h1>Ansatte forskning</h1>  
+        <p>På denne siden finner du kontaktinformasjon til alle som jobber i Forskningsavdelingen i SSB. Klikk på navnet for å lese mer om personen.</p>
+      </div>
       {researchers != [] ? renderResearchers() : null}
     </section>
   )
