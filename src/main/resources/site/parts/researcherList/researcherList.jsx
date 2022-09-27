6cd44c3d61d6ee957a78c6b83fc5a052c377f2ff
Kyle Danny <codingkyle92@gmail.com>
2022-09-27 09:34:51 +0200

    listing of employees in alphabetical order

--- src/main/resources/site/parts/researcherList/researcherList.jsx
+++ src/main/resources/site/parts/researcherList/researcherList.jsx
@@ -4,39 +4,52 @@ import PropTypes from 'prop-types'
 function ResearcherList(props) {
   const { title, researchers } = props
 
-  console.log(researchers)
-
   const groupedCollection = {};
+
+  const sortAlphabetically = (obj) => {
+    return Object.keys(obj)
+      .sort()
+      .reduce((accumulator, key) => {
+        accumulator[key] = obj[key];
+
+    return accumulator;
+  }, {});
+
+  }
 
   for (let i = 0; i < researchers.length; i++) {       
     let firstLetter = researchers[i].surname.charAt(0);
+    
     if (groupedCollection[firstLetter] == undefined) {             
       groupedCollection[firstLetter] = [];         
     }         
     groupedCollection[firstLetter].push(researchers[i]);     
   }
 
-  console.log(groupedCollection)
-
   const renderResearchers = () => {
     let html = []
-    for (const [letter, researchersList] of Object.entries(groupedCollection)) {
-      
+    const sortedGroupCollection = sortAlphabetically(groupedCollection)
+    for (const [letter, researchersList] of Object.entries(sortedGroupCollection)) {
+      html.push(<h2>letter: {letter}</h2>)
       let box = researchersList.map((researcher, i) => {
         return (
-          <li className={`${i === 0 ? 'first' : ''}`} >
-            <p>letter: {letter}</p>
-            <p>{researcher.surname}, {researcher.name}</p> 
-          </li>
+          <>
+            {/* {i === 0 ? <h2>letter: {letter}</h2> : null} */}
+            <li className={`${i === 0 ? 'first' : ''}`} >
+              <p>{researcher.surname}, {researcher.name}</p>
+              {researcher.area ? <p>{researcher.area}</p> : null}
+              <p>{researcher.phone} / {researcher.email} / Forskningsområde</p> 
+            </li>
+          </>
         )
-      }).flat()
-      console.log("divBox", box)
+      })
+
       html.push(box)
     }
-    console.log("html", html.flat().join(' '))
+
     return (
       <ul>
-        {html.join(' ')}
+        {html}
       </ul>
     ) 
   }
