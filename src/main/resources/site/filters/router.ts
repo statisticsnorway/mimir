cd1f2201907017f84d85da821a96924c9c105eea
johnnadeluy <johnnadel.salita@gmail.com>
2022-10-13 15:13:07 +0200

    Convert router to typescript; add types, remove cookies param for http client request
    MIMIR-1383

--- src/main/resources/site/filters/router.ts
+++ src/main/resources/site/filters/router.ts
@@ -1,14 +1,13 @@
+import { MunicipalityWithCounty } from '/lib/ssb/dataset/klass/municipalities'
+import { get, Site, Content } from '/lib/xp/content'
+
 const {
   request
 } = __non_webpack_require__('/lib/http-client')
-
 const {
   pageUrl,
   getSite
 } = __non_webpack_require__('/lib/xp/portal')
-const {
-  get
-} = __non_webpack_require__('/lib/xp/content')
 const {
   fromFilterCache
 } = __non_webpack_require__('/lib/ssb/cache/cache')
@@ -17,32 +16,32 @@ const {
   municipalsWithCounties
 } = __non_webpack_require__('/lib/ssb/dataset/klass/municipalities')
 
-
-exports.filter = function(req, next) {
+exports.filter = function(req: XP.Request, next: (req: XP.Request) => XP.Response): XP.Response {
   if (req.params.selfRequest) return next(req)
-  delete req.headers['Accept-Encoding']
-  let pageTitle = ''
-  const region = req.path.split('/').pop()
-  const municipality = getMunicipalityByName(municipalsWithCounties(), region)
+  const headers: Headers = req.headers
+  delete headers['Accept-Encoding']
+  let pageTitle: string = ''
+  const region: string | undefined = req.path.split('/').pop()
+  const municipality: MunicipalityWithCounty | undefined = getMunicipalityByName(municipalsWithCounties(), region as string)
   if (!municipality && region !== 'kommune') {
     return next(req)
   }
 
-  let targetId = null
+  let targetId: string | null = null
   if (req.path.indexOf('/kommunefakta/') > -1) {
-    targetId = get({
+    targetId = (get({
       key: '/ssb/kommunefakta/kommune'
-    })._id
+    }) as Content)._id
     pageTitle = `Kommunefakta ${municipality ? municipality.displayName : ''}`
   } else if (req.path.indexOf('/kommuneareal/') > -1) {
-    targetId = get({
+    targetId = (get({
       key: '/ssb/kommuneareal/kommune'
-    })._id
+    }) as Content)._id
     pageTitle = `Kommuneareal ${municipality ? municipality.displayName : ''}`
   } else if (req.path.indexOf('/barn-og-unge/') > -1) {
-    targetId = get({
+    targetId = (get({
       key: '/ssb/barn-og-unge/kommune'
-    })._id
+    }) as Content)._id
     pageTitle = `Barn og unge ${municipality ? municipality.displayName : ''}`
   }
 
@@ -50,21 +49,20 @@ exports.filter = function(req, next) {
     return next(req)
   }
 
-  const targetUrl = pageUrl({
+  const targetUrl: string = pageUrl({
     id: targetId
   })
 
-  const targetResponse = fromFilterCache(req, targetId, req.path, () => {
-    const headers = req.headers
-    delete req.headers['Connection'] // forbidden header name for http/2 and http/3
-    delete req.headers['Host']
+  const targetResponse: XP.Response = fromFilterCache(req, targetId, req.path, () => {
+    const headers: Headers = req.headers
+    delete headers['Connection'] // forbidden header name for http/2 and http/3
+    delete headers['Host']
 
     return request({
       url: `http://localhost:8080${targetUrl}`,
       headers,
-      cookies: req.cookies,
       params: {
-        selfRequest: true,
+        selfRequest: 'true',
         municipality: JSON.stringify(municipality),
         pageTitle
       },
@@ -74,11 +72,16 @@ exports.filter = function(req, next) {
   })
 
   if (pageTitle) {
-    const site = getSite()
-    targetResponse.body = targetResponse.body.replace(/(<title>)(.*?)(<\/title>)/i, `<title>${pageTitle} - ${site.displayName}</title>`)
+    const site: Content = getSite()
+    targetResponse.body = (targetResponse.body as string).replace(/(<title>)(.*?)(<\/title>)/i, `<title>${pageTitle} - ${site.displayName}</title>`)
   }
 
   return {
     body: targetResponse.body
   }
+}
+
+// XP.Request.headers type is set as readonly; we have to define our own type to delete the desired properties
+type Headers = {
+  [key: string]: string | undefined
 }
