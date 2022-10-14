b269622cf09f94914e26c953329782674ba0c647
Larsen, Glenruben Engen <gle@ssb.no>
2022-10-14 11:15:58 +0200

    Adds URI encoding to query parameters in solr url
    MIMIR-1383

--- src/main/resources/lib/ssb/utils/solrUtils.ts
+++ src/main/resources/lib/ssb/utils/solrUtils.ts
@@ -15,9 +15,10 @@ export function solrSearch(term: string,
   contentType: string,
   sortParam: string | undefined): SolrPrepResultAndTotal {
   const lang: string = language === 'en' ? 'en' : 'no'
-  const languageQuery: string = `fq=sprak:${lang}`
-  const contentTypeQuery: string = contentType ? `&fq=innholdstype:${contentType}` : ''
-  const subjectQuery: string = mainSubject ? `&fq=hovedemner:${mainSubject}` : ''
+  const languageQuery: string = `fq=sprak${encodeURIComponent(':')}${lang}`
+  // Note the use of uri encoded quotation marks, Solr needs these when a facet field is multiple words
+  const contentTypeQuery: string = contentType ? `&fq=innholdstype${encodeURIComponent(':"' + contentType + '"')}` : ''
+  const subjectQuery: string = mainSubject ? `&fq=hovedemner${encodeURIComponent(':"' + mainSubject + '"')}` : ''
   const sortQuery: string = sortParam ? `&sort=${sortParam}+desc` : ''
   const searchResult: SolrResult | undefined = querySolr({
     query: createQuery(term, numberOfHits, start, languageQuery, contentTypeQuery, subjectQuery, sortQuery)
