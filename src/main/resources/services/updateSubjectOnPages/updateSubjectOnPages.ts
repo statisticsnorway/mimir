b684e7ed355ad580f3cf8f9d1845eec93edbf2dd
Glenruben Larsen <gle@ssb.no>
2022-11-15 11:04:15 +0100

    Working example copies field to content from config

--- src/main/resources/services/updateSubjectOnPages/updateSubjectOnPages.ts
+++ src/main/resources/services/updateSubjectOnPages/updateSubjectOnPages.ts
@@ -1,10 +1,10 @@
 import type { Page } from 'site/content-types/page/page'
-import { query, type QueryResponse } from '/lib/xp/content'
+import { query, modify, type QueryResponse, type Content } from '/lib/xp/content'
 import type { SubjectTag } from '../../site/x-data/subjectTag/subjectTag'
 import { DefaultPageConfig } from 'site/pages/default/default-page-config'
 
 export function get(req: XP.Request): XP.Response {
-  const contentToFix: QueryResponse<Page, SubjectTag, object> = query({
+  const contentToFix: QueryResponse<Page, PageXData, object> = query({
     query: '',
     count: 50,
     contentTypes: [`${app.name}:page`],
@@ -22,16 +22,54 @@ export function get(req: XP.Request): XP.Response {
     },
   })
 
+  const fixedContents: Array<Content<Page, PageXData>> = []
+  // let contentKey: string
+  // let typeString: 'mainSubject' | 'subSubject' | undefined
+  // let subjectCode: string
+
   contentToFix.hits.forEach((hit) => {
     const testType: DefaultPageConfig = hit.page.config as DefaultPageConfig
-    if (testType?.subjectType) {
-      const typeString: string = testType.subjectType
-      const subjectCode: string = testType.subjectCode ?? 'no code'
+    if (testType?.subjectType && testType?.subjectCode) {
+      const contentKey: string = hit._id
+      const typeString: 'mainSubject' | 'subSubject' | undefined = testType.subjectType
+      const subjectCode: string = testType.subjectCode
+
+      fixedContents.push(edit(contentKey, typeString, subjectCode))
+
       log.info(`${typeString}: ${subjectCode}`)
     }
   })
 
   return {
-    body: { contentToFix }, //JSON.stringify({}, null, 2),
+    body: { fixedContents },
+    contentType: 'application/json',
+  }
+}
+
+function edit(key: string, type: 'mainSubject' | 'subSubject' | undefined, code: string): Content<Page, PageXData> {
+  function editor(content: Content<Page, PageXData>) {
+    // ;(content.x.mimir.subjectTag.subjectType = type), (content.x.mimir.subjectTag.subjectCode = code)
+    content.x = {
+      ...content.x,
+      mimir: {
+        subjectTag: {
+          subjectCode: code,
+          subjectType: type,
+        },
+      },
+    }
+    return content
+  }
+
+  return modify({
+    key: key,
+    requireValid: true,
+    editor: editor,
+  })
+}
+
+interface PageXData {
+  mimir: {
+    subjectTag: SubjectTag
   }
 }
