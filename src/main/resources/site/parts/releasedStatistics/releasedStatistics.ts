98245ec6a066f3b2b0046d656129f4d5e8bfb03c
Carina <cgn@ssb.no>
2022-12-14 08:38:28 +0100

    MIMIR-1450:Cleanup

--- src/main/resources/site/parts/releasedStatistics/releasedStatistics.ts
+++ src/main/resources/site/parts/releasedStatistics/releasedStatistics.ts
@@ -1,7 +1,6 @@
 __non_webpack_require__('/lib/ssb/polyfills/nashorn')
 
 import type { Content, QueryDSL } from '/lib/xp/content'
-import type { StatisticInListing } from '../../../lib/ssb/dashboard/statreg/types'
 import { render, type RenderResponse } from '/lib/enonic/react4xp'
 import type { ReleasedStatistics as ReleasedStatisticsPartConfig } from '.'
 import type { YearReleases } from '../../../lib/ssb/utils/variantUtils'
@@ -13,12 +12,9 @@ import { parseISO, getMonth, getYear, getDate } from 'date-fns'
 import { stringToServerTime } from '../../../lib/ssb/utils/dateUtils'
 
 const { fromPartCache } = __non_webpack_require__('/lib/ssb/cache/partCache')
-const { getAllStatisticsFromRepo } = __non_webpack_require__('/lib/ssb/statreg/statistics')
 const { renderError } = __non_webpack_require__('/lib/ssb/error/error')
 const { isEnabled } = __non_webpack_require__('/lib/featureToggle')
-const { checkLimitAndTrim } = __non_webpack_require__('/lib/ssb/utils/arrayUtils')
-const { addMonthNames, getReleasesForDay, groupStatisticsByYearMonthAndDay, prepareStatisticRelease } =
-  __non_webpack_require__('/lib/ssb/utils/variantUtils')
+const { addMonthNames, groupStatisticsByYearMonthAndDay } = __non_webpack_require__('/lib/ssb/utils/variantUtils')
 
 export function get(req: XP.Request): RenderResponse | XP.Response {
   try {
@@ -65,43 +61,39 @@ function getGroupedWithMonthNames(
   const numberOfReleases: number = part.config.numberOfStatistics ? parseInt(part.config.numberOfStatistics) : 8
 
   //To get releases 08.00 before data from statreg is updated
-  const queryNextReleaseTodayQuery: QueryDSL = {
-    range: {
-      field: 'data.nextRelease',
-      from: 'dateTime',
-      lte: stringToServerTime(),
-    },
-  } as unknown as QueryDSL
-
   const nextReleaseToday: ContentLight<ReleaseVariant>[] = getStatisticVariantsFromRepo(
     currentLanguage,
-    queryNextReleaseTodayQuery,
+    {
+      range: {
+        field: 'data.nextRelease',
+        from: 'dateTime',
+        lte: stringToServerTime(),
+      },
+    } as unknown as QueryDSL,
     numberOfReleases
   )
-
-  const previousReleasesQuery: QueryDSL = {
-    range: {
-      field: 'publish.from',
-      type: 'dateTime',
-      lte: new Date().toISOString(),
-    },
-  }
 
   const numberPreviousReleases: number =
     nextReleaseToday.length !== 0 ? numberOfReleases - nextReleaseToday.length : numberOfReleases
 
   const allPreviousStatisticVariantsFromRepo: ContentLight<ReleaseVariant>[] = getStatisticVariantsFromRepo(
     currentLanguage,
-    previousReleasesQuery,
+    {
+      range: {
+        field: 'publish.from',
+        type: 'dateTime',
+        lte: new Date().toISOString(),
+      },
+    },
     numberPreviousReleases
   )
 
   const releasesPreppedNextReleaseToday: PreparedStatistics[] = nextReleaseToday.map((variant) => {
-    return prepReleasesRepo(variant, parseISO(variant.data.nextRelease))
+    return prepReleases(variant, parseISO(variant.data.nextRelease))
   })
 
   const releasesPreppedPreviousRelease: PreparedStatistics[] = allPreviousStatisticVariantsFromRepo.map((variant) => {
-    return prepReleasesRepo(variant, parseISO(variant.data.previousRelease))
+    return prepReleases(variant, parseISO(variant.data.previousRelease))
   })
 
   const releasedStatistics: PreparedStatistics[] =
@@ -113,22 +105,7 @@ function getGroupedWithMonthNames(
   return addMonthNames(groupedByYearMonthAndDay, currentLanguage)
 }
 
-export function filterOnPreviousReleases(
-  stats: Array<StatisticInListing>,
-  numberOfReleases: number
-): Array<StatisticInListing> {
-  const releases: Array<StatisticInListing> = []
-  for (let i = 0; releases.length < numberOfReleases; i++) {
-    const day: Date = new Date()
-    day.setDate(day.getDate() - i)
-    const releasesOnThisDay: Array<StatisticInListing> = getReleasesForDay(stats, day)
-    const trimmed: Array<StatisticInListing> = checkLimitAndTrim(releases, releasesOnThisDay, numberOfReleases)
-    releases.push(...trimmed)
-  }
-  return releases
-}
-
-function prepReleasesRepo(variant: ContentLight<ReleaseVariant>, date: Date): PreparedStatistics {
+function prepReleases(variant: ContentLight<ReleaseVariant>, date: Date): PreparedStatistics {
   return {
     id: Number(variant.data.statisticId),
     name: variant.data.name,
@@ -139,7 +116,7 @@ function prepReleasesRepo(variant: ContentLight<ReleaseVariant>, date: Date): Pr
       monthNumber: getMonth(date),
       year: getYear(date),
       frequency: variant.data.frequency,
-      period: variant.data.period,
+      period: variant.data.period ? variant.data.period.toLowerCase() : '',
     },
   }
 }
