3324dc22d7839ae3ef98d1b2917fff61157158c3
Carina Nordseth <cgn@ssb.no>
2023-02-03 11:13:57 +0100

    MIMIR-1537:Some cleanup

--- src/main/resources/site/filters/router.ts
+++ src/main/resources/site/filters/router.ts
@@ -1,8 +1,7 @@
 import { MunicipalityWithCounty } from '/lib/ssb/dataset/klass/municipalities'
-import { get, Content } from '/lib/xp/content'
+import { Content, get } from '/lib/xp/content'
 
 const { request } = __non_webpack_require__('/lib/http-client')
-const { pageUrl, getSite } = __non_webpack_require__('/lib/xp/portal')
 const { fromFilterCache } = __non_webpack_require__('/lib/ssb/cache/cache')
 const { getMunicipalityByName, municipalsWithCounties } = __non_webpack_require__(
   '/lib/ssb/dataset/klass/municipalities'
@@ -10,9 +9,6 @@ const { getMunicipalityByName, municipalsWithCounties } = __non_webpack_require_
 
 exports.filter = function (req: XP.Request, next: (req: XP.Request) => XP.Response): XP.Response {
   if (req.params.selfRequest) return next(req)
-  const headers: Headers = req.headers
-  delete headers['Accept-Encoding']
-  let pageTitle = ''
   const region: string | undefined = req.path.split('/').pop()
   const municipality: MunicipalityWithCounty | undefined = getMunicipalityByName(
     municipalsWithCounties(),
@@ -21,53 +17,19 @@ exports.filter = function (req: XP.Request, next: (req: XP.Request) => XP.Respon
   if (!municipality && region !== 'kommune') {
     return next(req)
   }
-
-  let targetId: string | null = null
-  if (req.path.indexOf('/kommunefakta/') > -1) {
-    targetId = (
-      get({
-        key: '/ssb/kommunefakta/kommune',
-      }) as Content
-    )._id
-    pageTitle = `Kommunefakta ${municipality ? municipality.displayName : ''}`
-  } else if (req.path.indexOf('/kommuneareal/') > -1) {
-    targetId = (
-      get({
-        key: '/ssb/kommuneareal/kommune',
-      }) as Content
-    )._id
-    pageTitle = `Kommuneareal ${municipality ? municipality.displayName : ''}`
-  } else if (req.path.indexOf('/barn-og-unge/') > -1) {
-    targetId = (
-      get({
-        key: '/ssb/barn-og-unge/kommune',
-      }) as Content
-    )._id
-    pageTitle = `Barn og unge ${municipality ? municipality.displayName : ''}`
-  } else if (req.path.indexOf('/jakt-i-din-kommune/') > -1) {
-    targetId = (
-      get({
-        key: '/ssb/jakt-i-din-kommune/kommune',
-      }) as Content
-    )._id
-    pageTitle = `Jakt i din kommune ${municipality ? municipality.displayName : ''}`
-  }
+  const pageTitle = createPageTitle(req.path, municipality)
+  const targetId: string | null = getTargetId(req.path)
+  const municipalityUrl: string = createMunicipalityPath(req.url)
 
   if (!targetId) {
     return next(req)
   }
 
-  const targetUrl: string = pageUrl({
-    id: targetId,
-  })
-
-  const baseUrl: string = app.config && app.config['ssb.baseUrl'] ? app.config['ssb.baseUrl'] : 'https://www.ssb.no'
-
   const targetResponse: XP.Response = fromFilterCache(req, targetId, req.path, () => {
     const headers: Headers = req.headers
     const cookies: string | undefined = headers['Cookie']
     return request({
-      url: `${baseUrl}${targetUrl}`,
+      url: municipalityUrl,
       headers: {
         Cookie: cookies,
       },
@@ -82,16 +44,70 @@ exports.filter = function (req: XP.Request, next: (req: XP.Request) => XP.Respon
   })
 
   if (pageTitle) {
-    const site: Content = getSite()
     targetResponse.body = (targetResponse.body as string).replace(
       /(<title>)(.*?)(<\/title>)/i,
-      `<title>${pageTitle} - ${site.displayName}</title>`
+      `<title>${pageTitle}</title>`
     )
   }
 
   return {
     body: targetResponse.body,
   }
+}
+
+function createMunicipalityPath(path: string): string {
+  const municipalityName: string | undefined = path.split('/').pop()
+  const result: string | undefined =
+    municipalityName && municipalityName !== 'kommune' ? path.replace(`/${municipalityName}`, '/kommune') : path
+  return result
+}
+
+function createPageTitle(path: string, municipality: MunicipalityWithCounty | undefined): string {
+  let pageTitle = ''
+  if (path.indexOf('/kommunefakta/') > -1) {
+    pageTitle = `Kommunefakta ${municipality ? municipality.displayName : ''}`
+  } else if (path.indexOf('/kommuneareal/') > -1) {
+    pageTitle = `Kommuneareal ${municipality ? municipality.displayName : ''}`
+  } else if (path.indexOf('/barn-og-unge/') > -1) {
+    pageTitle = `Barn og unge ${municipality ? municipality.displayName : ''}`
+  } else if (path.indexOf('/jakt-i-din-kommune/') > -1) {
+    pageTitle = `Jakt i din kommune ${municipality ? municipality.displayName : ''}`
+  } else {
+    pageTitle = ''
+  }
+  return pageTitle
+}
+
+function getTargetId(path: string): string | null {
+  let targetId: string | null = null
+  if (path.indexOf('/kommunefakta/') > -1) {
+    targetId = (
+      get({
+        key: '/ssb/kommunefakta/kommune',
+      }) as Content
+    )._id
+  } else if (path.indexOf('/kommuneareal/') > -1) {
+    targetId = (
+      get({
+        key: '/ssb/kommuneareal/kommune',
+      }) as Content
+    )._id
+  } else if (path.indexOf('/barn-og-unge/') > -1) {
+    targetId = (
+      get({
+        key: '/ssb/barn-og-unge/kommune',
+      }) as Content
+    )._id
+  } else if (path.indexOf('/jakt-i-din-kommune/') > -1) {
+    targetId = (
+      get({
+        key: '/ssb/jakt-i-din-kommune/kommune',
+      }) as Content
+    )._id
+  } else {
+    targetId = null
+  }
+  return targetId
 }
 
 // XP.Request.headers type is set as readonly; we have to define our own type to delete the desired properties
