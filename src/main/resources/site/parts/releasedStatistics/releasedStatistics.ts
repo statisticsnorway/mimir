67d219c377622776993783ef2f1dccc53eaa0537
Rune Johansen <runejo@gmail.com>
2023-02-07 11:36:22 +0100

    Removing date-fns. WIP

--- src/main/resources/site/parts/releasedStatistics/releasedStatistics.ts
+++ src/main/resources/site/parts/releasedStatistics/releasedStatistics.ts
@@ -8,7 +8,7 @@ import { type Component, getComponent, getContent } from '/lib/xp/portal'
 import { localize } from '/lib/xp/i18n'
 import type { ContentLight, Release as ReleaseVariant } from '/lib/ssb/repo/statisticVariant'
 import { getStatisticVariantsFromRepo } from '/lib/ssb/repo/statisticVariant'
-import { getDate, getMonth, getYear, parseISO } from 'date-fns'
+import { parseISO } from 'date-fns'
 import { stringToServerTime } from '/lib/ssb/utils/dateUtils'
 
 const { fromPartCache } = __non_webpack_require__('/lib/ssb/cache/partCache')
@@ -88,6 +88,10 @@ function getGroupedWithMonthNames(
       : []
 
   const releasesPreppedNextReleaseToday: PreparedStatistics[] = nextReleaseToday.map((variant) => {
+    log.error('----------------------------')
+    log.error('parseISO: ' + parseISO(variant.data.nextRelease))
+    log.error('new Date: ' + new Date(variant.data.nextRelease))
+
     return prepReleases(variant, parseISO(variant.data.nextRelease), variant.data.nextPeriod)
   })
 
@@ -109,15 +113,23 @@ function prepReleases(
   date: Date,
   periodRelease: string | undefined
 ): PreparedStatistics {
+  // log.error('-------------------')
+  // log.error('getDate: ' + getDate(date))
+  // log.error('getMonth: ' + getMonth(date))
+  // log.error('getYear: ' + getYear(date))
+  // log.error('---')
+  // log.error('getDate: ' + date.getDate())
+  // log.error('getMonth: ' + date.getMonth())
+  // log.error('getYear: ' + date.getFullYear())
   return {
     id: Number(variant.data.statisticId),
     name: variant.data.name,
     shortName: variant.data.shortName,
     variant: {
       id: variant.data.variantId,
-      day: getDate(date),
-      monthNumber: getMonth(date),
-      year: getYear(date),
+      day: date.getDate(),
+      monthNumber: date.getMonth(),
+      year: date.getFullYear(),
       frequency: variant.data.frequency,
       period: periodRelease ? periodRelease.toLowerCase() : '',
     },
