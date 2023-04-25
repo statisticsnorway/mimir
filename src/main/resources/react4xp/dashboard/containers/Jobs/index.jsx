42f1251e8ef505677adb451bd064d159cd4f8628
johnnadeluy <johnnadel.salita@gmail.com>
2023-04-25 14:17:47 +0200

    Reuse update calculator job result parse and job log view for update sdds tables
    MIMIR-1686

--- src/main/resources/react4xp/dashboard/containers/Jobs/index.jsx
+++ src/main/resources/react4xp/dashboard/containers/Jobs/index.jsx
@@ -128,7 +128,7 @@ export function Jobs() {
       ) : (
         <span>{job.status}</span>
       )
-    } else if (job.task === 'Refresh dataset calculators') {
+    } else if (job.task === 'Refresh dataset calculators' || job.task === 'Refresh dataset for SDDS tables') {
       const skipped = job.result.result.filter((ds) => ds.status === 'Ingen nye data').length
       const updated = job.result.result.filter((ds) => ds.status === 'Dataset hentet og oppdatert').length
       const errorCount = job.result.result.filter((ds) => ds.hasError).length
@@ -139,7 +139,6 @@ export function Jobs() {
       ) : (
         <span>{job.status}</span>
       )
-    } else if (job.task === 'Refresh dataset for SDDS tables') {
     }
     return (
       <span>
@@ -215,7 +214,10 @@ export function Jobs() {
           </React.Fragment>
         )
       })
-    } else if (currentModalJob.task === 'Refresh dataset calculators') {
+    } else if (
+      currentModalJob.task === 'Refresh dataset calculators' ||
+      currentModalJob.task === 'Refresh dataset for SDDS tables'
+    ) {
       return currentModalJob.result.result.map((dataSource) => {
         return (
           <React.Fragment key={`refresh_dataset_log_${dataSource.id}`}>
