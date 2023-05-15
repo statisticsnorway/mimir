9086874e4752895cbc831a1d0cf6095b0b0d8eb4
Kyle Danny <codingkyle92@gmail.com>
2023-05-15 12:50:57 +0200

    fixed size of first image

--- src/main/resources/react4xp/_entries/PictureCardLinks.jsx
+++ src/main/resources/react4xp/_entries/PictureCardLinks.jsx
@@ -81,7 +81,9 @@ const PictureCardLinks = (props) => {
         return (
           <div
             key={`picture-card-link-${props.react4xpId}-${index}`}
-            className={getColSize(props.pictureCardLinks.length, index) + ' col-12 mb-4 mb-md-0'}
+            className={
+              getColSize(props.pictureCardLinks.length, index) + ' col-12 mb-4 mb-md-0 ' + `picture-card-link-${index}`
+            }
           >
             <PictureLink
               className='w-100'
