45320574acad8b1f724d5a7337dab90ce1a4726c
Carina Nordseth <cgn@ssb.no>
2023-06-02 16:09:01 +0200

    Remove logging

--- src/main/resources/site/filters/router.ts
+++ src/main/resources/site/filters/router.ts
@@ -22,7 +22,6 @@ exports.filter = function (req: XP.Request, next: (req: XP.Request) => XP.Respon
   const pageTitle = createPageTitle(req.path, municipality)
 
   if (paramKommune) {
-    log.info('Kommuneside via apache, kommune: ' + paramKommune + ' Request Url: ' + req.url)
     req.params = {
       selfRequest: 'true',
       municipality: JSON.stringify(municipality),
@@ -68,7 +67,6 @@ exports.filter = function (req: XP.Request, next: (req: XP.Request) => XP.Respon
     )
   }
 
-  log.info('Kommuneside via targetResponse, kommune: ' + region + ' Request Url: ' + req.url)
   return {
     body: targetResponse.body,
   }
