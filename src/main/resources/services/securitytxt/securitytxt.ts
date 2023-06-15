24e35acd9203d8017a23bd67ea5a8be51064b36e
omsaggau <omsaggau@gmail.com>
2023-06-15 10:23:21 +0200

    Remove policy and acknowledgements for now. Make issue in app in market about expire

--- src/main/resources/services/securitytxt/securitytxt.ts
+++ src/main/resources/services/securitytxt/securitytxt.ts
@@ -2,8 +2,6 @@ exports.get = () => {
   const body = `Contact: mailto:sikkerhetssenter@ssb.no
 Expires: 2023-11-31T06:00:00.000Z
 Preferred-Languages: nb,en
-Policy: We welcome any friendly bug report. Please contact us in advance if you wish to use methods that can trigger warnings or cause denial of service.
-Acknowledgments: Acknowledgements will be given publically for any higher severity bug reports, or we can respect your privacy. Tell us what you wish when reporting to us.
 Hiring: https://www.ssb.no/omssb/jobb-i-ssb`
   return {
     contentType: 'text/plain',
