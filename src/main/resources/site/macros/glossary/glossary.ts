e1b9045aa316ef0d07c82253905d840407e6dafd
johnnadeluy <johnnadel.salita@gmail.com>
2023-08-07 10:41:30 +0200

    Use React4xp object instead of render for wrapper and React4xp uniqueId
    MIMIR-1311

--- src/main/resources/site/macros/glossary/glossary.ts
+++ src/main/resources/site/macros/glossary/glossary.ts
@@ -1,4 +1,5 @@
-const { render } = __non_webpack_require__('/lib/enonic/react4xp')
+import { React4xp } from '/lib/enonic/react4xp'
+
 const { renderError } = __non_webpack_require__('/lib/ssb/error/error')
 
 exports.macro = function (context: XP.MacroContext) {
@@ -15,15 +16,24 @@ const renderMacro = (context: XP.MacroContext) => {
   if (!text) throw new Error('Missing param text')
   if (!explanation) throw new Error('Missing param explanation')
 
-  const props = {
-    text,
-    explanation,
-  }
+  const glossary = new React4xp('site/macros/glossary/glossary')
+    .setProps({
+      text,
+      explanation,
+    })
+    .uniqueId()
 
-  const react4xpId = `r4x-${(78364164096 + Math.floor(Math.random() * 2742745743360)).toString(36)}` // From lib-react4xp; used to generate uniqueId
-  return render('site/macros/glossary/glossary', props, context.request, {
-    id: react4xpId,
-    body: `<span id="${react4xpId}"></span>`,
-    ssr: context.request.mode === 'edit', // Component has to be clientside rendered so it doesn't get inserted twice
-  })
+  // Component has to be clientside rendered so it doesn't get inserted twice
+  const isInEditMode = context.request.mode === 'edit'
+  return {
+    body: glossary.renderBody({
+      body: `<span class="macro-glossary" id="${glossary.react4xpId}"></span>`,
+      request: context.request,
+      ssr: isInEditMode,
+    }),
+    pageContributions: glossary.renderPageContributions({
+      request: context.request,
+      ssr: isInEditMode,
+    }),
+  }
 }
