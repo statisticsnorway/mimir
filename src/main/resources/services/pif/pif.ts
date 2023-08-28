7076cf55cb797268945340c25686f021603accad
johnnadeluy <johnnadel.salita@gmail.com>
2023-08-28 13:38:25 +0200

    Change default code to 3 in Pif service to correctly represent data being shown in the radiogroup
    MIMIR-1553

--- src/main/resources/services/pif/pif.ts
+++ src/main/resources/services/pif/pif.ts
@@ -7,7 +7,7 @@ const { getCalculatorConfig, getPifDataset, isChronological, getChangeValue } =
   __non_webpack_require__('/lib/ssb/dataset/calculator')
 
 function get(req: HttpRequestParams): XP.Response {
-  const scopeCode: string | undefined = req.params?.scopeCode || '2'
+  const scopeCode: string | undefined = req.params?.scopeCode || '3'
   const productGroup: string | undefined = req.params?.productGroup || 'SITCT'
   const startValue: string | undefined = req.params?.startValue
   const startMonth: string | undefined = req.params?.startMonth || ''
