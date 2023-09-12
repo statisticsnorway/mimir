6406eb56c8afd1c3d8117faf8f6fcf56459ba360
Carina <cgn@ssb.no>
2023-09-12 15:15:25 +0200

    MIMIR-854:Still testing xml

--- src/main/resources/lib/ssb/rss/statkal.ts
+++ src/main/resources/lib/ssb/rss/statkal.ts
@@ -1,5 +1,6 @@
 import type { ContentLight, Release as ReleaseVariant } from '/lib/ssb/repo/statisticVariant'
 import { getUpcompingStatisticVariantsFromRepo } from '/lib/ssb/repo/statisticVariant'
+import { ReleasesInListing } from '/lib/ssb/dashboard/statreg/types'
 const { xmlEscape } = __non_webpack_require__('/lib/text-encoding')
 const {
   data: { forceArray },
@@ -7,27 +8,67 @@ const {
 
 export function getStatisticCalendarRss(): string {
   const allUpcomingReleases: ContentLight<ReleaseVariant>[] = getUpcompingStatisticVariantsFromRepo()
+  const allReleases: StatkalRelease[] = upcomingReleasesTotal(allUpcomingReleases)
+  //log.info(JSON.stringify(upcomingReleasesTotal(allUpcomingReleases)))
 
   const xml = `<?xml version="1.0" encoding="utf-8"?>
-	<rssitems count="${allUpcomingReleases.length}">
-	  ${[...allUpcomingReleases]
+	<rssitems count="${allReleases.length}">
+	  ${[...allReleases]
       .map(
-        (r: ContentLight<ReleaseVariant>) => `<rssitem>
-		<guid isPermalink="false">statkal-${r.data.variantId}</guid>
-		<title>${xmlEscape(r.data.name)}</title>
+        (r: StatkalRelease) => `<rssitem>
+		<guid isPermalink="false">statkal-${r.guid}</guid>
+		<title>${xmlEscape(r.title)}</title>
 		<link>www.ssb.no</link>
-		<description>${r.data.ingress ? xmlEscape(r.data.ingress) : ''}</description>
-		<category>${r.data.mainSubjects}</category>
-		<subject>${r.data.mainSubjects}</subject>
+		<description>${r.description ? xmlEscape(r.description) : ''}</description>
+		<category>${r.category}</category>
+		<subject>${r.subject}</subject>
 		<language>${r.language}</language>
-		${forceArray(r.data.contacts)
+		${forceArray(r.contacts)
       .map((c) => `<contact>${c}</contact>`)
       .join('')}
-		<pubDate>${r.data.nextRelease}</pubDate>
-		<shortname>${r.data.shortName}</shortname>
+		<pubDate>${r.pubDate}</pubDate>
+		<shortname>${r.shortname}</shortname>
 	  </rssitem>`
       )
       .join('')}
 	</rssitems>`
   return xml
+}
+
+function upcomingReleasesTotal(statisticVariants: ContentLight<ReleaseVariant>[]): StatkalRelease[] {
+  const ferdigListe: StatkalRelease[] = []
+  statisticVariants.forEach((statisticVariant) => {
+    const upcomingReleases: ReleasesInListing[] = statisticVariant.data.upcomingReleases
+      ? forceArray(statisticVariant.data.upcomingReleases)
+      : []
+
+    upcomingReleases.forEach((r) => {
+      ferdigListe.push({
+        guid: r.id,
+        title: statisticVariant.data.name,
+        link: '',
+        description: statisticVariant.data.ingress ?? '',
+        category: 'Test',
+        subject: 'Test',
+        language: statisticVariant.language,
+        pubDate: r.publishTime,
+        shortname: statisticVariant.data.shortName,
+        contacts: statisticVariant.data.contacts,
+      })
+    })
+  })
+  return ferdigListe
+}
+
+interface StatkalRelease {
+  guid: string
+  title: string
+  link: string
+  description: string
+  category: string
+  subject: string
+  language: string
+  pubDate: string
+  shortname: string
+  contacts: string[]
 }
