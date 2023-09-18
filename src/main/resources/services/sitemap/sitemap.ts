b735a172d25e9c70bc9a1c7cf6d28dea8607ad94
johnnadeluy <johnnadel.salita@gmail.com>
2023-09-18 15:05:38 +0200

    Fetch sitemap xml app config with query
    MIMIR-1854

--- src/main/resources/services/sitemap/sitemap.ts
+++ src/main/resources/services/sitemap/sitemap.ts
@@ -1,3 +1,42 @@
+import { query } from '/lib/xp/content'
+// import { attachmentUrl } from '/lib/xp/portal'
+
 exports.get = () => {
-  return {}
+  const sitemapXmlAppConfigQuery = query({
+    start: 0,
+    count: 2,
+    filters: {
+      boolean: {
+        must: [
+          {
+            hasValue: {
+              field: 'data.siteConfig.applicationKey',
+              values: ['com.enonic.app.sitemapxml'],
+            },
+          },
+        ],
+      },
+    },
+  }).hits[0] as unknown as SitemapXmlAppConfig
+
+  const sitemapXmlAppConfig =
+    sitemapXmlAppConfigQuery.data.siteConfig.find((config) => config.applicationKey === 'com.enonic.app.sitemapxml')
+      ?.config ?? {}
+
+  /* TODO: Generate new sitemap xml including links in sitemap xml app config
+   * Fetch sitemap xml from sitemap xml app from repo?
+   * Merge sitemap.xml from sitemap xml app with xml generated from service to sitemapindex.xml
+   * Return result to body
+   */
+
+  return {
+    contentType: 'text/json',
+    body: JSON.stringify(sitemapXmlAppConfig, null, 2),
+  }
+}
+
+type SitemapXmlAppConfig = {
+  data: {
+    siteConfig: [{ applicationKey: string; config: object }]
+  }
 }
