a9c1894438acf938adace2bc52340e5c6d6b15fa
johnnadeluy <johnnadel.salita@gmail.com>
2023-10-24 13:03:00 +0200

    Styling adjustments for dashboard connection timeout modal
    MIMIR-1910

--- src/main/resources/react4xp/dashboard/components/ConnectionTimeoutModal.jsx
+++ src/main/resources/react4xp/dashboard/components/ConnectionTimeoutModal.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal } from 'react-bootstrap'
+import { AlertTriangle } from 'react-feather'
 import PropTypes from 'prop-types'
 
 function ConnectionTimeoutModal(props) {
   const { isConnected, serverTimeReceived } = props
+  const [hide, setHide] = useState(false)
 
   // To prevent modal from opening during page render/reload, include serverTimeReceived check
   if (serverTimeReceived) {
     return (
-      <Modal size='lg' show={!isConnected} onHide={isConnected}>
-        <Modal.Body>Tilkobling avbrutt. Last siden på nytt.</Modal.Body>
+      <Modal
+        className='connection-timeout-modal'
+        size='lg'
+        show={!isConnected && !hide}
+        onHide={() => setHide(true)}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <AlertTriangle size={60} />
+            Tilkobling avbrutt.
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Last inn siden på nytt.</p>
+        </Modal.Body>
       </Modal>
     )
   }
