89cc69715f1683bed1a1e065c72a041fe716ee72
Carina <47381367+ssb-cgn@users.noreply.github.com>
2023-11-15 11:16:32 +0100

    [MIM-746] Mim 746 tbml preview (#2255)
    * First version tmbl preview
    
    * MIM-746:Added language
    
    * MIM-746:Added title and som refactoring
    
    * MIM-746: Added errormessage if statuscode not 200
    
    * MIM-746: change to same param name as old preview
    
    * Teste endring nonic ci for deploy utv
    
    * MIM:746:Refactoring after QA
    
    * remove logging

---
+++ src/main/resources/site/parts/tbmlPreview/tbmlPreview.ts
@@ -0,0 +1,67 @@
+import { type TableView, getTableViewData } from '/lib/ssb/parts/table'
+import { type TbprocessorParsedResponse, getTbmlData } from '/lib/ssb/dataset/tbprocessor/tbml'
+import { type TbmlDataUniform } from '/lib/types/xmlParser'
+import { render } from '/lib/thymeleaf'
+import { render as r4xpRender } from '/lib/enonic/react4xp'
+import { renderError } from '/lib/ssb/error/error'
+
+const view = resolve('./tbmlPreview.html')
+
+export function get(req: XP.Request): XP.Response {
+  try {
+    return renderPart(req)
+  } catch (e) {
+    return renderError(req, 'Error in part', e)
+  }
+}
+
+function renderPart(req: XP.Request): XP.Response {
+  const tbmlId = req.params.tbmlid
+  const language = req.params.sprak ?? 'no'
+  const baseUrl: string = app.config?.['ssb.tbprocessor.baseUrl'] || 'https://i.ssb.no/tbprocessor'
+  const tbProceessorUrl = `${baseUrl}/process/tbmldata/${tbmlId}?lang=${language}`
+  const tbmlData: TbprocessorParsedResponse<TbmlDataUniform> | null = tbmlId
+    ? (getTbmlData(tbProceessorUrl) as TbprocessorParsedResponse<TbmlDataUniform>)
+    : null
+
+  const id = 'tbml-previewId'
+  const body: string = render(view, {
+    tbmlPreviewId: id,
+    title: `Forhåndsvisning av TBML ID: ${tbmlId}`,
+    subTitle: `Språkversjon: ${language}`,
+    errorMessage:
+      tbmlData?.status !== 200
+        ? `Henting av tabell med tbmlId ${tbmlId} feilet, statuskode: ${tbmlData?.status} `
+        : null,
+  })
+
+  if (!tbmlData?.parsedBody) {
+    return {
+      body,
+    }
+  }
+
+  const table: TableView = getTableViewData(tbmlData.parsedBody)
+  const props: TableProps = {
+    displayName: `Forhåndsvisning TBML ${tbmlId}`,
+    table: {
+      caption: table.caption,
+      thead: table.thead,
+      tbody: table.tbody,
+      tfoot: table.tfoot,
+      tableClass: table.tableClass,
+      language: language === 'en' ? 'en' : 'no',
+      noteRefs: table.noteRefs,
+    },
+  }
+
+  return r4xpRender('Table', props, req, {
+    id,
+    body: body,
+  })
+}
+
+interface TableProps {
+  displayName: string
+  table: object
+}
