aebd97154843afec88ca1b621c50807ea98fa1a2
Carl-OW <142233642+Carl-OW@users.noreply.github.com>
2023-11-21 16:12:11 +0100

    fixes (text-align)

--- src/main/resources/site/parts/RichText/richText.jsx
+++ src/main/resources/site/parts/RichText/richText.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import '@statisticsnorway/ssb-component-library'
 
 const RichText = ({ text, textType }) => {
-  const processedText = text.replace(/<(\/*)p>/gm, '<$1span>')
-  const textElement = <div dangerouslySetInnerHTML={{ __html: processedText }} />
+  const divStyle = { maxWidth: 'none' }
+  const processedText = text.replace(
+    /<p(\sstyle="text-align:\s*(center|right|left|justify);?")?>(.*?)<\/p>/g,
+    (match, styleAttr, textAlign, innerHtml) => {
+      if (textAlign) {
+        divStyle.textAlign = textAlign
+      }
+      return `<span>${innerHtml}</span>`
+    }
+  )
+
+  const textElement = <div style={divStyle} dangerouslySetInnerHTML={{ __html: processedText }} />
 
   switch (textType) {
     case 'ingress':
