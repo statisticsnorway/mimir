709fb4ed1a4f9f42fdfe119ca8964716fc1794ac
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2023-11-22 08:17:43 +0100

    [MIM-1536] Mim 1536 update disconnected modal description dashboard (#2262)
    * Update connection timeout modal text and icons
    
    MIM-1536
    
    * Move text to button in modal body
    
    MIM-1536
    
    * Minor code refactoring; utilize reload function correctly
    
    MIM-1536
    
    * Remove unused space
    
    MIM-1536

--- src/main/resources/react4xp/dashboard/components/ConnectionTimeoutModal.jsx
+++ src/main/resources/react4xp/dashboard/components/ConnectionTimeoutModal.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import { Modal } from 'react-bootstrap'
-import { AlertTriangle } from 'react-feather'
+import { RefreshCw, ZapOff } from 'react-feather'
+import { Button } from '@statisticsnorway/ssb-component-library'
 import PropTypes from 'prop-types'
 
 function ConnectionTimeoutModal(props) {
   const { isConnected } = props
   const [hide, setHide] = useState(false)
+
+  function handlePageRefresh() {
+    window.location.reload()
+  }
 
   if (isConnected !== undefined && !isConnected) {
     return (
@@ -20,12 +25,14 @@ function ConnectionTimeoutModal(props) {
       >
         <Modal.Header closeButton>
           <Modal.Title>
-            <AlertTriangle size={60} />
-            Tilkobling avbrutt.
+            <ZapOff size={40} />
+            Siden er frakoblet
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>Last inn siden på nytt.</p>
+          <Button primary onClick={handlePageRefresh}>
+            <RefreshCw size={22} className='pe-2' /> Last inn siden på nytt
+          </Button>
         </Modal.Body>
       </Modal>
     )
