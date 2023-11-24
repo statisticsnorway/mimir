3ae0883afa091075da091946e0ad3de53deaece7
Carl-OW <142233642+Carl-OW@users.noreply.github.com>
2023-11-24 09:49:01 +0100

    MIM-1429 implemetere part for tekst (#2259)
    * first commit
    
    * code update
    
    * seems to work
    
    * commit (all textTypes renders correct)
    
    * added placeholder
    
    * Rename RichText.ts to richText.ts
    
    * Rename RichText.xml to richText.xml
    
    * Rename RichText.jsx to richText.jsx
    
    * fixes
    
    * fixes (text-align)
    
    * Switch to components from lib
    
    * Turn off hydration for part
    
    ---------
    
    Co-authored-by: omsaggau <omsaggau@gmail.com>

---
+++ src/main/resources/site/parts/RichText/richText.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { LeadParagraph, Text, Paragraph } from '@statisticsnorway/ssb-component-library'
+
+const RichText = ({ text, textType }) => {
+  const renderText = () => {
+    const textComponent = <span dangerouslySetInnerHTML={{ __html: text }} />
+    switch (textType) {
+      case 'ingress':
+        return <LeadParagraph>{textComponent}</LeadParagraph>
+      case 'brodtekst':
+        return <Paragraph>{textComponent}</Paragraph>
+      case 'mikrotekst':
+        return <Text small>{textComponent}</Text>
+      default:
+        return <p>{textComponent}</p>
+    }
+  }
+
+  return <div className='xp-part rich-text'>{renderText()}</div>
+}
+
+RichText.propTypes = {
+  text: PropTypes.string.isRequired,
+  textType: PropTypes.string.isRequired,
+}
+
+export default (props) => <RichText {...props} />
