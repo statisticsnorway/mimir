3bc3f0bca01e54a24b80b74e852ef082d96c3d9e
omsaggau <omsaggau@gmail.com>
2023-11-27 14:24:40 +0100

    Unify max width of all types to the max readable width

--- src/main/resources/site/parts/richText/richText.jsx
+++ src/main/resources/site/parts/richText/richText.jsx
@@ -17,7 +17,7 @@ const RichText = ({ text, textType }) => {
     }
   }
 
-  return <div className='xp-part rich-text'>{renderText()}</div>
+  return <div className='xp-part rich-text container'>{renderText()}</div>
 }
 
 RichText.propTypes = {
