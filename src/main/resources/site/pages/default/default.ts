ed3af609f3f4767974f8e046d34b4c3357479ea9
Glenruben <gle@ssb.no>
2023-12-15 12:09:50 +0100

    Hotfix fragment not working (#2354)
    * Create action for deploy to prod on push to master (#2141)
    
    * Create action for deploy to prod on push to master
    
    * Fix deploy to prod job
    
    * Change action env
    
    * Need to do more explicit test, fails if hideHeader
    is undefined
    
    Logical conjunction tries to evaluate possibly undefined key hideHeader
    Using ternary operator to test if feature flag is enabled first.
    
    * Added optional chaining to assignment
    
    ---------
    
    Co-authored-by: omsaggau <omsaggau@gmail.com>
    Co-authored-by: Anne-Siri <142512638+annesiri@users.noreply.github.com>

--- src/main/resources/site/pages/default/default.ts
+++ src/main/resources/site/pages/default/default.ts
@@ -130,7 +130,7 @@ export function get(req: XP.Request): XP.Response {
   }
   const language: Language = getLanguage(page) as Language
   const menuCacheLanguage: string = language.code === 'en' ? 'en' : 'nb'
-  const hideHeader = pageConfig.hideHeader && isEnabled('hide-header-in-qa', true, 'ssb')
+  const hideHeader = isEnabled('hide-header-in-qa', false, 'ssb') ? pageConfig?.hideHeader : false
   let header
   if (!hideHeader) {
     const headerContent: MenuContent | unknown = fromMenuCache(req, `header_${menuCacheLanguage}`, () => {
