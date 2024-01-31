63a5eb1a788fade247aa03fc9acb662605da10c9
Anne-Siri Trønnes <ntr@ssb.no>
2024-01-31 12:53:34 +0100

    directly exporting functions

--- src/main/resources/lib/vendor/dateFns.ts
+++ src/main/resources/lib/vendor/dateFns.ts
@@ -1,37 +1,19 @@
 import { type Locale } from 'date-fns'
-import { default as nb } from 'date-fns/locale/nb'
-import { default as nn } from 'date-fns/locale/nn'
-import { default as enGB } from 'date-fns/locale/en-GB'
-import { default as parseISO } from 'date-fns/parseISO'
-import { default as format } from 'date-fns/format'
-import { default as isWithinInterval } from 'date-fns/isWithinInterval'
-import { default as addDays } from 'date-fns/addDays'
-import { default as subDays } from 'date-fns/subDays'
-import { default as getMonth } from 'date-fns/getMonth'
-import { default as getYear } from 'date-fns/getYear'
-import { default as getDate } from 'date-fns/getDate'
-import { default as isAfter } from 'date-fns/isAfter'
-import { default as isBefore } from 'date-fns/isBefore'
-import { default as isSameDay } from 'date-fns/isSameDay'
-import { default as isSameSecond } from 'date-fns/isSameSecond'
-import { default as formatDistanceToNowStrict } from 'date-fns/formatDistanceToNowStrict'
 
-export {
-  Locale,
-  nb,
-  nn,
-  enGB,
-  parseISO,
-  addDays,
-  subDays,
-  getMonth,
-  format,
-  getYear,
-  getDate,
-  isAfter,
-  isBefore,
-  isSameDay,
-  isSameSecond,
-  isWithinInterval,
-  formatDistanceToNowStrict,
-}
+export { Locale }
+export { default as nb } from 'date-fns/locale/nb'
+export { default as nn } from 'date-fns/locale/nn'
+export { default as enGB } from 'date-fns/locale/en-GB'
+export { default as parseISO } from 'date-fns/parseISO'
+export { default as format } from 'date-fns/format'
+export { default as isWithinInterval } from 'date-fns/isWithinInterval'
+export { default as addDays } from 'date-fns/addDays'
+export { default as subDays } from 'date-fns/subDays'
+export { default as getMonth } from 'date-fns/getMonth'
+export { default as getYear } from 'date-fns/getYear'
+export { default as getDate } from 'date-fns/getDate'
+export { default as isAfter } from 'date-fns/isAfter'
+export { default as isBefore } from 'date-fns/isBefore'
+export { default as isSameDay } from 'date-fns/isSameDay'
+export { default as isSameSecond } from 'date-fns/isSameSecond'
+export { default as formatDistanceToNowStrict } from 'date-fns/formatDistanceToNowStrict'
