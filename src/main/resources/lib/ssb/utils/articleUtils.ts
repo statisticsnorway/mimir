fae34758770208570bc3901596c03ee54b03285b
Anne-Siri Trønnes <ntr@ssb.no>
2024-02-16 14:18:01 +0100

    improved parentPath

--- src/main/resources/lib/ssb/utils/articleUtils.ts
+++ src/main/resources/lib/ssb/utils/articleUtils.ts
@@ -57,9 +57,9 @@ export function getAllArticles(req: XP.Request, language: string, start: 0, coun
   const now: string = new Date().toISOString()
   const publishFromQuery = `(publish.from LIKE '*' AND publish.from < '${now}')`
   const pagePaths: Array<string> = mainSubjects.map((mainSubject) => `_parentPath LIKE "/content${mainSubject.path}/*"`)
-  pagePaths.push(`_parentPath LIKE "/content/ssb/innrapportering*"`)
-  pagePaths.push(`_parentPath LIKE "/content/ssb/omssb*"`)
-  pagePaths.push(`_parentPath LIKE "/content/ssb/forskning*"`)
+  pagePaths.push(`_parentPath LIKE "/content/ssb/innrapportering/*"`)
+  pagePaths.push(`_parentPath LIKE "/content/ssb/omssb/*"`)
+  pagePaths.push(`_parentPath LIKE "/content/ssb/forskning/*"`)
   const subjectQuery = `(${pagePaths.join(' OR ')})`
   const queryString = `${publishFromQuery} AND ${subjectQuery} ${languageQuery}`
 
