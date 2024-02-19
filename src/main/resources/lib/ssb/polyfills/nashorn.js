c5d20a4315171bc3d6ef6479da2789bd927a7f81
omsaggau <omsaggau@gmail.com>
2024-02-19 11:35:47 +0100

    Add Math.trunc polyfill

--- src/main/resources/lib/ssb/polyfills/nashorn.js
+++ src/main/resources/lib/ssb/polyfills/nashorn.js
@@ -224,3 +224,13 @@ if (!Object.assign) {
     return to;
   };
 }
+
+/** Math **************************************************************/
+
+// Math.trunc
+
+if (!Math.trunc) {
+  Math.trunc = function trunc(x) {
+    return x < 0 ? Math.ceil(x) : Math.floor(x);
+  };
+}
