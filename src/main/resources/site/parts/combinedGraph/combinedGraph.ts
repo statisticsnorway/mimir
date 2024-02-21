0c080e8f03e2ee4790887d86bbac9f51010b73d8
Carina <cgn@ssb.no>
2024-02-21 14:56:27 +0100

    MIM-1727:Ryddet i kode

--- src/main/resources/site/parts/combinedGraph/combinedGraph.ts
+++ src/main/resources/site/parts/combinedGraph/combinedGraph.ts
@@ -1,19 +1,12 @@
-// @ts-ignore
 import { get as getContentByKey, type Content } from '/lib/xp/content'
 import { getComponent, getContent } from '/lib/xp/portal'
 import { localize } from '/lib/xp/i18n'
 import { type HighchartsGraphConfig } from '/lib/types/highcharts'
 import { render } from '/lib/thymeleaf'
-import { render as r4XpRender } from '/lib/enonic/react4xp'
 import { scriptAsset } from '/lib/ssb/utils/utils'
-
-import * as util from '/lib/util'
+import { forceArray } from '/lib/ssb/utils/arrayUtils'
 import { createCombinedGraphObject } from '/lib/ssb/parts/highcharts/highchartsUtils'
 import { renderError } from '/lib/ssb/error/error'
-import { isEnabled } from '/lib/featureToggle'
-import { getPhrases } from '/lib/ssb/utils/language'
-import { GA_TRACKING_ID } from '/site/pages/default/default'
-import { type DataSource } from '/site/mixins/dataSource'
 import { type CombinedGraph } from '/site/content-types'
 
 const view = resolve('./combinedGraph.html')
@@ -23,7 +16,7 @@ export function get(req: XP.Request): XP.Response {
     const part = getComponent<XP.PartComponent.CombinedGraph>()
     if (!part) throw Error('No part found')
 
-    const highchartIds: Array<string> = part.config.combinedGraph ? util.data.forceArray(part.config.combinedGraph) : []
+    const highchartIds: Array<string> = part.config.combinedGraph ? forceArray(part.config.combinedGraph) : []
     return renderPart(req, highchartIds)
   } catch (e) {
     return renderError(req, 'Error in part', e)
@@ -65,13 +58,13 @@ function renderPart(req: XP.Request, highchartIds: Array<string>): XP.Response {
     locale: language === 'nb' ? 'no' : language,
   })
 
-  const highcharts: Array<HighchartsReactProps> = highchartIds
+  const highcharts: Array<HighchartsProps> = highchartIds
     .map((key) => {
-      const highchart: Content<CombinedGraph & DataSource> | null = getContentByKey({
+      const highchart: Content<CombinedGraph> | null = getContentByKey({
         key,
       })
-      const config: HighchartsExtendedProps | undefined = highchart ? determinConfigType(req, highchart) : undefined
-      return highchart && config ? createHighchartsReactProps(highchart, config) : {}
+      const config: HighchartsGraphConfig | undefined = highchart ? determinConfigType(req, highchart) : undefined
+      return highchart && config ? createHighchartsProps(highchart, config) : {}
     })
     .filter((key) => !!key)
 
@@ -81,88 +74,50 @@ function renderPart(req: XP.Request, highchartIds: Array<string>): XP.Response {
    </script>`
   )
 
-  const HighchartProps: object = {
-    highcharts,
-    phrases: getPhrases(page),
-    appName: app.name,
-    pageType: page.type,
-    GA_TRACKING_ID: GA_TRACKING_ID,
-  }
-
-  if (isEnabled('highchart-react', true, 'ssb')) {
-    // R4xp disables hydration in edit mode, but highcharts need hydration to show
-    // we sneaky swap mode since we want a render of higchart in edit mode
-    const _req = req
-    if (req.mode === 'edit') _req.mode = 'preview'
-
-    return r4XpRender('site/parts/highchart/Highchart', HighchartProps, _req, {
-      body: '<section class="xp-part highchart-wrapper"></section>',
-    })
-  } else {
-    return {
-      body: render(view, {
-        highcharts,
-        downloadText,
-        sourceText,
-        showDataTableEnabled: isEnabled('highchart-show-datatable', false, 'ssb'),
-        showAsGraphText,
-        showAsTableText,
-      }),
-      pageContributions: {
-        bodyEnd: [...inlineScript, scriptAsset('js/highchart.js')],
-      },
-      contentType: 'text/html',
-    }
+  return {
+    body: render(view, {
+      highcharts,
+      downloadText,
+      sourceText,
+      showAsGraphText,
+      showAsTableText,
+    }),
+    pageContributions: {
+      bodyEnd: [...inlineScript, scriptAsset('js/highchart.js')],
+    },
+    contentType: 'text/html',
   }
 }
 
-function determinConfigType(
-  req: XP.Request,
-  highchart: Content<CombinedGraph & DataSource>
-): HighchartsExtendedProps | undefined {
+function determinConfigType(req: XP.Request, highchart: Content<CombinedGraph>): HighchartsGraphConfig | undefined {
   if (highchart.data.dataSource?._selected === 'htmlTable') {
     return createDataFromHtmlTable(req, highchart)
   }
   return undefined
 }
 
-function createDataFromHtmlTable(
-  req: XP.Request,
-  highchart: Content<CombinedGraph & DataSource>
-): HighchartsExtendedProps {
+function createDataFromHtmlTable(req: XP.Request, highchart: Content<CombinedGraph>): HighchartsGraphConfig {
   log.info('\x1b[32m%s\x1b[0m', '2. createDataFromHtmlTable')
   return {
     ...createCombinedGraphObject(req, highchart, highchart.data, highchart.data.dataSource ?? undefined),
   }
 }
 
-function createHighchartsReactProps(
-  highchart: Content<CombinedGraph>,
-  config: HighchartsExtendedProps
-): HighchartsReactProps {
+function createHighchartsProps(highchart: Content<CombinedGraph>, config: HighchartsGraphConfig): HighchartsProps {
   return {
     config: config,
     contentKey: highchart._id,
-    footnoteText: highchart.data.footnoteText ? util.data.forceArray(highchart.data.footnoteText) : undefined,
+    footnoteText: highchart.data.footnoteText ? forceArray(highchart.data.footnoteText) : undefined,
     creditsEnabled: highchart.data.sourceList ? true : false,
-    sourceList: highchart.data.sourceList ? util.data.forceArray(highchart.data.sourceList) : undefined,
-    hideTitle: highchart.data.hideTitle,
+    sourceList: highchart.data.sourceList ? forceArray(highchart.data.sourceList) : undefined,
   }
 }
-type HighchartsExtendedProps = HighchartsGraphConfig & HighchartsReactExtraProps
 
-interface HighchartsReactProps {
-  config?: HighchartsExtendedProps
+interface HighchartsProps {
+  config?: HighchartsGraphConfig
   description?: string
-  //type?: string
   contentKey?: string
   footnoteText?: string[]
   creditsEnabled?: boolean
   sourceList?: CombinedGraph['sourceList']
-  hideTitle?: boolean
-}
-
-interface HighchartsReactExtraProps {
-  draft?: boolean
-  noDraftAvailable?: boolean
 }
