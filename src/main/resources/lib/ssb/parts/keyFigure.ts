43182d9092f7d0323f725a0c25dd9dc74f7f21fd
Carina <47381367+ssb-cgn@users.noreply.github.com>
2024-05-22 11:10:10 +0200

    [MIM-1660] MIM-1660:Kun hente tall fra gammelt kommunenr hvis nytt ikke har data (#2751)
    MIM-1660:Kun hente tall fra gammelt kommunenr hvis nytt ikke har data

--- src/main/resources/lib/ssb/parts/keyFigure.ts
+++ src/main/resources/lib/ssb/parts/keyFigure.ts
@@ -215,11 +215,7 @@ function getDataWithFilterStatbankApi(
       )
 
       // not all municipals have data, so if its missing, try the old one
-      if (
-        (!municipalData || municipalData.value === null || municipalData.value === 0) &&
-        municipality.changes &&
-        municipality.changes.length > 0
-      ) {
+      if (!municipalData && municipality.changes && municipality.changes.length > 0) {
         municipalData = getDataFromMunicipalityCode(ds, municipality.changes[0].oldCode, yAxisLabel, filterTarget)
       }
       if (municipalData && municipalData.value !== null) {
