ae44ae3e9bcbca973b3ddde50a4287baa70edc85
Anne-Siri Trønnes <ntr@ssb.no>
2024-05-29 14:07:13 +0200

    fixed formatting

--- src/main/resources/site/macros/keyFigure/keyFigure.ts
+++ src/main/resources/site/macros/keyFigure/keyFigure.ts
@@ -9,7 +9,7 @@ export function macro(context: XP.MacroContext<KeyFigureConfig>): XP.Response {
     const keyFigure: XP.Response = preview(context.request, config.keyFigure)
 
     if (keyFigure.status && keyFigure.status !== 200) throw new Error(`keyFigure with id ${config.keyFigure} missing`)
-    keyFigure.body = '<div class = "macro-keyfigure">' + keyFigure.body + '</div>'
+    keyFigure.body = `<div class="macro-keyfigure">${keyFigure.body}</div>`
 
     return keyFigure
   } catch (e) {
