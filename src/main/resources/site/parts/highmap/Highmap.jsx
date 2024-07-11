45997a9e87c64d2881da1aed92501e96368f9306
johnnadeluy <johnnadel.salita@gmail.com>
2024-07-11 12:50:46 +0200

    Remove unused import of useEffect hook in highmap

--- src/main/resources/site/parts/highmap/Highmap.jsx
+++ src/main/resources/site/parts/highmap/Highmap.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import PropTypes from 'prop-types'
