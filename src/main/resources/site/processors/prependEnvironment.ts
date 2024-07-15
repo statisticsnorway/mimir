0a7a0077aaa2175729807c19169a8bc3e894428b
johnnadeluy <johnnadel.salita@gmail.com>
2024-07-15 13:06:25 +0200

    Add functionality to prepend environment into title tag in head (wip)

--- src/main/resources/site/processors/prependEnvironment.ts
+++ src/main/resources/site/processors/prependEnvironment.ts
@@ -1,7 +1,18 @@
 import { XP_RUN_MODE } from '/lib/ssb/utils/utils'
 
 exports.responseProcessor = (req: XP.Request, res: XP.Response) => {
-  log.info(XP_RUN_MODE)
+  if (XP_RUN_MODE === 'PROD') return res
 
+  // Prepend environment to title tag in head for DEV, UTV, TEST, and QA
+  const headRegex = /<head[^>]*>([\s\S]*?)<\/head>/i
+  const titleRegex = /<title[^>]*>([^<]*)<\/title>/i
+
+  // TODO: XP_RUN_MODE only returns DEV, TEST or PROD
+  res.body = (res.body as string).replace(headRegex, (match, headContent) => {
+    const modifiedHeadContent = headContent.replace(titleRegex, (match: string, titleText: string) => {
+      return match.replace(titleText, `${XP_RUN_MODE}: ${titleText}`)
+    })
+    return match.replace(headContent, modifiedHeadContent)
+  })
   return res
 }
