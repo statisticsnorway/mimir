fd690bc47e797f476251a2cba1ee1eb7e8a89f06
johnnadeluy <johnnadel.salita@gmail.com>
2024-07-15 13:24:53 +0200

    Minor adjustments to header and title regex

--- src/main/resources/site/processors/prependEnvironment.ts
+++ src/main/resources/site/processors/prependEnvironment.ts
@@ -4,8 +4,8 @@ exports.responseProcessor = (req: XP.Request, res: XP.Response) => {
   if (XP_RUN_MODE === 'PROD') return res
 
   // Prepend environment to title tag in head for DEV, UTV, TEST, and QA
-  const headRegex = /<head[^>]*>([\s\S]*?)<\/head>/i
-  const titleRegex = /<title[^>]*>([^<]*)<\/title>/i
+  const headRegex = /<head>([\s\S]*?)<\/head>/
+  const titleRegex = /<title>(.*?)<\/title>/
 
   // TODO: XP_RUN_MODE only returns DEV, TEST or PROD
   res.body = (res.body as string).replace(headRegex, (match, headContent) => {
