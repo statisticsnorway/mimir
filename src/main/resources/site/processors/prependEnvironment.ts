4a1f7abd421079607ce0e8e841abf58f42f861d4
johnnadeluy <johnnadel.salita@gmail.com>
2024-07-16 09:13:25 +0200

    Add logging for testing (in test)

--- src/main/resources/site/processors/prependEnvironment.ts
+++ src/main/resources/site/processors/prependEnvironment.ts
@@ -1,13 +1,15 @@
 import { XP_RUN_MODE } from '/lib/ssb/utils/utils'
 
 exports.responseProcessor = (req: XP.Request, res: XP.Response) => {
-  if (XP_RUN_MODE === 'PROD') return res
+  log.info(`in ${XP_RUN_MODE} mode`)
+  if (XP_RUN_MODE === 'PROD') return res // TODO: It's possible UTV, TEST, and QA have RunMode PROD
 
   // Prepend environment to title tag in head for DEV, UTV, TEST, and QA
   const headRegex = /<head>([\s\S]*?)<\/head>/
   const titleRegex = /<title>(.*?)<\/title>/
 
-  // TODO: XP_RUN_MODE only returns DEV, TEST or PROD
+  // TODO: XP_RUN_MODE only returns DEV or PROD. Consider using baseUrl?
+  // const baseUrl = app.config && app.config['ssb.baseUrl']
   res.body = (res.body as string).replace(headRegex, (match, headContent) => {
     const modifiedHeadContent = headContent.replace(titleRegex, (match: string, titleText: string) => {
       return match.replace(titleText, `${XP_RUN_MODE}: ${titleText}`)
