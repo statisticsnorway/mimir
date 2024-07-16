6ad99bc3e4ca8b939ab7da39eb06e9ba8aa1f838
johnnadeluy <johnnadel.salita@gmail.com>
2024-07-16 09:38:39 +0200

    Fetch environment from baseUrl in app config

--- src/main/resources/site/processors/prependEnvironment.ts
+++ src/main/resources/site/processors/prependEnvironment.ts
@@ -1,20 +1,29 @@
 import { XP_RUN_MODE } from '/lib/ssb/utils/utils'
 
 exports.responseProcessor = (req: XP.Request, res: XP.Response) => {
-  log.info(`in ${XP_RUN_MODE} mode`)
-  if (XP_RUN_MODE === 'PROD') return res // TODO: It's possible UTV, TEST, and QA have RunMode PROD
-
   // Prepend environment to title tag in head for DEV, UTV, TEST, and QA
   const headRegex = /<head>([\s\S]*?)<\/head>/
   const titleRegex = /<title>(.*?)<\/title>/
 
   // TODO: XP_RUN_MODE only returns DEV or PROD. Consider using baseUrl?
-  // const baseUrl = app.config && app.config['ssb.baseUrl']
-  res.body = (res.body as string).replace(headRegex, (match, headContent) => {
-    const modifiedHeadContent = headContent.replace(titleRegex, (match: string, titleText: string) => {
-      return match.replace(titleText, `${XP_RUN_MODE}: ${titleText}`)
+  let environment: string = ''
+  const baseUrl = app.config && app.config['ssb.baseUrl']
+  if (XP_RUN_MODE === 'DEV') {
+    environment = XP_RUN_MODE
+  } else {
+    // XP_RUN_MODE for UTV, TEST, and QA is 'PROD', so we fetch environment from baseUrl here
+    const envRegEx = /(utv|qa|test)/
+    environment = baseUrl.match(envRegEx).toUpperCase()
+  }
+
+  if (environment) {
+    res.body = (res.body as string).replace(headRegex, (match, headContent) => {
+      const modifiedHeadContent = headContent.replace(titleRegex, (match: string, titleText: string) => {
+        return match.replace(titleText, `${environment}: ${titleText}`)
+      })
+      return match.replace(headContent, modifiedHeadContent)
     })
-    return match.replace(headContent, modifiedHeadContent)
-  })
+  }
+
   return res
 }
