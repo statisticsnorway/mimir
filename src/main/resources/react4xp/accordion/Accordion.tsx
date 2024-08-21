c3fb4a9a32ad7920451ed9d8ed071129bbbed117
johnnadeluy <johnnadel.salita@gmail.com>
2024-08-21 14:01:08 +0200

    Refactor Accordion from classes to functional React MIM-1948

--- src/main/resources/react4xp/accordion/Accordion.tsx
+++ src/main/resources/react4xp/accordion/Accordion.tsx
@@ -1,51 +1,48 @@
 import React from 'react'
 import { Accordion as AccordionComponent, NestedAccordion } from '@statisticsnorway/ssb-component-library'
 
+import { type AccordionData, type AccordionItems, type AccordionProps } from '../../lib/types/partTypes/accordion'
 import { sanitize } from '../../lib/ssb/utils/htmlUtils'
-import { type AccordionData, type AccordionProps } from '../../lib/types/partTypes/accordion'
 
-class Accordion extends React.Component<AccordionProps> {
-  renderNestedAccordions(items: AccordionData['items']) {
-    return items!.map((item, i) => (
-      <NestedAccordion key={i} header={item.title}>
-        {item.body && <div dangerouslySetInnerHTML={this.createMarkup(item.body)} />}
+function Accordion(props: AccordionProps) {
+  const { accordions } = props
+
+  function renderNestedAccordions(items: AccordionData['items']) {
+    return (items as AccordionProps['accordions'])!.map((item, i) => (
+      <NestedAccordion key={i} header={(item as AccordionItems).title}>
+        {item.body && <div dangerouslySetInnerHTML={createMarkup(item.body)} />}
       </NestedAccordion>
     ))
   }
 
-  createMarkup(html: string) {
+  function createMarkup(html: string) {
     return {
       __html: sanitize(html.replace(/&nbsp;/g, ' ')),
     }
   }
 
-  render() {
-    const location = window.location
-    const anchor = location && location.hash !== '' ? location.hash.substr(1) : undefined
+  const location = window.location
+  const anchor = location && location.hash !== '' ? location.hash.slice(1) : undefined
 
-    const { accordions } = this.props
-
-    return (
-      <section className='xp-part part-accordion container'>
-        <div className='row'>
-          {accordions.map((accordion, index) => (
-            <React.Fragment key={index}>
-              <AccordionComponent
-                className='col-12'
-                id={accordion.id}
-                header={accordion.open}
-                subHeader={accordion.subHeader}
-                openByDefault={anchor && accordion.id && accordion.id === anchor}
-              >
-                {accordion.body && <div dangerouslySetInnerHTML={this.createMarkup(accordion.body)} />}
-                {this.renderNestedAccordions(accordion.items)}
-              </AccordionComponent>
-            </React.Fragment>
-          ))}
-        </div>
-      </section>
-    )
-  }
+  return (
+    <section className='xp-part part-accordion container'>
+      <div className='row'>
+        {accordions.map((accordion, index) => (
+          <AccordionComponent
+            key={index}
+            className='col-12'
+            id={accordion.id}
+            header={accordion.open}
+            subHeader={accordion.subHeader}
+            openByDefault={anchor && accordion.id && accordion.id === anchor}
+          >
+            {accordion.body && <div dangerouslySetInnerHTML={createMarkup(accordion.body)} />}
+            {renderNestedAccordions(accordion.items)}
+          </AccordionComponent>
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default Accordion
