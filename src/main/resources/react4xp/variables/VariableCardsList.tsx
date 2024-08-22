47961c9856c534215dcc34a3d1520f29209392cb
johnnadeluy <johnnadel.salita@gmail.com>
2024-08-22 08:45:40 +0200

    Refactor VariableCardsList part from classes to functional React MIM-1948

--- src/main/resources/react4xp/variables/VariableCardsList.tsx
+++ src/main/resources/react4xp/variables/VariableCardsList.tsx
@@ -3,23 +3,18 @@ import { type VariablesProps } from '../../lib/types/partTypes/variables'
 import VariableCard from './VariableCard'
 
 interface VariableCardsListProps {
-  variables: VariablesProps['variables'][]
+  variables: VariablesProps['variables']
 }
 
-class VariableCardsList extends React.Component<VariableCardsListProps> {
-  constructor(props: VariableCardsListProps) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div className='variable-cardlist grid-column grid-4'>
-        {this.props.variables.map((variable, idx) => {
-          return <VariableCard key={idx} variable={variable} />
-        })}
-      </div>
-    )
-  }
+function VariableCardsList(props: VariableCardsListProps) {
+  const { variables } = props
+  return (
+    <div className='variable-cardlist grid-column grid-4'>
+      {variables.map((variable, idx) => {
+        return <VariableCard key={idx} variable={variable} />
+      })}
+    </div>
+  )
 }
 
 export default VariableCardsList
