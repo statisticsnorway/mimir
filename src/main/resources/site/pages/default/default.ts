db849c2ec90d17d8afa2062a225a764c42eb00f5
Glenruben Larsen <gle@ssb.no>
2024-08-30 11:07:01 +0200

    Update metaInfo for Solr to reflect type changes.

--- src/main/resources/site/pages/default/default.ts
+++ src/main/resources/site/pages/default/default.ts
@@ -259,6 +259,7 @@ export function get(req: XP.Request): XP.Response {
     headerBody: header?.body,
     footerBody: footer?.body,
     ...metaInfo,
+    metaInfoMainSubjects: metaInfo.metaInfoMainSubjects?.join(';'),
     jsonLd,
     pageMap,
     breadcrumbsReactId: breadcrumbId,
@@ -266,6 +267,8 @@ export function get(req: XP.Request): XP.Response {
     hideBreadcrumb,
     tableView: page.type === 'mimir:table',
   }
+
+  log.info(JSON.stringify(model, null, 2))
 
   const thymeleafRenderBody = render(view, model)
 
@@ -727,6 +730,7 @@ interface DefaultModel {
   pageMap: string | undefined
   headerBody: string | undefined
   footerBody: string | undefined
+  metaInfoMainSubjects: string | undefined
   breadcrumbsReactId: string | undefined
   hideHeader: boolean
   hideBreadcrumb: boolean
