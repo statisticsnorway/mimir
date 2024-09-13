35bc3c549a0bdca9fe5834c61e17e9afe3b22442
Carina <cgn@ssb.no>
2024-09-13 10:54:14 +0200

    Fixes statisticContacts

--- src/main/resources/site/parts/statisticContact/statisticContact.tsx
+++ src/main/resources/site/parts/statisticContact/statisticContact.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Row } from 'react-bootstrap'
-import { Link, ExpansionBox, Title } from '@statisticsnorway/ssb-component-library'
+import { Link, Title } from '@statisticsnorway/ssb-component-library'
 import { User } from 'react-feather'
 //import { type ContactModel } from '../../../lib/types/partTypes/contact'
 import { type StatisticContactProps, type Contact } from '/lib/types/partTypes/statisticContact'
 
 function StatisticContact(props: StatisticContactProps) {
   const { label, contacts } = props
-
-  console.log('Kontakter: ' + JSON.stringify(contacts, null, 4))
 
   function renderContact(contact: Contact) {
     return (
@@ -30,23 +28,23 @@ function StatisticContact(props: StatisticContactProps) {
     )
   }
 
-  function contactsInfo() {
+  function renderContactsInfo() {
     if (contacts?.length) {
-      return <Row className='contact-info'>{contacts.map((contact) => renderContact(contact))}</Row>
+      return <div className='contact-wrapper'>{contacts.map((contact) => renderContact(contact))}</div>
     }
     return null
   }
 
   return (
-    <React.Fragment>
+    <Row>
       <div className='title-wrapper'>
         <Title size={2}>{label}</Title>
         <div className='icon-wrapper'>
           <User size={52} />
         </div>
       </div>
-      {contacts?.length && <ExpansionBox header={label} text={contactsInfo()} openByDefault />}
-    </React.Fragment>
+      {contacts?.length && <div className='contact-list'>{renderContactsInfo()}</div>}
+    </Row>
   )
 }
 
