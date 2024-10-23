c8085b13cd97fa7e30143f22155163812060c239
Carl-OW <142233642+Carl-OW@users.noreply.github.com>
2024-10-23 18:12:19 +0200

    30 to 7 days cookie

--- src/main/resources/react4xp/_entries/Popup.tsx
+++ src/main/resources/react4xp/_entries/Popup.tsx
@@ -76,7 +76,7 @@ const Popup = () => {
     setIsVisible(false)
 
     const date = new Date()
-    date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000)
+    date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000)
     const expires = `expires=${date.toUTCString()}`
     document.cookie = `hidePopup=true; ${expires}; path=/; SameSite=Lax`
   }
