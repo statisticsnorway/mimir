901831c8983e0399528bd0e8a4dac24cf3c2ca93
johnnadeluy <johnnadel.salita@gmail.com>
2024-12-17 12:42:54 +0100

    Convert string to numbers for cell value MIM-1974

--- src/main/resources/lib/ssb/utils/tableExportUtils.ts
+++ src/main/resources/lib/ssb/utils/tableExportUtils.ts
@@ -6,10 +6,25 @@ interface TableExport {
 }
 
 export const exportTableToExcel = ({ tableElement, fileName = 'tabell.xlsx' }: TableExport): void => {
+  const sheetName = 'Sheet1'
   const workbook = XLSX.utils.table_to_book(tableElement, {
-    sheet: 'Sheet1',
+    sheet: sheetName,
     raw: true,
   })
+
+  const worksheet = workbook.Sheets[sheetName]
+  for (const cell in worksheet) {
+    // Check if SheetJS worksheet cell object has a value and that the cell isn't a SheetJS special property/metadata
+    if (worksheet.hasOwnProperty(cell) && cell[0] !== '!') {
+      const cellValue = worksheet[cell].v.replace(/\s/g, '').replace(/,/g, '.')
+      if (cellValue !== '') {
+        if (!isNaN(Number(cellValue))) {
+          worksheet[cell].v = parseFloat(cellValue.toLocaleString('nb-NO'))
+          worksheet[cell].t = 'n'
+        }
+      }
+    }
+  }
 
   XLSX.writeFile(workbook, fileName)
 }
