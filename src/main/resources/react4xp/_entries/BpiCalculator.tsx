8d1028c153f072e0d7e43e20c7c020663006244d
johnnadeluy <johnnadel.salita@gmail.com>
2025-01-23 14:06:14 +0100

    Show correct month for bpi calculator Add phrases MIM-2157

--- src/main/resources/react4xp/_entries/BpiCalculator.tsx
+++ src/main/resources/react4xp/_entries/BpiCalculator.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Title, Divider, Button } from '@statisticsnorway/ssb-component-library'
+import { Title, Divider, RadioGroup, Dropdown, Input, Button } from '@statisticsnorway/ssb-component-library'
 import { Container, Row, Col, Form } from 'react-bootstrap'
 
 import { BpiCalculatorProps } from '/lib/types/partTypes/bpiCalculator'
@@ -7,34 +7,103 @@ import { BpiCalculatorProps } from '/lib/types/partTypes/bpiCalculator'
 function BpiCalculator(props: BpiCalculatorProps) {
   const { phrases, nextPublishText } = props
 
+  function addDropdownChooseQuarter(id: 'start-quarter' | 'end-quarter') {
+    return (
+      <Dropdown
+        className='quarter'
+        id={id}
+        header={phrases.bpiChooseQuarterPeriod}
+        onSelect={() => {}}
+        error={''}
+        errorMessage={''}
+        selectedItem={{ id: `bpi-${id}`, title: phrases.bpiChooseQuarterPeriod }}
+        items={[]}
+      />
+    )
+  }
+
+  // TODO: Check if it's possible to make this into a React template component that can be used by all calculators
   function renderForm() {
     return (
       <div className='calculator-form'>
-        <Form>
-          <>Fields</>
-          <Divider />
-          <>Fields2</>
+        <Row>
+          <Col>
+            <Title size={2}>{phrases.bpiCalculatorTitle}</Title>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p className='publish-text'>{nextPublishText}</p>
+          </Col>
+        </Row>
+        <Form onSubmit={() => {}}>
+          <Container>
+            <Row className='mt-5'>
+              <Col className='col-12 col-lg-4'>
+                <Title size={3}>{phrases.bpiChooseResidentialType}</Title>
+                <RadioGroup onChange={() => {}} selectedValue='1' orientation='column' items={[]} />
+              </Col>
+              <Col className='col-12 col-lg-8'>
+                <Title size={3}>{phrases.bpiChooseRegion}</Title>
+                <Dropdown selectedItem={{ id: 'chooseRegion', title: phrases.bpiChooseRegion }} items={[]} />
+              </Col>
+            </Row>
+            <Divider className='my-5' />
+            {/* TODO: This part of the field is almost, if not, the same for all calculators */}
+            <Row>
+              <Col className='calculate-from col-12 col-lg-6'>
+                <Title size={3}>{phrases.calculatePriceChangeFrom}</Title>
+                <Container>
+                  <Row>
+                    <Col className='select-year col-sm-5'>
+                      <Input
+                        className='input-year'
+                        label={phrases.fromYear}
+                        ariaLabel={phrases.fromYearScreenReader}
+                        handleChange={() => {}}
+                        error={''}
+                        errorMessage={''}
+                        onBlur={() => {}}
+                      />
+                    </Col>
+                    <Col className='select-month col-12 col-sm-7'>{addDropdownChooseQuarter('start-quarter')}</Col>
+                  </Row>
+                </Container>
+              </Col>
+              <Col className='calculate-to col-12 col-lg-6'>
+                <Title size={3}>{phrases.calculatePriceChangeTo}</Title>
+                <Container>
+                  <Row>
+                    <Col className='select-year col-sm-5'>
+                      <Input
+                        className='input-year'
+                        label={phrases.toYear}
+                        ariaLabel={phrases.toYearScreenReader}
+                        handleChange={() => {}}
+                        error={''}
+                        errorMessage={''}
+                        onBlur={() => {}}
+                      />
+                    </Col>
+                    <Col className='select-month col-12 col-sm-7'>{addDropdownChooseQuarter('end-quarter')}</Col>
+                  </Row>
+                </Container>
+              </Col>
+            </Row>
+          </Container>
         </Form>
+        <Row className='submit'>
+          <Col>
+            <Button className='submit-button' primary type='submit'>
+              {phrases.seePriceChange}
+            </Button>
+          </Col>
+        </Row>
       </div>
     )
   }
 
-  return (
-    <Container className='content'>
-      <Row>
-        <Col className='col-12 col-md-6'>
-          <Title size={2}>{phrases.calculatePriceChange}</Title> // TODO: This is different for bpiCalculator
-        </Col>
-      </Row>
-      <Row>
-        <Col className='col-12 col-md-8'>
-          <p className='publish-text'>{nextPublishText}</p>
-        </Col>
-      </Row>
-      {renderForm()}
-      <Button></Button>
-    </Container>
-  )
+  return <Container className='content'>{renderForm()}</Container>
 }
 
 export default (props: BpiCalculatorProps) => <BpiCalculator {...props} />
--- src/main/resources/site/parts/bpiCalculator/bpiCalculator.ts
+++ src/main/resources/site/parts/bpiCalculator/bpiCalculator.ts
@@ -1,12 +1,13 @@
+import { quartersToMonths } from 'date-fns'
 import { type Content } from '/lib/xp/content'
 import { getContent, getComponent } from '/lib/xp/portal'
 import { localize } from '/lib/xp/i18n'
-import { Dataset } from '/lib/types/jsonstat-toolkit'
+import { Dataset, Dimension } from '/lib/types/jsonstat-toolkit'
 import { type CalculatorPeriod } from '/lib/types/calculator'
 import { type Phrases } from '/lib/types/language'
 import { render } from '/lib/enonic/react4xp'
 import { type DropdownItems as MonthDropdownItems } from '/lib/types/components'
-import { allMonths, lastPeriodKpi, monthLabel, nextPeriod } from '/lib/ssb/utils/calculatorUtils'
+import { allMonths, monthLabel } from '/lib/ssb/utils/calculatorUtils'
 
 import { renderError } from '/lib/ssb/error/error'
 import { getLanguage } from '/lib/ssb/utils/language'
@@ -26,39 +27,42 @@ export function preview(req: XP.Request) {
 }
 
 function renderPart(req: XP.Request): XP.Response {
-  const page = getContent()
+  const page = getContent<Content<CalculatorConfig>>()
   const config = getComponent<XP.PartComponent.BpiCalculator>()?.config
   if (!config) throw Error('No part found')
 
-  const language = getLanguage(page)
+  const language = getLanguage(page as Content)
   const phrases = language?.phrases as Phrases
   const calculatorConfig: Content<CalculatorConfig> | undefined = getCalculatorConfig()
   const bpiDataset: Dataset | null = calculatorConfig
     ? getCalculatorDatasetFromSource(calculatorConfig, 'bpiCalculator')
     : null
   const months: MonthDropdownItems = allMonths(phrases)
-  const lastUpdated: CalculatorPeriod = lastPeriodKpi(bpiDataset)
-  const nextUpdate: CalculatorPeriod = nextPeriod(lastUpdated.month as string, lastUpdated.year as string)
-  const nextReleaseMonth: number = (nextUpdate.month as number) === 12 ? 1 : (nextUpdate.month as number) + 1
+  const lastUpdated: CalculatorPeriod | undefined = lastQuartalPeriod(bpiDataset)
+  const nextUpdate: CalculatorPeriod = nextQuartalPeriod({
+    month: lastUpdated?.month as string,
+    year: lastUpdated?.year as string,
+  })
   const nextPublishText: string = localize({
     key: 'calculatorNextPublishText',
     locale: language?.code,
     values: [
-      monthLabel(months, language?.code, lastUpdated.month as string),
-      lastUpdated.year as string,
-      monthLabel(months, language?.code, nextUpdate.month as string),
-      monthLabel(months, language?.code, nextReleaseMonth),
+      monthLabel(months, language?.code, lastUpdated?.month as string),
+      lastUpdated?.year as string,
+      phrases.bpiNextQuarterPeriod,
+      monthLabel(months, language?.code, nextUpdate?.month),
     ],
   })
   const lastNumberText: string = localize({
     key: 'calculatorLastNumber',
     locale: language?.code,
-    values: [monthLabel(months, language?.code, lastUpdated.month as string), lastUpdated.year as string],
+    values: [monthLabel(months, language?.code, lastUpdated?.month as string), lastUpdated?.year as string],
   })
 
   return render(
     'BpiCalculator',
     {
+      phrases,
       nextPublishText,
       lastNumberText,
     },
@@ -67,4 +71,29 @@ function renderPart(req: XP.Request): XP.Response {
       body: '<section class="xp-part bpi-calculator container"></section>',
     }
   )
+}
+
+function lastQuartalPeriod(bpiData: Dataset | null): CalculatorPeriod | undefined {
+  const bpiDataDimension: Dimension | null = bpiData?.Dimension('Tid') as Dimension
+  const dataTime: string | undefined = bpiDataDimension?.id as string
+
+  if (dataTime) {
+    const lastTimeItem: string = dataTime[dataTime.length - 1]
+    const [year, quarters]: Array<string> = lastTimeItem.split('K')
+
+    return {
+      month: quartersToMonths(Number(quarters)) - 3 + 1, // Get the beginning month instead of the last month of the quarter
+      year,
+    }
+  }
+}
+
+function nextQuartalPeriod({ month, year }: CalculatorPeriod) {
+  const nextQuartalMonth = Math.ceil(Number(month) + 3)
+  const validNextQuartalMonth = nextQuartalMonth < 12 ? nextQuartalMonth : 1 // Ensures that january of next month is displayed correctly after calculation
+
+  return {
+    month: validNextQuartalMonth,
+    year: nextQuartalMonth === 1 ? Number(year) + 1 : year, // January of next year
+  }
 }
