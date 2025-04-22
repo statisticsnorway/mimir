c38fba2923a0b981c0b0577c2d81fbc0ed2a9cb8
johnnadeluy <johnnadel.salita@gmail.com>
2025-04-22 14:18:40 +0200

    Refactor refreshQueriesAsyc functionality MIM-2278

--- src/main/resources/lib/ssb/cron/task.ts
+++ src/main/resources/lib/ssb/cron/task.ts
@@ -9,26 +9,48 @@ import { logUserDataQuery, Events } from '/lib/ssb/repo/query'
 import { completeJobLog, JOB_STATUS_COMPLETE } from '/lib/ssb/repo/job'
 import { type DataSource } from '/site/mixins/dataSource'
 
+function executeQuery(
+  httpQuery: Content<DataSource>,
+  httpQueriesBatchLength: number,
+  httpQueryIndex: number,
+  jobLogResult: Array<CreateOrUpdateStatus>
+) {
+  progress({
+    current: httpQueryIndex,
+    total: httpQueriesBatchLength,
+    info: `Refresh dataset ${httpQuery._id}`,
+  })
+
+  logUserDataQuery(httpQuery._id, {
+    message: Events.GET_DATA_STARTED,
+  })
+
+  const result: CreateOrUpdateStatus = refreshDataset(httpQuery, DATASET_BRANCH)
+  logUserDataQuery(httpQuery._id, {
+    message: result.status,
+  })
+
+  jobLogResult.push(result)
+}
+
 export function refreshQueriesAsync(
   httpQueries: Array<Content<DataSource>>,
   jobLogId: string,
   filterInfo: RSSFilterLogData,
-  batchSize = 4
+  maxNumberOfBatches = 4
 ): Array<string> {
-  // Calculate number of batches and items per batch
   const totalQueries = httpQueries.length
-  const numberOfBatches = Math.min(batchSize, totalQueries)
+  const numberOfBatches = Math.min(maxNumberOfBatches, totalQueries)
   const itemsPerBatch = Math.ceil(totalQueries / numberOfBatches)
-
   const httpQueriesBatches: Array<Array<Content<DataSource>>> = splitEvery(itemsPerBatch, httpQueries)
   const jobLogResult: Array<CreateOrUpdateStatus> = []
-  let batchNumber = 0
-  let completedBatches = 0
+  const completedBatches = [...Array(numberOfBatches)].map(() => 0)
+
   log.info(
     `Starting refreshQueriesAsync with ${totalQueries} queries split into ${numberOfBatches} batches of approximately ${itemsPerBatch} items each`
   )
-  return httpQueriesBatches.map((httpQueriesBatch: Array<Content<DataSource>>) => {
-    batchNumber++
+
+  return httpQueriesBatches.map((httpQueriesBatch: Array<Content<DataSource>>, batchNumber) => {
     return executeFunction({
       description: `RefreshRows_Batch_${batchNumber}_of_${numberOfBatches}`,
       func: function () {
@@ -39,71 +61,41 @@ export function refreshQueriesAsync(
         })
 
         httpQueriesBatch.forEach((httpQuery: Content<DataSource>, httpQueryIndex) => {
-          progress({
-            current: httpQueryIndex,
-            total: httpQueriesBatch.length,
-            info: `Refresh dataset ${httpQuery._id}`,
-          })
+          executeQuery(httpQuery, httpQueriesBatch.length, httpQueryIndex, jobLogResult)
+        })
 
-          logUserDataQuery(httpQuery._id, {
-            message: Events.GET_DATA_STARTED,
-          })
+        completedBatches[batchNumber] = 1
+        if (completedBatches.some((value) => value === 0)) return
 
-          const result: CreateOrUpdateStatus = refreshDataset(httpQuery, DATASET_BRANCH)
-          logUserDataQuery(httpQuery._id, {
-            message: result.status,
-          })
+        if (jobLogResult.length !== totalQueries) {
+          log.info(`Total progress: ${jobLogResult.length} of ${totalQueries} refreshed`)
+          const failedDatasets = httpQueries.filter(
+            (query) => !jobLogResult.some((result) => result.dataquery._id === query._id)
+          )
+          log.info(
+            `${failedDatasets.length} dataset(s) failed to refresh. Failed dataset id(s): ${failedDatasets.map((ds) => ds._id).join(', ')}. Retrying...`
+          )
+          const retryResult: CreateOrUpdateStatus[] = failedDatasets.map((dataset) =>
+            refreshDataset(dataset, DATASET_BRANCH)
+          )
 
-          jobLogResult.push(result)
+          jobLogResult.push(...retryResult)
+        }
 
-          if (httpQueryIndex === httpQueriesBatch.length - 1) {
-            // Since this task runs asynchronously, we need to have an additional batch number counter and check to see if all the batches have been completed
-            // before logging and updating the job logs. That is to prevent logging and updating the job log multiple times for each batch.
-            completedBatches++
-            if (completedBatches === numberOfBatches) {
-              log.info(`Total progress: ${jobLogResult.length} of ${totalQueries} refreshed`)
-              if (jobLogResult.length === totalQueries) {
-                completeJobLog(jobLogId, JOB_STATUS_COMPLETE, {
-                  filterInfo,
-                  result: jobLogResult.map((r) => ({
-                    id: r.dataquery._id,
-                    displayName: r.dataquery.displayName,
-                    contentType: r.dataquery.type,
-                    dataSourceType: r.dataquery.data.dataSource?._selected,
-                    status: r.status,
-                  })),
-                })
-              } else {
-                const failedDatasets = httpQueries.filter(
-                  (query) => !jobLogResult.some((result) => result.dataquery._id === query._id)
-                )
-                log.info(
-                  `${failedDatasets.length} dataset(s) failed to refresh. Failed dataset id(s): ${failedDatasets.map((ds) => ds._id).join(', ')}. Retrying...`
-                )
-                const retryResult: CreateOrUpdateStatus[] = failedDatasets.map((dataset) =>
-                  refreshDataset(dataset, DATASET_BRANCH)
-                )
+        log.info(`Total progress: ${jobLogResult.length} of ${totalQueries} refreshed`)
+        completeJobLog(jobLogId, JOB_STATUS_COMPLETE, {
+          filterInfo,
+          result: jobLogResult.map((r) => ({
+            id: r.dataquery._id,
+            displayName: r.dataquery.displayName,
+            contentType: r.dataquery.type,
+            dataSourceType: r.dataquery.data.dataSource?._selected,
+            status: r.status,
+          })),
+        })
 
-                jobLogResult.push(...retryResult)
-
-                log.info(`Total progress: ${jobLogResult.length} of ${totalQueries} refreshed`)
-
-                completeJobLog(jobLogId, JOB_STATUS_COMPLETE, {
-                  filterInfo,
-                  result: jobLogResult.map((r) => ({
-                    id: r.dataquery._id,
-                    displayName: r.dataquery.displayName,
-                    contentType: r.dataquery.type,
-                    dataSourceType: r.dataquery.data.dataSource?._selected,
-                    status: r.status,
-                  })),
-                })
-              }
-              progress({
-                info: `Total progress: ${jobLogResult.length} of ${totalQueries} refreshed`,
-              })
-            }
-          }
+        progress({
+          info: `Total progress: ${jobLogResult.length} of ${totalQueries} refreshed`,
         })
       },
     })
