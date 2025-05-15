ef5d05ad79e6d33d0aec89a3af741007ad445a36
Carl-OW <142233642+Carl-OW@users.noreply.github.com>
2025-05-15 15:53:13 +0200

    basic basic

--- src/main/resources/react4xp/_entries/CookieBanner.tsx
+++ src/main/resources/react4xp/_entries/CookieBanner.tsx
@@ -1,10 +1,19 @@
+import React, { useState } from 'react'
 import { Button } from '@statisticsnorway/ssb-component-library'
 
 const CookieBanner = () => {
+  const [visible, setVisible] = useState(true)
+
+  if (!visible) return null
+
   return (
-    <div>
-      <Button>First Button</Button>
-      <Button>Second Button</Button>
+    <div className='cookie-banner-overlay'>
+      <div className='cookie-banner-popup'>
+        <div className='cookie-banner__buttons'>
+          <Button onClick={() => setVisible(false)}>First</Button>
+          <Button onClick={() => setVisible(false)}>Second</Button>
+        </div>
+      </div>
     </div>
   )
 }
