db103740a79358f5017a1eb89e73500e4d4bfb5b
Carl-OW <142233642+Carl-OW@users.noreply.github.com>
2025-05-19 14:35:57 +0200

    functions

--- src/main/resources/react4xp/_entries/CookieBanner.tsx
+++ src/main/resources/react4xp/_entries/CookieBanner.tsx
@@ -1,8 +1,59 @@
-// src/main/resources/react4xp/components/CookieBanner.tsx
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '@statisticsnorway/ssb-component-library'
 
 const CookieBanner = () => {
+  const [visible, setVisible] = useState(false)
+
+  useEffect(() => {
+    const cookieConsentPresent = document.cookie.includes('cookie-consent=')
+    const storedState = localStorage.getItem('seenCookieConsent')
+
+    if (!cookieConsentPresent && storedState === 'yes') {
+      blockOptionalCookies()
+      localStorage.setItem('seenCookieConsent', 'cleared')
+    }
+
+    if (cookieConsentPresent && storedState !== 'yes') {
+      localStorage.setItem('seenCookieConsent', 'yes')
+    }
+
+    setVisible(!cookieConsentPresent)
+  }, [])
+
+
+  const acceptAllCookies = () => {
+    document.cookie = 'cookie-consent=all; path=/; max-age=31536000; SameSite=Lax'
+    setVisible(false)
+  }
+
+  const acceptOnlyNecessaryCookies = () => {
+    document.cookie = 'cookie-consent=necessary; path=/; max-age=31536000; SameSite=Lax'
+    blockOptionalCookies()
+    setVisible(false)
+  }
+
+  function blockOptionalCookies() {
+    document.cookie
+      .split(';')
+      .map((c) => c.trim())
+      .forEach((cookie) => {
+        const name = cookie.split('=')[0]
+
+        const isCookieToPurge = [
+          '_ga',
+          'nmstat',
+          'app.browse.RecentItemsList',
+        ].some((prefix) => name.startsWith(prefix))
+
+        if (isCookieToPurge) {
+          document.cookie = `${name}=; Max-Age=0; path=/; SameSite=Lax`
+        }
+      })
+  }
+
+
+  if (!visible) return null
+
   return (
     <div className='cookie-banner'>
       <div className='cookie-banner-content'>
@@ -16,8 +67,12 @@ const CookieBanner = () => {
           Les mer om informasjonskapsler
         </a>
         <div className='cookie-banner-buttons'>
-          <Button className='cookie-button-accept'>Godta alle</Button>
-          <Button className='cookie-button-decline'>Kun nødvendige</Button>
+          <Button className='cookie-button-accept' onClick={acceptAllCookies}>
+            Godta alle
+          </Button>
+          <Button className='cookie-button-decline' onClick={acceptOnlyNecessaryCookies}>
+            Kun nødvendige
+          </Button>
         </div>
       </div>
     </div>
