786afa6e11a695afde578f598e62efd02e836420
Carl-OW <142233642+Carl-OW@users.noreply.github.com>
2025-05-19 11:57:42 +0200

    Basic component

--- src/main/resources/react4xp/_entries/CookieBanner.tsx
+++ src/main/resources/react4xp/_entries/CookieBanner.tsx
@@ -1,17 +1,23 @@
-import React, { useState } from 'react'
+// src/main/resources/react4xp/components/CookieBanner.tsx
+import React from 'react'
 import { Button } from '@statisticsnorway/ssb-component-library'
 
 const CookieBanner = () => {
-  const [visible, setVisible] = useState(true)
-
-  if (!visible) return null
-
   return (
-    <div className='cookie-banner-overlay'>
-      <div className='cookie-banner-popup'>
-        <div className='cookie-banner__buttons'>
-          <Button onClick={() => setVisible(false)}>First</Button>
-          <Button onClick={() => setVisible(false)}>Second</Button>
+    <div className='cookie-banner'>
+      <div className='cookie-banner-content'>
+        <h3 className='cookie-banner-title'>Vi bruker informasjonskapsler</h3>
+        <p className='cookie-banner-text'>
+          Nødvendige informasjonskapsler gjør at ssb.no fungerer og er sikkert, og kan derfor ikke velges bort. Vi har
+          tre valgfrie informasjonskapsler som lar oss se hvor brukerne våre klikker og beveger seg på nettsiden.
+          Informasjonen er anonym, deles aldri med andre og brukes til å forbedre ssb.no.
+        </p>
+        <a href='/om-ssb/informasjonskapsler' className='cookie-banner-link'>
+          Les mer om informasjonskapsler
+        </a>
+        <div className='cookie-banner-buttons'>
+          <Button className='cookie-button-accept'>Godta alle</Button>
+          <Button className='cookie-button-decline'>Kun nødvendige</Button>
         </div>
       </div>
     </div>
