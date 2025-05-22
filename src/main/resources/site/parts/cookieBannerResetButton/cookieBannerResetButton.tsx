08ea9b8a763ffdf116ef0dc3e7d7d8f389af83fb
Carl-OW <142233642+Carl-OW@users.noreply.github.com>
2025-05-22 09:29:49 +0200

    simple button with no onlick

--- src/main/resources/site/parts/cookieBannerResetButton/cookieBannerResetButton.tsx
+++ src/main/resources/site/parts/cookieBannerResetButton/cookieBannerResetButton.tsx
@@ -4,11 +4,9 @@ import { Button } from '@statisticsnorway/ssb-component-library'
 function CookieBannerResetButton() {
   return (
     <div>
-      <Button primary>
-        Tilbakestill informasjonskapsler
-      </Button>
+      <Button primary>Tilbakestill informasjonskapsler</Button>
     </div>
   )
 }
 
-export default (props: object ) => <CookieBannerResetButton {...props} />
+export default (props: object) => <CookieBannerResetButton {...props} />
