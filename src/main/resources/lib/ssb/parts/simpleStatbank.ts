849137fd0b98783f536a9db1b9ce682ce3daa776
johnnadeluy <johnnadel.salita@gmail.com>
2025-06-13 08:28:40 +0200

    Ensure that data dimensions and filter dimension time are arrays

--- src/main/resources/lib/ssb/parts/simpleStatbank.ts
+++ src/main/resources/lib/ssb/parts/simpleStatbank.ts
@@ -5,6 +5,7 @@ import { fetchStatbankApiDataQuery } from '/lib/ssb/dataset/statbankApi/statbank
 import { type SimpleStatbankResult, type DimensionData } from '/lib/types/partTypes/simpleStatbank'
 import { createHumanReadableFormat } from '/lib/ssb/utils/utils'
 import { localizeTimePeriod } from '/lib/ssb/utils/language'
+import { ensureArray } from '../utils/arrayUtils'
 
 export function getStatbankApiData(
   dimensionCode: string,
@@ -14,9 +15,9 @@ export function getStatbankApiData(
   const statbankApiData: JSONstat = fetchStatbankApiDataQuery(urlOrId, query)
   const dataset: Dataset | null = statbankApiData ? JSONstat(statbankApiData).Dataset('dataset') : null
   const filterDimensionCode: Dimension | Dimension[] | null | undefined = dataset?.Dimension(dimensionCode)
-  const dataDimensions: Array<string> = (filterDimensionCode as Dimension)?.id as Array<string>
+  const dataDimensions = ensureArray((filterDimensionCode as Dimension)?.id)
   const filterDimensionTime: Dimension | null = dataset?.Dimension('Tid') as Dimension
-  const timeDimensions: Array<string> = filterDimensionTime?.id as Array<string>
+  const timeDimensions = ensureArray(filterDimensionTime?.id)
 
   try {
     const result: DimensionData[] = dataDimensions?.length
