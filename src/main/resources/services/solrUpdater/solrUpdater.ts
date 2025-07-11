6651e03cd3db13a72676532ef35ebf8b8a1c8808
johnnadeluy <johnnadel.salita@gmail.com>
2025-07-11 13:09:39 +0200

    Add logging to solrUpdater service MIM-2341

--- src/main/resources/services/solrUpdater/solrUpdater.ts
+++ src/main/resources/services/solrUpdater/solrUpdater.ts
@@ -1,7 +1,7 @@
 import { Content, query } from '/lib/xp/content'
 import { pageUrl } from '/lib/xp/portal'
 import { render } from '/lib/thymeleaf'
-import { subDays } from '/lib/vendor/dateFns'
+import { subDays, format } from '/lib/vendor/dateFns'
 import { Article, Page, Statistics } from '/site/content-types'
 
 export const get = (): XP.Response => {
@@ -15,6 +15,10 @@ export const get = (): XP.Response => {
     query: `modifiedTime >= dateTime('${yesterday}') OR publish.from >= dateTime('${yesterday}')`,
     contentTypes: [`${app.name}:statistics`, `${app.name}:article`, `${app.name}:page`],
   })
+
+  log.info(
+    `Found ${changedContent.hits.length} changed content items for the solrUpdater service since yesterday: ${format(yesterday, 'yyyy-MM-dd HH:mm')} (${yesterday}).`
+  )
 
   const urls: string[] = changedContent.hits.map((content) => {
     return (
