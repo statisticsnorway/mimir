f9bd08d94050603f6bc3778eb574651cddcd32f3
Olav Landsverk <stud-oll@ssb.no>
2025-07-22 12:52:35 +0200

    fix: use Content API with draft context

--- src/main/resources/services/postMarkdown/postMarkdown.ts
+++ src/main/resources/services/postMarkdown/postMarkdown.ts
@@ -1,4 +1,6 @@
 import { exists as existsContent, get as getContent } from '/lib/xp/content'
+import { get as getContext, run as runWithContext } from '/lib/xp/context'
+import { type ContextCallback } from '/lib/ssb/repo/common'
 import { connectMarkdownRepo } from '/lib/ssb/utils/markdownUtils'
 
 export const post = (req: XP.Request): XP.Response => {
@@ -29,9 +31,11 @@ export const post = (req: XP.Request): XP.Response => {
 
   const previewId = typeof node.previewId === 'string' ? node.previewId : ''
   const previewExists = previewId
-    ? existsContent({
-        key: previewId,
-      })
+    ? withDraftContext(() =>
+        existsContent({
+          key: previewId,
+        })
+      )
     : false
 
   let previewPath: string
@@ -54,9 +58,17 @@ export const post = (req: XP.Request): XP.Response => {
 }
 
 function getPreviewPath(previewId: string): string {
-  const content = getContent({
-    key: previewId,
-  })
+  const content = withDraftContext(() =>
+    getContent({
+      key: previewId,
+    })
+  )
   const contentPath = content?._path ?? ''
   return '/admin/site/preview/default/draft' + contentPath
+}
+
+function withDraftContext<T>(callback: ContextCallback<T>): T {
+  const draftContext = getContext()
+  draftContext.branch = 'draft'
+  return runWithContext(draftContext, callback)
 }
