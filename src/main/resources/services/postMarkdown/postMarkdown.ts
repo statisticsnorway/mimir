9705175db16684de934caca8b6e0a3ba76f27910
olavlan-ssb <stud-oll@ssb.no>
2025-07-22 15:11:29 +0200

    feat: preview creation in postMarkdown service [MIM-2344] (#3338)
    * fix: define response body
    
    * feat: check if preview content exists
    
    * feat: get preview path if preview exists
    
    * fix: use Content API with draft context
    
    * feat: create preview if it doesn't exist

--- src/main/resources/services/postMarkdown/postMarkdown.ts
+++ src/main/resources/services/postMarkdown/postMarkdown.ts
@@ -1,3 +1,7 @@
+import { create as createContent, exists as existsContent, get as getContent } from '/lib/xp/content'
+import { get as getContext, run as runWithContext } from '/lib/xp/context'
+import { type RepoConnection } from '/lib/xp/node'
+import { type ContextCallback } from '/lib/ssb/repo/common'
 import { connectMarkdownRepo } from '/lib/ssb/utils/markdownUtils'
 
 export const post = (req: XP.Request): XP.Response => {
@@ -13,9 +17,9 @@ export const post = (req: XP.Request): XP.Response => {
   const nodeId = typeof json._id === 'string' ? json._id : ''
   const nodeExists = nodeId ? conn.exists(nodeId) : false
 
-  let result
+  let node
   if (nodeExists) {
-    result = conn.modify({
+    node = conn.modify({
       key: nodeId,
       editor: (node) => ({
         ...node,
@@ -23,12 +27,71 @@ export const post = (req: XP.Request): XP.Response => {
       }),
     })
   } else {
-    result = conn.create(data)
+    node = conn.create(data)
+  }
+
+  let previewId = typeof node.previewId === 'string' ? node.previewId : ''
+  const previewExists = previewId
+    ? withDraftContext(() =>
+        existsContent({
+          key: previewId,
+        })
+      )
+    : false
+  if (!previewExists) {
+    previewId = createPreview(conn, node._id, node.displayName)
+  }
+
+  const body = {
+    _id: node._id,
+    previewPath: getPreviewPath(previewId),
   }
 
   return {
     status: 200,
-    body: result,
+    body: body,
     contentType: 'application/json',
   }
+}
+
+function getPreviewPath(previewId: string): string {
+  const content = withDraftContext(() =>
+    getContent({
+      key: previewId,
+    })
+  )
+  const contentPath = content?._path ?? ''
+  return '/admin/site/preview/default/draft' + contentPath
+}
+
+function createPreview(conn: RepoConnection, nodeId: string, displayName: string): string {
+  const page = withDraftContext(() =>
+    createContent({
+      displayName: displayName,
+      parentPath: '/ssb/pubmd',
+      contentType: 'mimir:markdown',
+      workflow: {
+        state: 'IN_PROGRESS',
+      },
+      data: {
+        nodeId: nodeId,
+      },
+    })
+  )
+
+  conn.modify({
+    key: nodeId,
+    editor: (node) => ({
+      ...node,
+      previewId: page._id,
+    }),
+  })
+
+  return page._id
+}
+
+function withDraftContext<T>(callback: ContextCallback<T>): T {
+  const draftContext = getContext()
+  draftContext.branch = 'draft'
+  return runWithContext(draftContext, callback)
 }
