82c72bb30dd178eb14fd8bce4d3e885b31d29f22
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2025-07-24 09:15:31 +0200

    feat: Add macro for keyfigure in text format [MIM-2347] (#3327)
    * feat: Create new keyFigureText macro
    MIM-2326
    
    * Add text input option using predefined variables
    MIM-2347
    
    * Add list of variables to help text for text input field
    Rename [endring] to [endringstekst]
    MIM-2347
    
    * Default to changeText when there is no srChangeText
    MIM-2347
    
    * Revert change to article preface
    
    * refactor: Split change direction, text, and period into different variables.
    Split functions to decrease code complexity as well
    MIM-2347
    
    * refactor: Make missing variables messages a bit more visible
    
    * refactor: endringsdato -> endringsperiod
    
    * Strip away extra word "endring" for change period, also ensure that there aren't any extra spaces
    
    * Add explanation for stripping the word 'endring' from change period
    
    * fix: Change variable syntax for keyfigure text macro so the edit function won't be broken
    
    * fix: Fix typo for missing endringsperiode message and refer to correct variable in help text
    
    * Add variable to display source text list for key figure
    
    * refactor: Rename kilde -> kildetekst and revise source text parsing
    
    * Remove unintentional commit
    
    * TextLine -> TextArea

---
+++ src/main/resources/site/macros/keyFigureText/keyFigureText.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface KeyFigureTextProps {
+  text: string
+}
+
+const KeyFigureText = (props: KeyFigureTextProps) => <>{props.text}</>
+
+export default KeyFigureText
