ecbb50766eebf999e928852163b23196aa643b61
johnnadeluy <johnnadel.salita@gmail.com>
2025-07-24 13:41:55 +0200

    feat: Support ingress with macro for articles in subjectArticleList part

--- src/main/resources/site/parts/subjectArticleList/subjectArticleList.tsx
+++ src/main/resources/site/parts/subjectArticleList/subjectArticleList.tsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 import { type SubjectArticleListProps } from '/lib/types/partTypes/subjectArticleList'
 import { type DropdownItem } from '/lib/types/partTypes/publicationArchive'
 import { usePagination } from '/lib/ssb/utils/customHooks/paginationHooks'
+import { sanitize } from '/lib/ssb/utils/htmlUtils'
 
 /* TODO:
 - Fikse sortering (?)
@@ -89,7 +90,7 @@ function SubjectArticleList(props: SubjectArticleListProps) {
               <Link ref={getCurrentElementRef(i)} href={article.url} linkType='header' headingSize={3} standAlone>
                 {article.title}
               </Link>
-              <p className='truncate-2-lines'>{article.preface}</p>
+              <p className='truncate-2-lines' dangerouslySetInnerHTML={{ __html: sanitize(article.preface) }} />
               <time dateTime={article.publishDate}>{article.publishDateHuman}</time>
             </li>
           )
