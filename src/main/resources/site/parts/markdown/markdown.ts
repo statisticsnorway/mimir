f9f48136fcb5f7c186c05acf116c90baf4aca408
olavlan-ssb <stud-oll@ssb.no>
2025-07-24 13:22:28 +0200

    feat: add content rendering to markdown part [MIM-2345] (#3343)
    * refactor: add getMarkdownText function in utils
    
    * refactor: add getMarkdownTextFromComponent
    
    * fix: (re)move null/falsy checks
    
    * feat: get markdown text from content when exists

--- src/main/resources/site/parts/markdown/markdown.ts
+++ src/main/resources/site/parts/markdown/markdown.ts
@@ -1,19 +1,19 @@
-import { getComponent } from '/lib/xp/portal'
-import { getMarkdownNode } from '/lib/ssb/utils/markdownUtils'
+import { getComponent, getContent } from '/lib/xp/portal'
+import { type Content } from '/lib/xp/content'
+import { getMarkdownText } from '/lib/ssb/utils/markdownUtils'
 import { render } from '/lib/enonic/react4xp'
 import { render as renderMarkdown } from '/lib/markdown'
+import { type Markdown } from '/site/content-types'
 
 export function get(req: XP.Request): XP.Response {
+  const content = getContent<Content<Markdown>>()
   const component = getComponent<XP.PartComponent.Markdown>()
-  const markdownContent = component?.config.markdownContent
 
-  let markdownText
-  if (markdownContent?._selected == 'fromNode') {
-    const nodeId = markdownContent?.fromNode.nodeId ?? ''
-    const node = nodeId ? getMarkdownNode(nodeId) : null
-    markdownText = node?.markdown
-  } else {
-    markdownText = markdownContent?.fromText.text
+  let markdownText: string = ''
+  if (content?.type == 'mimir:markdown') {
+    markdownText = getMarkdownTextFromContent(content)
+  } else if (component) {
+    markdownText = getMarkdownTextFromComponent(component)
   }
 
   const props = {
@@ -23,4 +23,19 @@ export function get(req: XP.Request): XP.Response {
   return render(component, props, req, {
     body: '<section class="xp-part"></section>',
   })
+}
+
+function getMarkdownTextFromComponent(component: XP.PartComponent.Markdown): string {
+  const optionSet = component.config.markdownContent
+  if (optionSet._selected == 'fromNode') {
+    const nodeId = optionSet.fromNode.nodeId ?? ''
+    return nodeId ? getMarkdownText(nodeId) : ''
+  } else {
+    return optionSet.fromText.text ?? ''
+  }
+}
+
+function getMarkdownTextFromContent(content: Content<Markdown>): string {
+  const nodeId = content.data.nodeId ?? ''
+  return nodeId ? getMarkdownText(nodeId) : ''
 }
