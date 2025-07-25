8c893e61002e414560d476bf285f35010f4a1cac
johnnadeluy <johnnadel.salita@gmail.com>
2025-07-25 12:38:30 +0200

    Revert RSS news ingress fetching and html strip function

--- src/main/resources/lib/ssb/utils/textUtils.ts
+++ src/main/resources/lib/ssb/utils/textUtils.ts
@@ -1,7 +1,3 @@
 export function sanitizeForSolr(term: string): string {
   return term.replace(/['<>;.,´`"]/g, '').replace(/\\+/g, '&2B')
 }
-
-export function stripHtmlFromText(text: string): string {
-  return text.replace(/<[^>]*>/g, '')
-}
