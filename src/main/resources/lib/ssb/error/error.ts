756d0283638c4b41b94f1d52e4e1125ebd00ae7b
Anne-Siri <142512638+annesiri@users.noreply.github.com>
2025-09-01 14:31:56 +0200

    fix: improve logging of errors in mimir app [MIM-2297] (#3420)
    * improvment error
    
    * improved error messages
    
    * adde more logging
    
    * removed unneccessary code
    
    * fix typo

--- src/main/resources/lib/ssb/error/error.ts
+++ src/main/resources/lib/ssb/error/error.ts
@@ -3,20 +3,22 @@ import { render } from '/lib/thymeleaf'
 export interface ErrorInterface {
   errorTitle: string
   errorBody: string
-  errorLog: void
 }
 
 const errorView = resolve('./error.html')
 
 export function renderError(req: XP.Request, title: string, exception: Error): XP.Response {
   const model: ErrorInterface = {
-    errorBody: exception.message,
+    errorBody: exception?.message ?? '',
     errorTitle: title,
-    errorLog: log.error(exception.stack),
   }
 
-  const body: string | undefined =
-    req.mode === 'edit' || req.mode === 'preview' || req.mode === 'inline' ? render(errorView, model) : undefined
+  log.error(`${title}: ${exception?.stack}`)
+
+  const body: string =
+    req?.mode && (req.mode === 'edit' || req.mode === 'preview' || req.mode === 'inline')
+      ? render(errorView, model)
+      : ''
 
   return {
     body,
