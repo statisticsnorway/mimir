32bc6a9a46c8ca6c348089b4ca097c01cf1ddfc2
Anne-Siri Trønnes <ntr@ssb.no>
2025-09-10 14:47:44 +0200

    supress warnings from bootstrap using @import

--- tsup/client-asset.ts
+++ tsup/client-asset.ts
@@ -42,6 +42,8 @@ export default function buildAssetConfig(): Options {
         // These settings should match build:style in package.json somewhat
         style: isDev ? 'expanded' : 'compressed',
         loadPaths: ['node_modules', 'node_modules/bootstrap/scss', 'node_modules/@statisticsnorway'],
+        // Supress warnings from bootstrap using depricated @import
+        quietDeps: true,
         async transform(source) {
           const transformers: postcss.AcceptedPlugin[] = [autoprefixer]
           if (!isDev) transformers.push(cssnanoPlugin)
