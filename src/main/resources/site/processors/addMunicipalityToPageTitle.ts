5692e6e5a587cfd822a7daeb8fd2c65dd3bf36a4
Anne-Siri <142512638+annesiri@users.noreply.github.com>
2025-09-29 11:07:12 +0200

    fix: add municipality to html title for kommunefakta [MIM-2325] (#3462)
    * fix: Improve title for kommunefakta to improve SEO
    
    * using log.info instead of debug
    
    * adding debug logging
    
    * for debugging in utv
    
    * adding debugging for utv
    
    * test setting pageTitle
    
    * fix: add municipality to html title for kommunefakta
    
    * added comment

---
+++ src/main/resources/site/processors/addMunicipalityToPageTitle.ts
@@ -0,0 +1,20 @@
+// Processor to add municipality name to html title for "kommunefakta" after SEO-app has modified title
+
+exports.responseProcessor = (req: XP.Request, res: XP.Response) => {
+  const paramKommune: string | undefined = req.params.kommune
+
+  if (paramKommune) {
+    const headRegex = /<head>([\s\S]*?)<\/head>/
+    const titleRegex = /<title>(.*?)<\/title>/
+
+    res.body = (res.body as string).replace(headRegex, (match, headContent) => {
+      const modifiedHeadContent = headContent.replace(titleRegex, (titleElement: string, titleText: string) => {
+        const newTitleText = titleText.replace('Kommunefakta', `Kommunefakta ${paramKommune}`)
+        return titleElement.replace(titleText, newTitleText)
+      })
+      return match.replace(headContent, modifiedHeadContent)
+    })
+  }
+
+  return res
+}
