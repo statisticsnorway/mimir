e6133744430f5213f9daa661f4cea8f78de3079d
johnnadeluy <johnnadel.salita@gmail.com>
2025-10-22 10:17:11 +0200

    Fix comment

--- src/main/resources/tasks/publishStatisticsDatasetJob/publishStatisticsDatasetJob.ts
+++ src/main/resources/tasks/publishStatisticsDatasetJob/publishStatisticsDatasetJob.ts
@@ -1,6 +1,6 @@
 import { publishDataset } from '/lib/ssb/dataset/publishOld'
 
 export function run(): void {
-  log.info(`Run Task: publishJob ${new Date()}`)
+  log.info(`Run Task: publishStatisticsDatasetJob ${new Date()}`)
   publishDataset()
 }
