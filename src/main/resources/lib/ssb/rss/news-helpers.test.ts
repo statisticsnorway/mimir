1eef5ca1d8f7b9b826ae772b2914cbd080c9fee1
Anne-Siri Trønnes <ntr@ssb.no>
2025-10-27 14:17:42 +0100

    simplified tests and mock data

--- src/main/resources/lib/ssb/rss/news-helpers.test.ts
+++ src/main/resources/lib/ssb/rss/news-helpers.test.ts
@@ -25,8 +25,6 @@ describe('findLatestRelease', () => {
 
     expect(latestRelease.toDateString()).toBe('Thu Jun 26 2025')
     expect(latestVariant.frekvens).toBe('Måned')
-    expect(latestVariant.nextRelease).toBe('2025-07-24 08:00:00.0')
-    expect(latestVariant.previousRelease).toBe('2025-06-26 08:00:00.0')
   })
 
   test('should find previous release before release hours same day as next release', () => {
@@ -36,8 +34,6 @@ describe('findLatestRelease', () => {
 
     expect(latestRelease.toDateString()).toBe('Thu Jun 26 2025')
     expect(latestVariant.frekvens).toBe('Måned')
-    expect(latestVariant.nextRelease).toBe('2025-07-24 08:00:00.0')
-    expect(latestVariant.previousRelease).toBe('2025-06-26 08:00:00.0')
   })
 
   test('should find next release when next release date passed', () => {
@@ -47,8 +43,6 @@ describe('findLatestRelease', () => {
 
     expect(latestRelease.toDateString()).toBe('Tue Aug 12 2025')
     expect(latestVariant.frekvens).toBe('Kvartal')
-    expect(latestVariant.nextRelease).toBe('2025-08-12 08:00:00.0')
-    expect(latestVariant.previousRelease).toBe('2025-05-08 08:00:00.0')
   })
 })
 
@@ -69,31 +63,7 @@ const statistic: StatisticInListing = {
       previousTo: '2025-03-31 00:00:00.0',
       nextRelease: '2025-08-12 08:00:00.0',
       nextReleaseId: '200562',
-      upcomingReleases: [
-        {
-          id: '200562',
-          publishTime: '2025-08-12 08:00:00.0',
-          periodFrom: '2025-04-01 00:00:00.0',
-          periodTo: '2025-06-30 00:00:00.0',
-        },
-      ],
-    },
-    {
-      id: '180005',
-      frekvens: 'År',
-      previousRelease: '2025-02-10 08:00:00.0',
-      previousFrom: '2024-01-01 00:00:00.0',
-      previousTo: '2024-12-31 00:00:00.0',
-      nextRelease: '2026-02-11 08:00:00.0',
-      nextReleaseId: '200565',
-      upcomingReleases: [
-        {
-          id: '200565',
-          publishTime: '2026-02-11 08:00:00.0',
-          periodFrom: '2025-01-01 00:00:00.0',
-          periodTo: '2025-12-31 00:00:00.0',
-        },
-      ],
+      upcomingReleases: [],
     },
     {
       id: '180006',
@@ -103,26 +73,7 @@ const statistic: StatisticInListing = {
       previousTo: '2025-05-31 00:00:00.0',
       nextRelease: '2025-07-24 08:00:00.0',
       nextReleaseId: '200532',
-      upcomingReleases: [
-        {
-          id: '200555',
-          publishTime: '2025-10-23 08:00:00.0',
-          periodFrom: '2025-09-01 00:00:00.0',
-          periodTo: '2025-09-30 00:00:00.0',
-        },
-        {
-          id: '200556',
-          publishTime: '2025-11-27 08:00:00.0',
-          periodFrom: '2025-10-01 00:00:00.0',
-          periodTo: '2025-10-31 00:00:00.0',
-        },
-        {
-          id: '200542',
-          publishTime: '2025-12-22 08:00:00.0',
-          periodFrom: '2025-11-01 00:00:00.0',
-          periodTo: '2025-11-30 00:00:00.0',
-        },
-      ],
+      upcomingReleases: [],
     },
   ],
 }
