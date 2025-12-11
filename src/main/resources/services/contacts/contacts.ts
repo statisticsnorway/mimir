c432f3f6263aeeeef6562762727f220176d1ee92
johnnadeluy <johnnadel.salita@gmail.com>
2025-12-11 13:57:50 +0100

    refactor: Fix contacts service types

--- src/main/resources/services/contacts/contacts.ts
+++ src/main/resources/services/contacts/contacts.ts
@@ -2,8 +2,9 @@ import '/lib/ssb/polyfills/nashorn'
 import { type Request, type Response } from '@enonic-types/core'
 import { handleRepoGet } from '/lib/ssb/dashboard/statreg/repoUtils'
 import { getContactsFromRepo } from '/lib/ssb/statreg/contacts'
+import { type Contact as StatRegContacts } from '/lib/ssb/dashboard/statreg/types'
 
-const toOption = ({ id, name, email, mobile, telephone }) => ({
+const toOption = ({ id, name, email, mobile, telephone }: StatRegContacts) => ({
   id,
   displayName: name,
   description: email,
@@ -13,20 +14,23 @@ const toOption = ({ id, name, email, mobile, telephone }) => ({
   telephone,
 })
 
-const filterByDisplayName = (contacts, filters) => {
-  log.info(`searching ${filters.query} in ${contacts.length}`)
-  return contacts.filter((c) =>
-    filters.query != undefined ? c.name.toLowerCase().includes(filters.query.toLowerCase()) : 'a'
+const filterByDisplayName = (contacts: StatRegContacts[], filters: Request['params']) => {
+  log.info(`searching ${filters.query} in ${contacts?.length}`)
+  return (
+    contacts?.filter((c) =>
+      filters.query != undefined ? c.name.toLowerCase().includes((filters.query as string).toLowerCase()) : true
+    ) || []
   )
 }
 
-const filterByIds = (contacts, filters) => {
+const filterByIds = (contacts: StatRegContacts[], filters: Request['params']) => {
   return (
-    filters.ids &&
-    filters.ids.split(',').reduce((acc, id) => {
+    (filters?.ids as string | undefined)?.split(',').reduce((acc: StatRegContacts[], id) => {
       const found = contacts.find((c) => `${c.id}` === id)
-      return found ? acc.concat(found) : acc.concat({ id: id, name: 'Slettet kontakt', email: '' })
-    }, [])
+      return found
+        ? acc.concat(found)
+        : acc.concat({ id: id as unknown as number, name: 'Slettet kontakt', email: '' } as StatRegContacts)
+    }, []) || []
   )
 }
 
