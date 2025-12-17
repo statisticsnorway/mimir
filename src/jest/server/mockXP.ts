1090c1c4493a26594ea99b07a82cef3e3d58aae8
Anne-Siri Trønnes <ntr@ssb.no>
2025-12-17 13:56:23 +0100

    test: add mocking for lib-node and lib-repo in mockXP server

--- src/jest/server/mockXP.ts
+++ src/jest/server/mockXP.ts
@@ -1,8 +1,9 @@
-import { App, LibContent, LibPortal, Server } from '@enonic/mock-xp'
+import { App, LibContent, LibNode, LibPortal, LibRepo, Server } from '@enonic/mock-xp'
 import { type Log } from '@enonic-types/core'
 import { type getContent as getContentType, type imageUrl as imageUrlType } from '@enonic-types/lib-portal'
 import { type get as getType } from '@enonic-types/lib-content'
 import { jest } from '@jest/globals'
+import { type create, type get } from '@enonic-types/lib-repo'
 
 // Setting up mock server
 const APP_KEY = 'mimir'
@@ -33,6 +34,20 @@ export const mockLibContent = new LibContent({
   server,
 })
 
+// MOCK LIB CONTENT
+
+jest.mock(
+  '/lib/xp/content',
+  () => {
+    return {
+      get: jest.fn<typeof getType>((params) => mockLibContent.get(params)),
+    }
+  },
+  { virtual: true }
+)
+
+// MOCK LIB PORTAL
+
 export const mockLibPortal = new LibPortal({
   app,
   server,
@@ -57,12 +72,25 @@ jest.mock(
   { virtual: true }
 )
 
+// MOCK LIB REPO
+
+const mockLibRepo = new LibRepo({ server })
+
+export const mockStatregRepo = mockLibRepo.create({ id: 'no.ssb.statreg' })
+
 jest.mock(
-  '/lib/xp/content',
+  '/lib/xp/repo',
   () => {
     return {
-      get: jest.fn<typeof getType>((params) => mockLibContent.get(params)),
+      get: jest.fn<typeof get>((key) => mockLibRepo.get(key)),
+      create: jest.fn<typeof create>((content) => mockLibRepo.create(content)),
     }
   },
   { virtual: true }
 )
+
+// MOCK LIB NODE
+
+export const mockLibNode = new LibNode({ server })
+
+export const mockConn = mockLibNode.connect({ repoId: 'no.ssb.statreg', branch: 'master' })
