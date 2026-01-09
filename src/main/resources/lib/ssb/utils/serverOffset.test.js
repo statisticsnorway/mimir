033bb35afb15ff70221054c15f64b9764b53e3c3
Carina <cgn@ssb.no>
2026-01-09 10:38:20 +0100

    Added mocking lib/time

--- src/main/resources/lib/ssb/utils/serverOffset.test.js
+++ src/main/resources/lib/ssb/utils/serverOffset.test.js
@@ -1,5 +1,27 @@
 import { getServerOffsetInMs } from './serverOffset'
 
+jest.mock(
+  '/lib/time',
+  () => {
+    return {
+      Instant: { ofEpochMilli: (ms) => ({ ms }) },
+      ZoneId: { of: (id) => ({ id }) },
+      ZonedDateTime: {
+        ofInstant: (instant, zoneId) => ({
+          getOffset: () => ({
+            getTotalSeconds: () => {
+              if (zoneId.id !== 'Europe/Oslo') return 0
+              const m = new Date(instant.ms).getUTCMonth() + 1
+              return m >= 4 && m <= 9 ? 7200 : 3600
+            },
+          }),
+        }),
+      },
+    }
+  },
+  { virtual: true }
+)
+
 // run npm run test:utc to run these tests with TZ=UTC
 describe('date-fns-tz getServerOffsetInMs (UTC server)', () => {
   if (process.env.TZ === 'UTC') {
