bc5803fe0186ac021897c3926809d37187bd0c84
Glenruben Larsen <gle@ssb.no>
2026-06-15 14:33:42 +0200

    Add feature toggle for experimental search

--- src/main/resources/services/freeTextNewSearch/freeTextNewSearch.ts
+++ src/main/resources/services/freeTextNewSearch/freeTextNewSearch.ts
@@ -2,8 +2,16 @@ import { type Request, type Response } from '@enonic-types/core'
 import { solrTestNewSearch } from '/lib/ssb/utils/solrUtils'
 
 import { sanitizeForSolr } from '/lib/ssb/utils/textUtils'
+import { isEnabled } from '/lib/types/featureToggle'
 
 export function get(req: Request): Response {
+  if (!isEnabled('delete-joblog-cron', false, 'ssb')) {
+    return {
+      body: { error: 'feature toggle disabled' },
+      status: 403,
+    }
+  }
+
   const searchTerm = req.params.sok ? sanitizeForSolr(req.params.sok.toString()) : undefined
   // const language = req.params.language ? req.params.language.toString() : 'nb'
   // const count = req.params.count ? parseInt(req.params.count.toString()) : 15
