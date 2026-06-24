7c1f5d776bea2cd2b771da24b6351c04fc39c740
johnnadeluy <johnnadel.salita@gmail.com>
2026-06-24 09:20:28 +0200

    fix: Bugfix for key figures with k-kommuner

--- src/main/resources/lib/ssb/parts/highcharts/data/statBank.ts
+++ src/main/resources/lib/ssb/parts/highcharts/data/statBank.ts
@@ -3,9 +3,9 @@ import { getMunicipality } from '/lib/ssb/dataset/klass/municipalities'
 import { DataSource as DataSourceType } from '/lib/ssb/repo/dataset'
 import { type Dimension, type Dataset, type Category, type Data } from '/lib/types/jsonstat-toolkit'
 import { type RequestWithCode, type MunicipalityWithCounty } from '/lib/types/municipalities'
+import { type SeriesAndCategories } from '/lib/ssb/parts/highcharts/highchartsData'
 import { type Highchart, type CombinedGraph } from '/site/content-types'
 import { type DataSource } from '/site/mixins/dataSource'
-import { type SeriesAndCategories } from '/lib/ssb/parts/highcharts/highchartsData'
 
 type HighchartsValue = number | string | null
 
@@ -249,7 +249,7 @@ const barNegativeFormat = (
   }
 }
 
-const getCategoryByMunicipalityCode = (dimension: Dimension, code: string): Category | null => {
+export const getCategoryByMunicipalityCode = (dimension: Dimension, code: string): Category | null => {
   if (!code) return null
 
   const category = getCategory(dimension.Category(code))
