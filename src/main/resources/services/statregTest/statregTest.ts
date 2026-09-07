d4a6dc56931dcbf2923c2704e010224c4379ed4f
johnnadeluy <johnnadel.salita@gmail.com>
2026-09-07 15:58:22 +0200

    feat: Add statreg test service [MIM-3080]

---
+++ src/main/resources/services/statregTest/statregTest.ts
@@ -0,0 +1,27 @@
+import { type Response } from '@enonic-types/core'
+import { request, HttpResponse } from '/lib/http-client'
+
+export function get(): Response {
+  try {
+    const response: HttpResponse = request({
+      url: 'https://ssbno-statreg.intern.test.ssb.no/statistikkregisteret/api/releases',
+      method: 'GET',
+    })
+
+    return {
+      status: response.status,
+      contentType: 'application/json',
+      body: response.body,
+    }
+  } catch (error) {
+    log.error(`statregTest failed to fetch StatReg data: ${error}`)
+
+    return {
+      status: 500,
+      contentType: 'application/json',
+      body: JSON.stringify({
+        error: 'Failed to fetch data from StatReg',
+      }),
+    }
+  }
+}
